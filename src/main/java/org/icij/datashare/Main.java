49eed2598f075c7befeac3e985a2c1ba44f88181
Julien <julien.pierre.martin@gmail.com>
2016-05-09 11:22:17 +0200

    Added enum for specifying NLPPipeline and DocumentReader

--- src/main/java/org/icij/datashare/Main.java
+++ src/main/java/org/icij/datashare/Main.java
@@ -1,28 +1,31 @@
 package org.icij.datashare;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
 import java.util.*;
 import java.util.logging.Logger;
 
 import org.icij.datashare.text.Language;
 import static org.icij.datashare.text.Language.ENGLISH;
-import static org.icij.datashare.text.Language.FRENCH;
 import static org.icij.datashare.text.Language.SPANISH;
+import static org.icij.datashare.text.Language.FRENCH;
 
-import org.icij.datashare.text.NamedEntityCategory;
-import static org.icij.datashare.text.NamedEntityCategory.PERSON;
-import static org.icij.datashare.text.NamedEntityCategory.ORGANIZATION;
-import static org.icij.datashare.text.NamedEntityCategory.LOCATION;
+import org.icij.datashare.text.processing.NamedEntityCategory;
+import static org.icij.datashare.text.processing.NamedEntityCategory.PERSON;
+import static org.icij.datashare.text.processing.NamedEntityCategory.ORGANIZATION;
+import static org.icij.datashare.text.processing.NamedEntityCategory.LOCATION;
 
-import static org.icij.datashare.text.processing.NLPStage.*;
+import org.icij.datashare.text.processing.NLPStage;
+import static org.icij.datashare.text.processing.NLPStage.POS;
+import static org.icij.datashare.text.processing.NLPStage.NER;
 
 import org.icij.datashare.text.processing.NLPPipeline;
 import org.icij.datashare.text.processing.NLPPipelineFactory;
-import org.icij.datashare.text.processing.NLPStage;
+import org.icij.datashare.text.processing.NLPPipelineFactory.NLPPipelineType;
 
-import static org.icij.datashare.util.function.ThrowingFunctions.joinListComma;
+import static org.icij.datashare.text.processing.NLPPipelineFactory.NLPPipelineType.CORENLP;
+import static org.icij.datashare.text.processing.NLPPipelineFactory.NLPPipelineType.OPENNLP;
+
+import static org.icij.datashare.util.function.ThrowingFunctionUtils.join;
 
 /**
  * Datashare
@@ -37,21 +40,21 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            Language                  language  = ENGLISH;
-            List<NLPStage>            stages    = Arrays.asList(POS, NER);
-            List<NamedEntityCategory> entities  = Arrays.asList(PERSON, ORGANIZATION, LOCATION);
-            List<String>              pipelines = Arrays.asList("OpenNLP", "CoreNLP");
+            Language                    language  = ENGLISH;
+            List<NLPStage>              stages    = Arrays.asList(POS, NER);
+            List<NamedEntityCategory>   entities  = Arrays.asList(PERSON, ORGANIZATION, LOCATION);
+            List<NLPPipelineType>       pipelines = Arrays.asList(OPENNLP, CORENLP);
 
             String                    text      = PARAGRAPH.get(language);
             //Path                      path      = Paths.get(filepath);
 
             Properties props = new Properties();
             props.setProperty("language", language.toString());
-            props.setProperty("stages",   joinListComma.apply(stages));
-            props.setProperty("entities", joinListComma.apply(entities));
+            props.setProperty("stages",   join.apply(",").apply(stages));
+            props.setProperty("entities", join.apply(",").apply(entities));
 
-            for (String name : pipelines) {
-                Optional<NLPPipeline> pipeline = NLPPipelineFactory.build(logger, name, props);
+            for (NLPPipelineType type : pipelines) {
+                Optional<NLPPipeline> pipeline = NLPPipelineFactory.build(type, props, logger);
                 if (pipeline.isPresent()) {
                     pipeline.get().run(text);
                 }
@@ -74,15 +77,19 @@ public class Main {
 
     private static final Map<Language, String> PARAGRAPH =
             new HashMap<Language, String>(){{
+
                 put(ENGLISH, "Hi. How are you? This is Mike Brandson. I'd like you to tell me \n" +
                         "how POS-tagging and NER work, please!\n" +
                         "Thank you very much, Albert Einstein. Greetings from Berne, Switzerland.");
-                put(FRENCH, "Salut! Comment ça va ? C'est M. Henri Dupont. J'aimerais que vous me disiez \n" +
-                        "comment l'étiquetteur morpho-syntaxique et la reconnaissance d'entités nommées fonctionnent, s'il-vous-plaît !\n" +
+
+                put(FRENCH, "Salut! Comment ça va ? C'est M. Henri Dupont. J'aimerais que vous me disiez comment l'étiquetteur\n" +
+                        "\"morpho-syntaxique\" (POS tagger) et la reconnaissance d'entités nommées fonctionnent, s'il-vous-plaît !\n" +
                         "Merci beaucoup, Julien Martin. Salutations de Paris, France.");
+
                 put(SPANISH, "Leer y ﬁrmar dos originales del acuerdo adjunto entre Pablo Calleja y sus cliente in Barcelona, España,\n" +
                         "así como también el \"Schedule B\". Un original firmado por los socios será devuelto a ustedes Julien Martin\n" +
                         "para su referencia.");
+
             }};
 
 }
--- src/main/java/org/icij/datashare/text/processing/NLPPipelineFactory.java
+++ src/main/java/org/icij/datashare/text/processing/NLPPipelineFactory.java
@@ -3,10 +3,9 @@ package org.icij.datashare.text.processing;
 import org.icij.datashare.text.processing.corenlp.CoreNLPPipeline;
 import org.icij.datashare.text.processing.opennlp.OpenNLPPipeline;
 
-import java.io.IOException;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.Properties;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.WARNING;
@@ -17,16 +16,33 @@ import static java.util.logging.Level.WARNING;
  */
 public class NLPPipelineFactory {
 
-    public static Optional<NLPPipeline> build(Logger logger, String name, Properties props)  {
-        if (name == null || name.isEmpty()) {
-            return Optional.empty();
-        }
+    public enum NLPPipelineType {
+        OPENNLP,
+        CORENLP,
+        NONE,
+        UNKNOWN;
 
-        switch (name.trim().toLowerCase()) {
-            case "opennlp": return Optional.of(new OpenNLPPipeline(logger, props));
-            case "corenlp": return Optional.of(new CoreNLPPipeline(logger, props));
+        public static NLPPipelineType parse(final String pipeline) throws IllegalArgumentException {
+            if (pipeline == null || pipeline.isEmpty())
+                return NONE;
+            try {
+                return valueOf(pipeline.toUpperCase(Locale.ROOT));
+            } catch (IllegalArgumentException e) {
+                //throw new IllegalArgumentException(String.format("\"%s\" is not a valid natural language processing pipeline.", pipeline));
+                return UNKNOWN;
+            }
+        }
+    }
+
+    public static Optional<NLPPipeline> build(NLPPipelineType type, Properties props, Logger logger)  {
+        if (type == null)
+            return Optional.empty();
+
+        switch (type) {
+            case OPENNLP: return Optional.of(new OpenNLPPipeline(logger, props));
+            case CORENLP: return Optional.of(new CoreNLPPipeline(logger, props));
             default:
-                logger.log(WARNING, "Unknown NLPPipeline " + name);
+                logger.log(WARNING, "Unknown NLPPipeline type " + type);
                 return Optional.empty();
         }
     }
