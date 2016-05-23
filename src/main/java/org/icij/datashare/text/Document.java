3183f7b4f6f61d927873305964a326f70e7fe44c
Julien <julien.pierre.martin@gmail.com>
2016-05-23 11:25:41 +0200

    Added Document data structure

---
+++ src/main/java/org/icij/datashare/text/Document.java
@@ -0,0 +1,143 @@
+package org.icij.datashare.text;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Date;
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.SEVERE;
+import static java.util.logging.Level.WARNING;
+
+import org.icij.datashare.text.reading.DocumentParser;
+
+import static org.icij.datashare.text.hashing.Hasher.SHA_512;
+
+
+/**
+ * Created by julien on 4/26/16.
+ */
+public class Document {
+
+    private static final Logger LOGGER = Logger.getLogger(Document.class.getName());
+
+
+    public static Optional<Document> create(Path path)  {
+        if (path == null) {
+            LOGGER.log(WARNING, "Failed to create Document. Path is null");
+            return Optional.empty();
+        }
+        if ( ! Files.exists(path)) {
+            LOGGER.log(WARNING, "Failed to create Document. " + path + " does not exist.");
+            return Optional.empty();
+        }
+        if ( ! Files.isRegularFile(path)) {
+            LOGGER.log(WARNING, "Failed to create Document. " + path + " is not a regular file.");
+            return Optional.empty();
+        }
+        if ( ! Files.isReadable(path)) {
+            LOGGER.log(WARNING, "Failed to create Document. " + path + " is not readable.");
+            return Optional.empty();
+        }
+
+        return Optional.of(new Document(path));
+    }
+
+
+    private Path path;
+
+    private Date asOf;
+
+
+    private String content;
+
+    private String hash;
+
+    private String type;
+
+    private int length;
+
+    private Language language;
+
+    private Map<String, String> metadata;
+
+    private boolean read = false;
+
+    private boolean processed = false;
+
+
+    private Document(Path p) {
+        path = p;
+        asOf = new Date();
+    }
+
+
+    public Path getPath() {
+        return path;
+    }
+
+    public String getName() { return path.getName(path.getNameCount()-1).toString(); }
+
+    public Date getAsOf() { return asOf; }
+
+
+    public Optional<String> getContent() { return Optional.ofNullable(content); }
+
+    public Optional<String> getHash() { return Optional.ofNullable(hash); }
+
+    public Optional<String> getType() { return Optional.ofNullable(type); }
+
+    public Optional<Integer> getLength() { return Optional.ofNullable(length); }
+
+    public Optional<Language> getLanguage() { return Optional.ofNullable(language); }
+
+    public Optional<Map<String, String>> getMetadata() { return Optional.ofNullable(metadata); }
+
+
+    public boolean read() {
+        return read(true);
+    }
+
+    public boolean read(boolean enableOcr) {
+        if (read)
+            return true;
+
+        Optional<DocumentParser> optParser = DocumentParser.create();
+        if ( ! optParser.isPresent()) {
+            LOGGER.log(SEVERE, "Failed to create suitable DocumentParser " + path);
+            return false;
+        }
+        DocumentParser parser = optParser.get();
+
+        if ( ! enableOcr)
+            parser.disableOcr();
+
+        Optional<String> optCont = parser.parse(path);
+        if ( ! optCont.isPresent()) {
+            LOGGER.log(SEVERE, "Empty content of " + path);
+            return false;
+        }
+        content = optCont.get();
+
+        hash = SHA_512.hash(content);
+        if (hash.isEmpty()) {
+            LOGGER.log(SEVERE, "Failed to hash content of " + path);
+            return false;
+        }
+
+        language = parser.getLanguage().orElse(Language.UNKNOWN);
+
+        Optional<Map<String, String>> optMetadata = parser.getMetadata();
+        if (optMetadata.isPresent()) {
+            metadata = optMetadata.get();
+            type = parser.getType().orElse("");
+            length = parser.getLength().orElse(0);
+        }
+        read = true;
+
+        return true;
+    }
+
+}
