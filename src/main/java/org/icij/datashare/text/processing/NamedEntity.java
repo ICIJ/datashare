304c81530d66eca429fc2f45237084e1beec23e5
Julien <julien.pierre.martin@gmail.com>
2016-06-03 09:48:38 +0200

    Added Part of Speech and changed hash to SHA 384

--- src/main/java/org/icij/datashare/text/processing/NamedEntity.java
+++ src/main/java/org/icij/datashare/text/processing/NamedEntity.java
@@ -1,11 +1,14 @@
 package org.icij.datashare.text.processing;
 
 import me.xuender.unidecode.Unidecode;
+import org.icij.datashare.text.Language;
+import org.icij.datashare.text.hashing.Hasher;
 
 import java.nio.file.Path;
 import java.util.*;
 import java.util.logging.Logger;
 import static java.util.logging.Level.SEVERE;
+import static org.icij.datashare.text.hashing.Hasher.SHA_384;
 import static org.icij.datashare.text.hashing.Hasher.SHA_512;
 
 import static org.icij.datashare.text.processing.NLPPipeline.NLPPipelineType;
@@ -18,12 +21,12 @@ public class NamedEntity {
 
     private static final Logger LOGGER = Logger.getLogger(NamedEntity.class.getName());
 
+    private static final Hasher HASHER = SHA_384;
+
 
     public static Optional<NamedEntity> create(NamedEntityCategory cat, String mention)  {
         if (cat == null || mention.isEmpty()) {
-            LOGGER.log(SEVERE, "Failed to create NamedEntity; " +
-                    "undefined category " + cat +
-                    ", mention " + mention);
+            LOGGER.log(SEVERE, "Undefined parameter(s): category " + cat + ", mention " + mention);
             return Optional.empty();
         }
         return Optional.of(new NamedEntity(cat, mention));
@@ -57,11 +60,11 @@ public class NamedEntity {
     private NamedEntity(NamedEntityCategory cat, String men) {
         category = cat;
         mention = men;
-        hash = SHA_512.hash(getMentionNormalForm());
+        hash = HASHER.hash(getMentionNormalForm());
     }
 
 
-    // Actual String denoting entity
+    // Actual string denoting the named entity
     private String mention;
 
     // Mention's hash
@@ -70,8 +73,11 @@ public class NamedEntity {
     // Category (Pers, Org, Loc)
     private NamedEntityCategory category;
 
-    // Approximative offset (chars from beginning) in document
+    // Approximative offset in document (lower bound ong number of chars from beginning)
     private int offset = -1;
+
+    // Parts-of-speech associated with mention
+    private String partOfSpeech;
 
     // Tokens appearing just before
     private List<String> leftContext;
@@ -87,6 +93,9 @@ public class NamedEntity {
 
     // Type of pipeline which extracted
     private NLPPipelineType extractor;
+
+    // Language setting by pipeline at extraction
+    private Language extractorLanguage;
 
 
     public String getMention() { return mention; }
@@ -118,9 +127,19 @@ public class NamedEntity {
     }
 
 
+    public Optional<String> getPartOfSpeech() { return Optional.ofNullable(partOfSpeech); }
+
+    public void setPartOfSpeech(String pos) { partOfSpeech = pos; }
+
+
     public Optional<NLPPipelineType> getExtractor() { return Optional.ofNullable(extractor); }
 
     public void setExtractor(NLPPipelineType extr) { extractor = extr; }
+
+
+    public Optional<Language> getExtractorLanguage() { return Optional.ofNullable(extractorLanguage); }
+
+    public void setExtractorLanguage(Language extrLang) { extractorLanguage = extrLang; }
 
 
     public Optional<List<String>> getRightContext() { return Optional.ofNullable(rightContext); }
@@ -149,29 +168,41 @@ public class NamedEntity {
         if (off.isPresent())
             features.add(String.valueOf(off.getAsInt()));
         else
-            features.add("-1");
-
-        Optional<Path> p = getDocumentPath();
-        if (p.isPresent())
-            features.add(p.get().toString());
-        else
-            features.add("none");
+            features.add("NONE");
 
         Optional<NLPPipelineType> extr = getExtractor();
         if (extr.isPresent())
             features.add(extr.get().toString());
         else
-            features.add("none");
+            features.add("NONE");
+
+        Optional<Language> extrLang = getExtractorLanguage();
+        if (extrLang.isPresent())
+            features.add(extrLang.get().toString().toUpperCase(Locale.ROOT));
+        else
+            features.add("NONE");
+
+        Optional<String> pos = getPartOfSpeech();
+        if (pos.isPresent())
+            features.add(pos.get().toUpperCase(Locale.ROOT));
+        else
+            features.add("NONE");
 
         features.add(getMentionNormalForm());
 
         features.add(getHash());
 
+        Optional<Path> p = getDocumentPath();
+        if (p.isPresent())
+            features.add(p.get().toString());
+        else
+            features.add("NONE");
+
         Optional<String> docHash = getDocument();
         if (docHash.isPresent())
             features.add(docHash.get());
         else
-            features.add("none");
+            features.add("NONE");
 
         return String.join(";", features);
     }
