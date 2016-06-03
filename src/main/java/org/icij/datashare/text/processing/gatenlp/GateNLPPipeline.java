95c0a0a1b26cd32a1c860718a13b749c3169e9d7
Julien <julien.pierre.martin@gmail.com>
2016-06-03 09:50:24 +0200

    Minor code refactoring

--- src/main/java/org/icij/datashare/text/processing/gatenlp/GateNLPPipeline.java
+++ src/main/java/org/icij/datashare/text/processing/gatenlp/GateNLPPipeline.java
@@ -1,24 +1,19 @@
 package org.icij.datashare.text.processing.gatenlp;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.*;
 import java.util.*;
+import static java.util.Arrays.asList;
 import static java.util.logging.Level.INFO;
 import static java.util.logging.Level.SEVERE;
 
 import gate.Annotation;
 import gate.AnnotationSet;
-import gate.util.OffsetComparator;
 import gate.util.GateException;
 import gate.creole.ResourceInstantiationException;
 import es.upm.oeg.icij.entity.extractor.extractor.gate.GATENLPDocument;
 import es.upm.oeg.icij.entity.extractor.extractor.gate.GATENLPApplication;
 
-import org.icij.datashare.text.Document;
 import org.icij.datashare.text.Language;
 import org.icij.datashare.text.hashing.Hasher;
 import org.icij.datashare.text.processing.AbstractNLPPipeline;
@@ -42,6 +37,7 @@ public final class GateNLPPipeline extends AbstractNLPPipeline {
     private static final Path RESOURCES_BASEDIR =
             Paths.get( GateNLPPipeline.class.getPackage().getName().replace(".", "/") );
 
+    // Gate annotation types: {Person, Company, Country} + {Token, Sentence}
     public static final Map<NamedEntityCategory, String> NECATEGORY_TO_GATETYPE =
             new HashMap<NamedEntityCategory, String>(){{
                 put(ORGANIZATION, "Company");
@@ -67,9 +63,9 @@ public final class GateNLPPipeline extends AbstractNLPPipeline {
         stageDependencies.get(TOKEN).add(SENTENCE);
         stageDependencies.get(NER)  .add(TOKEN);
 
-        // Theoretically supports any Language
+        // Supposed to support any language
         for (Language lang : Language.values()) {
-            supportedStages.put(lang, new HashSet<>(Arrays.asList(SENTENCE, TOKEN, NER)));
+            supportedStages.put(lang, new HashSet<>(asList(SENTENCE, TOKEN, NER)));
         }
         supportedStages.remove(NONE);
         supportedStages.remove(UNKNOWN);
@@ -82,64 +78,62 @@ public final class GateNLPPipeline extends AbstractNLPPipeline {
     @Override
     protected void process(String input) {
 
-        Optional<String> docHash = (document != null) ? document.getHash()              : Optional.empty();
-        Optional<Path>   docPath = (document != null) ? Optional.of(document.getPath()) : Optional.empty();
+        Optional<String> docHash = Optional.ofNullable(documentHash);
+        Optional<Path>   docPath = Optional.ofNullable(documentPath);
 
         // Create Gate document
         String gateDocName = docHash.isPresent() ?
                 docHash.get() :
-                String.join(".", Arrays.asList(Hasher.SHA_512.hash(input), "txt"));
-        GATENLPDocument gateDoc;
+                String.join(".", asList(Hasher.SHA_512.hash(input), "txt"));
+
         try {
-            gateDoc = new GATENLPDocument(gateDocName, input);
+            GATENLPDocument gateDoc = new GATENLPDocument(gateDocName, input);
+
+            // Split input into tokens
+            // Group tokens into sentences
+            // Tag tokens with their recognized named entity category
+            pipeline.annotate(gateDoc);
+
+            // Retrieve recognized Named Entities
+            for (NamedEntityCategory category : targetEntities) {
+                AnnotationSet annotSet = gateDoc.getAnnotationSet(NECATEGORY_TO_GATETYPE.get(category));
+                if (annotSet != null) {
+                    for (Annotation annotation : new ArrayList<>(annotSet)) {
+                        String mention = annotation.getFeatures().get("string").toString();
+                        int mentionOffset = annotation.getStartNode().getOffset().intValue();
+                        Optional<NamedEntity> optEntity = NamedEntity.create(category, mention, mentionOffset);
+                        if (optEntity.isPresent()) {
+                            NamedEntity entity = optEntity.get();
+                            docHash.ifPresent(entity::setDocument);
+                            docPath.ifPresent(entity::setDocumentPath);
+                            entity.setExtractor(NLPPipelineType.GATENLP);
+                            entity.setExtractorLanguage(language);
+                            entities.add(entity);
+                        }
+                    }
+
+                } else {
+                    LOGGER.log(INFO, "Failed to retrieve any named entity");
+                }
+            }
+
         } catch (ResourceInstantiationException e) {
             LOGGER.log(SEVERE, "Failed to create Gate Document");
-            return;
-        }
-
-        // Split input into tokens
-        // Group tokens into sentences
-        // Tag tokens with their recognized named entity category
-        pipeline.annotate(gateDoc);
-
-        // Retrieve recognized Named Entities
-        for (NamedEntityCategory category : targetEntityCategories) {
-            AnnotationSet set = gateDoc.getAnnotationSet(NECATEGORY_TO_GATETYPE.get(category)); // {Person, Company, Country} + {Token, Sentence}
-
-            if (set != null) {
-                List<Annotation> EntityList = new ArrayList<>(set);
-                Collections.sort(EntityList, new OffsetComparator());
-
-                for (Annotation annotation : EntityList) {
-                    String mention = annotation.getFeatures().get("string").toString();
-                    int mentionOffset = annotation.getStartNode().getOffset().intValue();
-                    Optional<NamedEntity> optEntity = NamedEntity.create(category, mention, mentionOffset);
-                    if (optEntity.isPresent()) {
-                        NamedEntity entity = optEntity.get();
-                        docHash.ifPresent(entity::setDocument);
-                        docPath.ifPresent(entity::setDocumentPath);
-                        entity.setExtractor(NLPPipelineType.GATENLP);
-                        entities.add(entity);
-                    }
-                }
-
-            } else {
-                LOGGER.log(INFO, "Failed to retrieve any named entity");
-            }
         }
 
     }
 
     @Override
     protected boolean initialize() {
-        if (!super.initialize())
+        if ( ! super.initialize())
             return false;
 
         if (pipeline != null)
             return true;
 
-        ClassLoader loader = this.getClass().getClassLoader();
+        // ClassLoader loader = this.getClass().getClassLoader();
 
+        try {
 //        URL resDirURL = loader.getResource(RESOURCES_BASEDIR.toString());
 //        if (resDirURL == null ) {
 //            LOGGER.log(SEVERE, "Failed to get resource folder");
@@ -147,12 +141,7 @@ public final class GateNLPPipeline extends AbstractNLPPipeline {
 //        }
 //        File resourceDirectory = new File( resDirURL.getFile() );
 
-        Path resourceDirectoryPath = Paths.get(
-                System.getProperty("user.dir"),
-                "src", "main", "resources",
-                RESOURCES_BASEDIR.toString());
-
-        try {
+            Path resourceDirectoryPath = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", RESOURCES_BASEDIR.toString());
 
             pipeline = new GATENLPApplication(resourceDirectoryPath.toFile());
 
@@ -162,6 +151,12 @@ public final class GateNLPPipeline extends AbstractNLPPipeline {
         }
 
         return true;
+    }
+
+    @Override
+    protected void terminate() {
+        super.terminate();
+        pipeline = null;
     }
 
 }
