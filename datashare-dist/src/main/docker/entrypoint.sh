e5c1587e859058965ad5d66f71485e6a72cca906
Bruno Thomas <bruno@barreverte.fr>
2018-02-14 13:49:54 +0100

    [build][dist] adds tesseract in the docker container

--- datashare-dist/src/main/docker/entrypoint.sh
+++ datashare-dist/src/main/docker/entrypoint.sh
@@ -7,6 +7,7 @@ if [ "$1" = 'sh' ];
 then
     exec "$@"
 else
+    source /etc/profile.d/java.sh
     CLASSPATH=$(find /home/datashare/lib/ -name '*.jar' -exec echo {} \+ | sed 's/ /:/g')
-    exec java -cp "${CLASSPATH}" ${MAIN_CLASS} "$@"
+    exec java -cp "/home/datashare/dist/:${CLASSPATH}" ${MAIN_CLASS} "$@"
 fi
