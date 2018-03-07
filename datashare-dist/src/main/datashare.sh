3edbe35a6f4d08834b0c986da7c2ab53a4bcd510
Bruno Thomas <bruno@barreverte.fr>
2018-03-07 14:41:17 +0100

    [dist] datashare script wait for ES to be up

--- datashare-dist/src/main/datashare.sh
+++ datashare-dist/src/main/datashare.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-set -e
+
 datashare_version=0.7
 redis_container_name=redis:4.0.1-alpine
 elasticsearch_container_name=docker.elastic.co/elasticsearch/elasticsearch:6.1.0
 
+function create_elasticsearch_config_file {
 cat > /tmp/elasticsearch.yml << EOF
 http.host: 0.0.0.0
 transport.host: 0.0.0.0
@@ -16,7 +17,9 @@ http.cors.enabled : true
 http.cors.allow-origin : "*"
 http.cors.allow-methods : OPTIONS, HEAD, GET, POST, PUT, DELETE
 EOF
+}
 
+function create_docker_compose_file {
 cat > /tmp/datashare.yml << EOF
 version: '2'
 services:
@@ -30,7 +33,23 @@ services:
     ports:
       - "9200:9200"
 EOF
+}
 
+function wait_idx_is_up {
+    echo -n "waiting for index to be up..."
+    for i in `seq 1 300`; do
+        sleep 0.1
+        curl --silent localhost:9200 > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "OK"
+            return
+        fi
+    done
+    echo "KO"
+}
+
+create_elasticsearch_config_file
+create_docker_compose_file
 docker-compose -f /tmp/datashare.yml -p datashare up -d
 
 read -p "Folder path that contains documents [${PWD}] : " data_path
@@ -38,6 +57,8 @@ read -p 'Folder path for cache (datashare will store models here) [/tmp/dist] :'
 
 data_path=${data_path:-${PWD}}
 dist_path=${dist_path:-/tmp/dist}
+
+wait_idx_is_up
 
 docker run -ti -p 8080:8080 --network datashare_default -e DS_JAVA_OPTS="${DS_JAVA_OPTS}" \
  -v ${data_path}:/home/datashare/data:ro -v ${dist_path}:/home/datashare/dist icij/datashare:${datashare_version} $@
