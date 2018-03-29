31cf328b65ff4fece20e9706cd55ae263ec12b39
Bruno Thomas <bruno@barreverte.fr>
2018-03-29 09:55:22 +0200

    [refactor] dead code

--- datashare-api/src/main/java/org/icij/datashare/text/indexing/Indexer.java
+++ datashare-api/src/main/java/org/icij/datashare/text/indexing/Indexer.java
@@ -9,11 +9,9 @@ import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.util.*;
-import java.util.function.Function;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
-
-import static org.icij.datashare.function.ThrowingFunctions.joinComma;
 
 
 public interface Indexer extends Closeable {
@@ -21,105 +19,7 @@ public interface Indexer extends Closeable {
 
     Searcher search(Class<? extends Entity> entityClass);
 
-    enum NodeType {
-        LOCAL ("localhost",   1, 0),
-        REMOTE("kc.icij.org", 8, 2);
-
-        private final String defaultHost;
-        private final int    defaultIndexShards;
-        private final int    defaultIndexReplicas;
-
-        NodeType(final String host, final int indexShards, final int indexReplicas) {
-            this.defaultHost          = host;
-            this.defaultIndexShards   = indexShards;
-            this.defaultIndexReplicas = indexReplicas;
-        }
-
-        public static Optional<NodeType> parse(final String nodeType) {
-            if (nodeType == null || nodeType.isEmpty())
-                return Optional.empty();
-            try {
-                return Optional.of(valueOf(nodeType.toUpperCase(Locale.ROOT)));
-            } catch (IllegalArgumentException e) {
-                return Optional.empty();
-            }
-        }
-    }
-
-    enum Property {
-        NODE_TYPE,
-        HOSTS,
-        PORTS,
-        CLUSTER,
-        SHARDS,
-        REPLICAS,
-        INDEX_TYPE,
-        INDEX_JOIN_FIELD,
-        DOC_TYPE_FIELD;
-
-        public String getName() {
-            return name().toLowerCase().replace('_', '-');
-        }
-
-        public static Function<Indexer.NodeType, Function<List<String>, Function<List<Integer>, Properties>>>
-                build =
-                indexerNodeType -> indexerHostnames -> indexerHostports -> {
-                    Properties properties = new Properties();
-                    properties.setProperty(NODE_TYPE.getName(), indexerNodeType.toString());
-                    properties.setProperty(HOSTS.getName(),     joinComma.apply(indexerHostnames));
-                    properties.setProperty(PORTS.getName(),     joinComma.apply(indexerHostports));
-                    return properties;
-                };
-    }
-
-    /**
-     * Close connection to index node(s)
-     */
     void close();
-
-    /**
-     * Blocking until index is up or timed out
-     * @param index the index to wait for
-     * @return true if index is up and running; false otherwise
-     */
-    boolean awaitIndexIsUp(String index);
-
-    /**
-     * Create a new index store
-     *
-     * @param index the name of new index
-     * @return true if successfully created; false otherwise
-     */
-    boolean createIndex(final String index);
-
-    /**
-     * Delete an index store
-     *
-     * @param index the name of index to be deleted
-     * @return true if successfully deleted; false otherwise
-     */
-    boolean deleteIndex(String index);
-
-    /**
-     * @return the list of all existing indices
-     */
-    List<String> getIndices();
-
-    /**
-     * Refresh specified indices to make changes accessible to search
-     *
-     * @param indices the list of indices to refresh
-     * @return true if refreshed all shards successfully; false otherwise
-     */
-    boolean refreshIndices(String... indices);
-
-    /**
-     * Commit indices changes
-     *
-     * @param indices the list of indices to commit
-     * @return true if committed all shards successfully; false otherwise
-     */
-    boolean commitIndices(String... indices);
 
     /**
      * Add document to index from JSON as a Map
@@ -153,42 +53,6 @@ public interface Indexer extends Closeable {
 
     <T extends Entity> T get(String id);
     <T extends Entity> T get(String id, String parent);
-
-    /**
-     * Delete document {@code index} / {@code type} / {@code id}
-     *
-     * @param index the index store
-     * @param type  the index document type
-     * @param id    the index document id
-     * @return true if document successfully deleted; false otherwise
-     */
-    boolean delete(String index, String type, String id);
-
-    /**
-     * {@link Indexer#delete(String, String, String)} with {@code parent}
-     */
-    boolean delete(String index, String type, String id, String parent);
-
-    /**
-     * Get documents matching {@code query}, any type in any index
-     *
-     * @param query the string query to match
-     * @return the stream of search results as JSON Maps
-     */
-    Stream<Map<String, Object>> search(String query);
-    Stream<Map<String, Object>> search(String query, int from, int to);
-
-
-    /**
-     * Get documents matching {@code query} and {@code type} in given {@code indices}
-     *
-     * @param query   the string query to match
-     * @param type    the index document type
-     * @param indices the list of indices in which to search
-     * @return the stream of search results as JSON Maps
-     */
-    Stream<Map<String, Object>> search(String query, String type, String... indices);
-    Stream<Map<String, Object>> search(String query, int from, int to, String type, String... indices);
 
     interface Searcher {
         Searcher ofStatus(Document.Status indexed);
--- datashare-index/src/main/java/org/icij/datashare/text/indexing/elasticsearch/ElasticsearchIndexer.java
+++ datashare-index/src/main/java/org/icij/datashare/text/indexing/elasticsearch/ElasticsearchIndexer.java
@@ -1,22 +1,9 @@
 package org.icij.datashare.text.indexing.elasticsearch;
 
 import com.google.inject.Inject;
-import org.apache.lucene.search.join.ScoreMode;
-import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
-import org.elasticsearch.action.admin.cluster.state.ClusterStateRequest;
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
-import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
-import org.elasticsearch.action.admin.indices.flush.FlushRequest;
-import org.elasticsearch.action.admin.indices.flush.FlushResponse;
-import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
-import org.elasticsearch.action.admin.indices.refresh.RefreshResponse;
 import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
@@ -26,20 +13,14 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.cluster.health.ClusterHealthStatus;
-import org.elasticsearch.cluster.metadata.IndexMetaData;
-import org.elasticsearch.common.collect.ImmutableOpenMap;
-import org.elasticsearch.common.unit.ByteSizeUnit;
-import org.elasticsearch.common.unit.ByteSizeValue;
-import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.index.query.*;
-import org.elasticsearch.join.query.JoinQueryBuilders;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.ConstantScoreQueryBuilder;
+import org.elasticsearch.index.query.TermsQueryBuilder;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.script.ScriptType;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.search.sort.SortOrder;
 import org.icij.datashare.Entity;
 import org.icij.datashare.PropertiesProvider;
 import org.icij.datashare.json.JsonObjectMapper;
@@ -49,27 +30,24 @@ import org.icij.datashare.text.indexing.Indexer;
 import org.icij.datashare.text.nlp.Pipeline;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
-import static org.elasticsearch.common.unit.TimeValue.timeValueSeconds;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
-import static org.elasticsearch.index.query.QueryBuilders.*;
-import static org.icij.datashare.text.indexing.elasticsearch.ElasticsearchConfiguration.*;
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.icij.datashare.text.indexing.elasticsearch.ElasticsearchConfiguration.createESClient;
 
 
 public class ElasticsearchIndexer implements Indexer {
-    private static final int           BULKPROCESSOR_FLUSH_ACTIONS   = 2000;
-    private static final ByteSizeValue BULKPROCESSOR_FLUSH_SIZE      = new ByteSizeValue(5, ByteSizeUnit.MB);
-    private static final TimeValue     BULKPROCESSOR_FLUSH_TIME      = timeValueSeconds(20);
-    private static final int           BULKPROCESSOR_CONCURRENT_REQS = 1;
-
     private final Client client;
     private final ElasticsearchConfiguration esCfg;
 
@@ -90,92 +68,6 @@ public class ElasticsearchIndexer implements Indexer {
         client.close();
         LOGGER.info("Elasticsearch connection closed");
     }
-
-    // __________ Admin indices __________
-    @Override
-    public boolean createIndex(String index) {
-        try {
-            final CreateIndexRequest  req  = new CreateIndexRequest(index).settings(esCfg.getIndexSettings());
-            final CreateIndexResponse resp = client.admin()
-                    .indices()
-                    .create(req)
-                    .get();
-            refreshIndices(index);
-            awaitIndexIsUp(index);
-            return resp.isAcknowledged();
-
-        } catch (InterruptedException | ExecutionException e) {
-            LOGGER.error("Failed to create index " + index, e);
-            return false;
-        }
-    }
-
-    @Override
-    public List<String> getIndices() {
-        try {
-            final ClusterStateRequest req = new ClusterStateRequest();
-            final ImmutableOpenMap<String, IndexMetaData> indicesMap = client.admin().cluster()
-                    .state(req)
-                    .get()
-                    .getState()
-                    .getMetaData()
-                    .getIndices();
-            List<String> indices = new ArrayList<>();
-            indicesMap.keysIt().forEachRemaining( indices::add );
-            return indices;
-        } catch (InterruptedException | ExecutionException e) {
-            LOGGER.error("Failed to get indices", e);
-            return emptyList();
-        }
-    }
-
-    @Override
-    public boolean commitIndices(String... indices) {
-        try {
-            final FlushRequest   req = new FlushRequest(indices);
-            final FlushResponse resp = client.admin()
-                    .indices()
-                    .flush(req)
-                    .get();
-            return resp.getFailedShards() > 1;
-        } catch (InterruptedException | ExecutionException e) {
-            LOGGER.error("Failed to flush indices " + String.join(", ", asList(indices)),e);
-            return false;
-        }
-    }
-
-    @Override
-    public boolean deleteIndex(String index) {
-        try {
-            final DeleteIndexRequest  req  = new DeleteIndexRequest(index);
-            final DeleteIndexResponse resp = client.admin()
-                    .indices()
-                    .delete(req)
-                    .get();
-            return resp.isAcknowledged();
-        } catch (InterruptedException | ExecutionException e) {
-            LOGGER.error("Failed to delete index " + index, e);
-            return false;
-        }
-    }
-
-    @Override
-    public boolean refreshIndices(String... indices) {
-        try {
-            final RefreshRequest  req  = new RefreshRequest(indices);
-            final RefreshResponse resp = client.admin()
-                    .indices()
-                    .refresh(req)
-                    .get();
-            return resp.getFailedShards() > 1;
-        } catch (InterruptedException | ExecutionException e) {
-            LOGGER.error("Failed to refresh indices " + String.join(", ", asList(indices)), e);
-            return false;
-        }
-    }
-
-
-    // __________  Add Document __________
 
     @Override
     public boolean add(String index, String type, String id, Map<String, Object> json) {
@@ -209,9 +101,7 @@ public class ElasticsearchIndexer implements Indexer {
                         .field("status", Document.Status.DONE)
                         .endObject()).routing(routing));
         bulkRequest.add(new UpdateRequest(esCfg.indexName, esCfg.indexType, parent.getId())
-                .script(new Script(
-                        ScriptType.INLINE,
-                        "painless",
+                .script(new Script(ScriptType.INLINE, "painless",
                         "if (!ctx._source.nerTags.contains(params.nerTag)) ctx._source.nerTags.add(params.nerTag)",
                         new HashMap<String, Object>() {{put("nerTag", nerType.toString());}})).routing(routing));
 
@@ -268,7 +158,6 @@ public class ElasticsearchIndexer implements Indexer {
         return (parent != null) ? req.routing(parent) : req;
     }
 
-
     public <T extends Entity> T get(String id) {
         return get(id, id);
     }
@@ -293,108 +182,9 @@ public class ElasticsearchIndexer implements Indexer {
         return null;
     }
 
-    // __________ Delete Document __________
-    @Override
-    public boolean delete(String index, String type, String id) {
-        try {
-            final DeleteResponse resp = client.delete( deleteRequest(index, type, id) ).get();
-            return asList(RestStatus.FOUND, RestStatus.OK).contains(resp.status());
-        } catch (InterruptedException | ExecutionException e) {
-            LOGGER.error("Failed to delete doc " + id  + " of type " + type + " in index " + index, e);
-            return false;
-        }
-    }
-
-    @Override
-    public boolean delete(String index, String type, String id, String parent) {
-        try {
-            final DeleteResponse resp = client.delete( deleteRequest(index, type, id, parent) ).get();
-            return asList(RestStatus.FOUND, RestStatus.OK).contains(resp.status());
-        } catch (InterruptedException | ExecutionException e) {
-            LOGGER.error("Failed to delete doc " + id + " of type " + type + " in index " + index, e);
-            return false;
-        }
-    }
-
-    private DeleteRequest deleteRequest(String index, String type, String id) {
-        return deleteRequest(index, type, id, null);
-    }
-
-    private DeleteRequest deleteRequest(String index, String type, String id, String parent) {
-        DeleteRequest req = new DeleteRequest(index, esCfg.indexType, id);
-        return (parent != null) ? req.routing(parent) : req;
-    }
-
-    // __________ Search Document(s) __________
-
-    @Override
-    public Stream<Map<String, Object>> search(String query) {
-        return search( queryStringQuery(query) );
-    }
-
-    @Override
-    public Stream<Map<String, Object>> search(String query, int from, int size) {
-        return search(queryStringQuery(query), from, size);
-    }
-
-
-    @Override
-    public Stream<Map<String, Object>> search(String query, String type, String... indices) {
-        return search( queryStringQuery(query), type, indices);
-    }
-
-    @Override
-    public Stream<Map<String, Object>> search(String query, int from, int to, String type, String... indices) {
-        return search( queryStringQuery(query), from, to, type, indices);
-    }
-
     @Override
     public Searcher search(Class<? extends Entity> entityClass) {
         return new ElasticsearchSearcher(client, esCfg, entityClass);
-    }
-
-    // __________ Search Document(s) with(out) children __________
-
-    private Stream<Map<String, Object>> search(QueryBuilder query) {
-        return resultStream(searchRequest().setQuery( query ));
-    }
-
-    private Stream<Map<String, Object>> search(QueryBuilder query, int from, int size) {
-        return resultStream(searchRequest(from, size).setQuery( query ));
-    }
-
-    private Stream<Map<String, Object>> search(QueryBuilder query, String type) {
-        return resultStream(searchRequest().setQuery( query ).setTypes( type ));
-    }
-
-    private Stream<Map<String, Object>> search(QueryBuilder query, String type, String... indices) {
-        return resultStream(searchRequest().setQuery( query ).setTypes( type ).setIndices( indices ));
-    }
-
-    private Stream<Map<String, Object>> search(QueryBuilder query, int from, int to, String type, String... indices) {
-        return resultStream(searchRequest(from, to).setQuery( query ).setTypes( type ).setIndices( indices ));
-    }
-
-    /**
-     * https://www.elastic.co/guide/en/elasticsearch/client/java-api/current/java-search.html
-     */
-    private SearchRequestBuilder searchRequest() {
-        return searchRequest(DEFAULT_SEARCH_FROM, DEFAULT_SEARCH_SIZE);
-    }
-
-    private SearchRequestBuilder searchRequest(int from, int size) {
-        return client.prepareSearch()
-                .addSort("_doc", SortOrder.ASC)
-                .setFrom(from)
-                .setSize(size);
-    }
-
-    private static <T extends Entity> Stream<T> resultStream(Class<T> cls, SearchRequestBuilder srchReq) {
-        return resultStream(cls, searchHitIterable(srchReq));
-    }
-
-    private static Iterable<SearchHit> searchHitIterable(SearchRequestBuilder searchRequest) {
-        return () -> searchRequest.get().getHits().iterator();
     }
 
     private static Stream<SearchHit> searchHitStream(Iterable<SearchHit> searchHitIterable) {
@@ -407,66 +197,6 @@ public class ElasticsearchIndexer implements Indexer {
 
     private static <T extends Entity> T hitToObject(SearchHit searchHit, Class<T> cls) {
         return JsonObjectMapper.getObject(searchHit.getId(), searchHit.getSourceAsMap(), cls);
-    }
-
-    /**
-     * Build a search result as JSON Maps from search hit
-     * (Add "_index", "_type", "_id" metadata to Map)
-     *
-     * @param searchHit the search hit to build the Map from
-     * @return a Stream of JSONs as {@code Map}s
-     */
-    private static Map<String, Object> hitToJson(SearchHit searchHit) {
-        return new HashMap<String, Object>( searchHit.getSourceAsMap() ) {{
-            put("_index", searchHit.getIndex());
-            put("_type",  searchHit.getType());
-            put("_id",    searchHit.getId());
-        }};
-    }
-
-    private static Stream<Map<String, Object>> resultStream(Iterable<SearchHit> searchHitIterable) {
-        return searchHitStream(searchHitIterable).map( ElasticsearchIndexer::hitToJson );
-    }
-
-    private static Stream<Map<String, Object>> resultStream(SearchRequestBuilder srchReq) {
-        return resultStream( searchHitIterable(srchReq) );
-    }
-
-    /**
-     * https://www.elastic.co/guide/en/elasticsearch/reference/2.3/query-dsl-has-child-query.html
-     */
-    private static QueryBuilder hasChildQuery(String type, QueryBuilder query) {
-        return JoinQueryBuilders.hasChildQuery(type, query, ScoreMode.None);
-    }
-
-    private static QueryBuilder mustHasChildQuery(String type) {
-        return boolQuery().must( hasChildQuery(type, matchAllQuery()) );
-    }
-
-    private static QueryBuilder mustHasChildQuery(String type, String query) {
-        return boolQuery().must( hasChildQuery(type,  queryStringQuery(query)) );
-    }
-
-    /**
-     * https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-has-parent-query.html
-     */
-    private static QueryBuilder hasParentQuery(String type, QueryBuilder query) {
-        return JoinQueryBuilders.hasParentQuery(type, query, false);
-    }
-
-    @Override
-    public boolean awaitIndexIsUp(String index) {
-        return awaitIndexIsUp(index, ClusterHealthStatus.GREEN);
-    }
-
-    private boolean awaitIndexIsUp(String index, ClusterHealthStatus status) {
-        ClusterHealthResponse resp = client.admin()
-                .cluster()
-                .prepareHealth(index)
-                .setWaitForStatus(status)
-                .setTimeout(timeValueSeconds(DEFAULT_TIMEOUT_INSEC))
-                .get();
-        return ! resp.isTimedOut();
     }
 
     public ElasticsearchIndexer withRefresh(WriteRequest.RefreshPolicy refresh) {
