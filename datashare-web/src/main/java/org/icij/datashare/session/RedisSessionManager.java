50e82332c0585ec3ee56af79b12b78cff074b2d2
Bruno Thomas <bruno@barreverte.fr>
2018-06-01 15:43:30 +0200

    [web] adds ttl to session manager

--- datashare-web/src/main/java/org/icij/datashare/session/RedisSessionManager.java
+++ datashare-web/src/main/java/org/icij/datashare/session/RedisSessionManager.java
@@ -2,17 +2,26 @@ package org.icij.datashare.session;
 
 import org.icij.datashare.PropertiesProvider;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
 
 import java.util.Map;
 
+import static java.util.Optional.ofNullable;
+
 public class RedisSessionManager {
     private final Jedis redis;
+    private final Integer ttl;
+
     public RedisSessionManager(PropertiesProvider propertiesProvider) {
         this.redis = new Jedis(propertiesProvider.getProperties().getProperty("messageBusAddress"));
+        this.ttl = Integer.valueOf(ofNullable(propertiesProvider.getProperties().getProperty("sessionTtlSeconds")).orElse("0"));
     }
 
     public void createSession(String id, Map<String, String> sessionMap) {
-        this.redis.hmset(id, sessionMap);
+        Transaction transaction = this.redis.multi();
+        transaction.hmset(id, sessionMap);
+        transaction.expire(id, this.ttl);
+        transaction.exec();
     }
 
     public Map<String, String> getSession(String id) {
--- datashare-web/src/test/java/org/icij/datashare/session/RedisSessionManagerTest.java
+++ datashare-web/src/test/java/org/icij/datashare/session/RedisSessionManagerTest.java
@@ -11,6 +11,7 @@ import static org.fest.assertions.Assertions.assertThat;
 public class RedisSessionManagerTest {
     RedisSessionManager sessionManager = new RedisSessionManager(new PropertiesProvider(new HashMap<String, String>() {{
         put("messageBusAddress", "redis");
+        put("sessionTtlSeconds", "1");
     }}));
 
     @Test
@@ -19,5 +20,14 @@ public class RedisSessionManagerTest {
         sessionManager.createSession("id", sessionMap);
 
         assertThat(sessionManager.getSession("id")).isEqualTo(sessionMap);
+    }
+
+    @Test
+    public void test_session_ttl() throws Exception {
+        Map<String, String> sessionMap = new HashMap<String, String>() {{ put("key", "val");}};
+        sessionManager.createSession("id", sessionMap);
+        Thread.sleep(2000); // BT: beurk this test is not deterministic. I don't see how to test it differently
+
+        assertThat(sessionManager.getSession("id")).isEmpty();
     }
 }
