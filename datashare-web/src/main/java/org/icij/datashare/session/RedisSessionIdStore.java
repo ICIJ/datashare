62529c0acbf3fca449ec09a22981f20458f837ea
Bruno Thomas <bruno@barreverte.fr>
2018-06-25 13:31:40 +0200

    [fix][multiuser] jedis is not threadsafe : should use JedisPool

--- datashare-web/src/main/java/org/icij/datashare/session/RedisSessionIdStore.java
+++ datashare-web/src/main/java/org/icij/datashare/session/RedisSessionIdStore.java
@@ -4,35 +4,43 @@ import com.google.inject.Inject;
 import net.codestory.http.security.SessionIdStore;
 import org.icij.datashare.PropertiesProvider;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Transaction;
 
 import static java.util.Optional.ofNullable;
 
 public class RedisSessionIdStore implements SessionIdStore {
-    private final Jedis redis;
+    private final JedisPool redis;
     private final Integer ttl;
 
     @Inject
     public RedisSessionIdStore(PropertiesProvider propertiesProvider) {
-        this.redis = new Jedis(propertiesProvider.getProperties().getProperty("messageBusAddress"));
+        this.redis = new JedisPool(new JedisPoolConfig(), propertiesProvider.getProperties().getProperty("messageBusAddress"));
         this.ttl = Integer.valueOf(ofNullable(propertiesProvider.getProperties().getProperty("sessionTtlSeconds")).orElse("1"));
     }
 
     @Override
     public void put(final String sessionId, final String login) {
-        Transaction transaction = this.redis.multi();
-        transaction.set(sessionId, login);
-        transaction.expire(sessionId, this.ttl);
-        transaction.exec();
+        try (Jedis jedis = redis.getResource()) {
+            Transaction transaction = jedis.multi();
+            transaction.set(sessionId, login);
+            transaction.expire(sessionId, this.ttl);
+            transaction.exec();
+        }
     }
 
     @Override
     public void remove(String sessionId) {
-        this.redis.del(sessionId);
+        try (Jedis jedis = redis.getResource()) {
+            jedis.del(sessionId);
+        }
     }
 
     @Override
     public String getLogin(String sessionId) {
-        return this.redis.get(sessionId);
+        try (Jedis jedis = redis.getResource()) {
+            return jedis.get(sessionId);
+        }
     }
 }
--- datashare-web/src/main/java/org/icij/datashare/session/RedisUsers.java
+++ datashare-web/src/main/java/org/icij/datashare/session/RedisUsers.java
@@ -5,17 +5,19 @@ import net.codestory.http.security.User;
 import net.codestory.http.security.Users;
 import org.icij.datashare.PropertiesProvider;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Transaction;
 
 import static java.util.Optional.ofNullable;
 
 public class RedisUsers implements Users {
-    private final Jedis redis;
+    private final JedisPool redis;
     private final Integer ttl;
 
     @Inject
     public RedisUsers(PropertiesProvider propertiesProvider) {
-        this.redis = new Jedis(propertiesProvider.getProperties().getProperty("messageBusAddress"));
+        redis = new JedisPool(new JedisPoolConfig(), propertiesProvider.getProperties().getProperty("messageBusAddress"));
         this.ttl = Integer.valueOf(ofNullable(propertiesProvider.getProperties().getProperty("sessionTtlSeconds")).orElse("1"));
     }
 
@@ -30,17 +32,23 @@ public class RedisUsers implements Users {
     }
 
     void createUser(OAuth2User user) {
-        Transaction transaction = this.redis.multi();
-        transaction.hmset(user.login(), user.userMap);
-        transaction.expire(user.login(), this.ttl);
-        transaction.exec();
+        try (Jedis jedis = redis.getResource()) {
+            Transaction transaction = jedis.multi();
+            transaction.hmset(user.login(), user.userMap);
+            transaction.expire(user.login(), this.ttl);
+            transaction.exec();
+        }
     }
 
     OAuth2User getUser(String login) {
-        return new OAuth2User(this.redis.hgetAll(login));
+        try (Jedis jedis = redis.getResource()) {
+            return new OAuth2User(jedis.hgetAll(login));
+        }
     }
 
     void removeUser(String login) {
-        this.redis.del(login);
+        try (Jedis jedis = redis.getResource()) {
+            jedis.del(login);
+        }
     }
 }
