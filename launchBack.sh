b888be42127b59533a55bac30cdb5b9d95eec3fe
Bruno Thomas <bruno@barreverte.fr>
2018-07-10 10:41:56 +0200

    [dev] updates launchBack for cors headers

--- launchBack.sh
+++ launchBack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+VERSION=$(cat pom.xml | grep '<version>[0-9.]\+' | sed 's/<version>\([0-9.]\+\)<\/version>/\1/g' | tr -d '[:space:]')
 
-CLASSPATH=$(find datashare-dist/target/datashare-dist-0.8-all/lib/ -name '*.jar' -exec echo {} \+ | sed 's/ /:/g')
+CLASSPATH=$(find datashare-dist/target/datashare-dist-${VERSION}-all/lib/ -name '*.jar' -exec echo {} \+ | sed 's/ /:/g')
 
-java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -DPROD_MODE=true -cp "dist/:${CLASSPATH}" org.icij.datashare.cli.DatashareCli $*
+java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -DPROD_MODE=true -cp "dist/:${CLASSPATH}" org.icij.datashare.cli.DatashareCli --cors '*' $*
