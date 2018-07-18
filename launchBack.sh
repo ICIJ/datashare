16473358519a0ee318142a44d7cd05f80e795fdb
Bruno Thomas <bruno@barreverte.fr>
2018-07-18 15:19:23 +0200

    [fix] load order of datashare jar

--- launchBack.sh
+++ launchBack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -x
 VERSION=$(cat pom.xml | grep '<version>[0-9.]\+' | sed 's/<version>\([0-9.]\+\)<\/version>/\1/g' | tr -d '[:space:]')
 
-CLASSPATH=$(find datashare-dist/target/datashare-dist-${VERSION}-all/lib/ -name '*.jar' -exec echo {} \+ | sed 's/ /:/g')
+CLASSPATH=$(find datashare-dist/target/datashare-dist-${VERSION}-all/lib/ -name '*.jar' | sort  | xargs | sed 's/ /:/g')
 
 java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -Djavax.net.ssl.trustStorePassword=changeit -DPROD_MODE=true -cp "dist/:${CLASSPATH}" org.icij.datashare.cli.DatashareCli --cors '*' "$@"
