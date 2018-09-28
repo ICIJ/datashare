1b697285fbd07c945ea185fc257e3c9cde8ab71b
Bruno Thomas <bruno@barreverte.fr>
2018-09-28 16:14:37 +0000

    updates the datashare.sh script

--- datashare-dist/src/main/datashare.sh
+++ datashare-dist/src/main/datashare.sh
@@ -4,21 +4,6 @@ datashare_version=0.30
 redis_image=redis:4.0.1-alpine
 elasticsearch_image=docker.elastic.co/elasticsearch/elasticsearch:6.3.0
 
-function create_elasticsearch_config_file {
-cat > /tmp/elasticsearch.yml << EOF
-http.host: 0.0.0.0
-transport.host: 0.0.0.0
-cluster.name: "datashare"
-discovery.type: "single-node"
-discovery.zen.minimum_master_nodes: 1
-xpack.license.self_generated.type: basic
-# CORS
-http.cors.enabled : true
-http.cors.allow-origin : "*"
-http.cors.allow-methods : OPTIONS, HEAD, GET, POST, PUT, DELETE
-EOF
-}
-
 function create_docker_compose_file {
 cat > /tmp/datashare.yml << EOF
 version: '2'
@@ -30,19 +15,48 @@ services:
 
   elasticsearch:
     image: ${elasticsearch_image}
-    volumes:
-      - /tmp/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml
+    environment:
+      - "http.host=0.0.0.0"
+      - "transport.host=0.0.0.0"
+      - "cluster.name=datashare"
+      - "discovery.type=single-node"
+      - "discovery.zen.minimum_master_nodes=1"
+      - "xpack.license.self_generated.type=basic"
+      - "http.cors.enabled=true"
+      - "http.cors.allow-origin=*"
+      - "http.cors.allow-methods=OPTIONS, HEAD, GET, POST, PUT, DELETE"
     ports:
       - "9200:9200"
 EOF
 }
 
-create_elasticsearch_config_file
+function usage {
+  echo "Usage : $0 [-D data_directory][-M ner_models_directory] datashare args"
+  exit 1
+}
+
+while getopts ":D:M:h" opt; do
+  case $opt in
+    D)
+      data_path=$OPTARG
+      shift "$((OPTIND-1))"
+      ;;
+    M)
+      dist_path=$OPTARG
+      shift "$((OPTIND-1))"
+      ;;
+    h)
+      usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      ;;
+  esac
+done
+
 create_docker_compose_file
 docker-compose -f /tmp/datashare.yml -p datashare up -d
-
-read -p "Folder path that contains documents [${PWD}] : " data_path
-read -p 'Folder path for cache (datashare will store models here) [/tmp/dist] :' dist_path
 
 data_path=${data_path:-${PWD}}
 dist_path=${dist_path:-/tmp/dist}
