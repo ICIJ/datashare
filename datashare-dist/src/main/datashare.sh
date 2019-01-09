392ba44559216debf9153ca1fd57a41c57bdacfc
Bruno Thomas <bruno@barreverte.fr>
2019-01-09 18:50:27 +0000

    [deploy] improves the datashare script

--- datashare-dist/src/main/datashare.sh
+++ datashare-dist/src/main/datashare.sh
@@ -10,8 +10,6 @@ version: '2'
 services:
   redis:
     image: ${redis_image}
-    ports:
-      - 6379:6379
 
   elasticsearch:
     image: ${elasticsearch_image}
@@ -25,27 +23,40 @@ services:
       - "http.cors.enabled=true"
       - "http.cors.allow-origin=*"
       - "http.cors.allow-methods=OPTIONS, HEAD, GET, POST, PUT, DELETE"
-    ports:
-      - "9200:9200"
 EOF
 }
 
 function usage {
-  echo "Usage : $0 [-D data_directory][-M ner_models_directory] datashare args"
+  cmd=$(basename $0)
+  echo "Usage : ${cmd} [-D data/directory][-M ner/models/directory][-B][-h] datashare args"
+  echo "    Data directory: where the documents are located. Default value current dir."
+  echo "    Ner models directory: where NER models are stored. Default value /tmp/dist"
+  echo "    -B: background mode"
+  echo "    -h: this help"
+  echo "    datashare args: type ${cmd} without arguments to see the datashare args list"
+
   exit 1
 }
 
-while getopts ":D:M:h" opt; do
-  case $opt in
+NB_ARGS=0
+BACKGROUND=""
+while getopts ":D:M:hB" opt
+do
+  case "${opt}" in
     D)
       data_path=$OPTARG
-      shift "$((OPTIND-1))"
+      ((NB_ARGS=NB_ARGS+2))
       ;;
     M)
       dist_path=$OPTARG
-      shift "$((OPTIND-1))"
+      ((NB_ARGS=NB_ARGS+2))
+      ;;
+    B)
+      ((NB_ARGS=NB_ARGS+1))
+      BACKGROUND="-d"
       ;;
     h)
+      ((NB_ARGS=NB_ARGS+1))
       usage
       ;;
     :)
@@ -60,6 +71,14 @@ docker-compose -f /tmp/datashare.yml -p datashare up -d
 
 data_path=${data_path:-${PWD}}
 dist_path=${dist_path:-/tmp/dist}
+echo "binding data directory to ${data_path}"
+echo "binding NER models directory to ${dist_path}"
 
-docker run -ti -p 8080:8080 --network datashare_default -e DS_JAVA_OPTS="${DS_JAVA_OPTS}" \
+shift ${NB_ARGS}
+image_running=$(docker inspect --format='{{.Config.Image}}' datashare 2>/dev/null)
+if [ -n "${image_running}" ]; then
+  docker rm -f datashare > /dev/null
+fi
+
+docker run -ti ${BACKGROUND} -p 8080:8080 --network datashare_default --name datashare --rm -e DS_JAVA_OPTS="${DS_JAVA_OPTS}" \
  -v ${data_path}:/home/datashare/data:ro -v ${dist_path}:/home/datashare/dist icij/datashare:${datashare_version} "$@"
