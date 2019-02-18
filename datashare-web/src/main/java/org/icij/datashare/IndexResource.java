e93ad46e085d25cc33973b41c2ce5b6a9407ae25
Bruno Thomas <bruno@barreverte.fr>
2019-02-18 17:27:30 +0000

    removes hack with s/datashare/user-datashare/
    cf ICIJ/datashare/#80

--- datashare-web/src/main/java/org/icij/datashare/IndexResource.java
+++ datashare-web/src/main/java/org/icij/datashare/IndexResource.java
@@ -5,6 +5,7 @@ import net.codestory.http.Context;
 import net.codestory.http.Query;
 import net.codestory.http.annotations.*;
 import net.codestory.http.errors.ForbiddenException;
+import net.codestory.http.errors.UnauthorizedException;
 import net.codestory.http.io.InputStreams;
 import net.codestory.http.payload.Payload;
 import net.codestory.http.types.ContentTypes;
@@ -43,14 +44,14 @@ public class IndexResource {
         return indexer.createIndex(((User)context.currentUser()).indexName()) ? created() : ok();
     }
 
-    @Get("/search/:path")
-    public Payload esGet(final String path, Context context) throws IOException {
-        return createPayload(http.newCall(new Request.Builder().url(getUrl(path, context)).get().build()).execute());
+    @Get("/search/:index/:path")
+    public Payload esGet(final String index, final String path, Context context) throws IOException {
+        return createPayload(http.newCall(new Request.Builder().url(getUrl(index, path, context)).get().build()).execute());
     }
 
-    @Post("/search/:path")
-    public Payload esPost(final String path, Context context, final net.codestory.http.Request request) throws IOException {
-        return createPayload(http.newCall(new Request.Builder().url(getUrl(path, context)).post(new RequestBody() {
+    @Post("/search/:index/:path")
+    public Payload esPost(final String index, final String path, Context context, final net.codestory.http.Request request) throws IOException {
+        return createPayload(http.newCall(new Request.Builder().url(getUrl(index, path, context)).post(new RequestBody() {
             @Override
             public MediaType contentType() {
                 return MediaType.parse(request.contentType());
@@ -62,20 +63,20 @@ public class IndexResource {
         }).build()).execute());
     }
 
-    @Head("/search/:path")
-    public Payload esHead(final String path, Context context) throws IOException {
-        return createPayload(http.newCall(new Request.Builder().url(getUrl(path, context)).head().build()).execute());
+    @Head("/search/:index/:path")
+    public Payload esHead(final String index, final String path, Context context) throws IOException {
+        return createPayload(http.newCall(new Request.Builder().url(getUrl(index, path, context)).head().build()).execute());
     }
 
-    @Options("/search/:path")
-    public Payload esOptions(final String path, Context context) throws IOException {
-        return createPayload(http.newCall(new Request.Builder().url(getUrl(path, context)).method("OPTIONS", null).build()).execute());
+    @Options("/search/:index/:path")
+    public Payload esOptions(final String index, final String path, Context context) throws IOException {
+        return createPayload(http.newCall(new Request.Builder().url(getUrl(index, path, context)).method("OPTIONS", null).build()).execute());
     }
 
     @Get("/src/:index/:id?routing=:routing")
     public Payload getSourceFile(final String index, final String id,
                                  final String routing, final Context context) throws IOException {
-        if (((HashMapUser)context.currentUser()).getIndices().contains(index)) {
+        if (isGranted((HashMapUser)context.currentUser(), index)) {
             return routing == null ? getPayload(indexer.get(index, id)) : getPayload(indexer.get(index, id, routing));
         }
         throw new ForbiddenException();
@@ -92,12 +93,19 @@ public class IndexResource {
     }
 
     @NotNull
-    private String getUrl(String path, Context context) {
-        String s = es_url + "/" + path.replace("datashare", context.currentUser().login() + "-" + "datashare");
-        if (context.query().keyValues().size() > 0) {
-            s += "?" + getQueryAsString(context.query());
+    private String getUrl(String index, String path, Context context) {
+        if (isGranted((HashMapUser)context.currentUser(), index)) {
+            String s = es_url + "/" + index + "/" + path;
+            if (context.query().keyValues().size() > 0) {
+                s += "?" + getQueryAsString(context.query());
+            }
+            return s;
         }
-        return s;
+        throw new UnauthorizedException();
+    }
+
+    private boolean isGranted(HashMapUser user, String index) {
+        return user.getIndices().contains(index);
     }
 
     static String getQueryAsString(final Query query) {
--- datashare-web/src/test/java/org/icij/datashare/IndexResourceTest.java
+++ datashare-web/src/test/java/org/icij/datashare/IndexResourceTest.java
@@ -3,10 +3,9 @@ package org.icij.datashare;
 import net.codestory.http.WebServer;
 import net.codestory.http.filters.basic.BasicAuthFilter;
 import net.codestory.http.misc.Env;
-import net.codestory.http.security.Users;
 import net.codestory.rest.FluentRestTest;
-import org.icij.datashare.session.LocalUserFilter;
 import org.icij.datashare.session.HashMapUser;
+import org.icij.datashare.session.LocalUserFilter;
 import org.icij.datashare.text.indexing.Indexer;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,52 +35,55 @@ public class IndexResourceTest implements FluentRestTest {
 
     @Test
     public void test_no_auth_get_forward_request_to_elastic() {
-        get("/api/index/search/foo/bar").should().respond(200)
+        get("/api/index/search/local-datashare/foo/bar").should().respond(200)
                 .contain("I am elastic GET")
-                .contain("uri=foo/bar");
+                .contain("uri=local-datashare/foo/bar");
+    }
+    @Test
+    public void test_no_auth_get_unauthorized_on_unknown_index() {
+        get("/api/index/search/hacker/bar/baz").should().respond(401);
     }
     @Test
     public void test_no_auth_post_forward_request_to_elastic_with_body() {
         String body = "{\"body\": \"es\"}";
-        post("/api/index/search/foo/bar", body).should().respond(200)
+        post("/api/index/search/local-datashare/foo/bar", body).should().respond(200)
                         .contain("I am elastic POST")
-                        .contain("uri=foo/bar")
+                        .contain("uri=local-datashare/foo/bar")
                         .contain(body);
     }
     @Test
-    public void test_auth_forward_request_with_user_login_as_index_prefix() {
+    public void test_auth_forward_request_with_user_logged_on() {
         server.configure(routes -> routes.add(new IndexResource(new PropertiesProvider(new HashMap<String, String>() {{
                     put("elasticsearchAddress", "http://localhost:" + mockElastic.port());
-                }}), mockIndexer)).filter(new BasicAuthFilter("/", "icij", Users.singleUser("cecile","pass"))));
+                }}), mockIndexer)).filter(new BasicAuthFilter("/", "icij", HashMapUser.singleUser("cecile"))));
 
-        get("/api/index/search/index_name/foo/bar?routing=baz").withPreemptiveAuthentication("cecile", "pass").should().respond(200)
-                .contain("uri=index_name/foo/bar?routing=baz");
-        post("/api/index/search/index_name/foo/bar").withPreemptiveAuthentication("cecile", "pass").should().respond(200)
-                .contain("uri=index_name/foo/bar");
-
-        get("/api/index/search/datashare/foo/bar?routing=baz").withPreemptiveAuthentication("cecile", "pass").should().respond(200)
+        get("/api/index/search/cecile-datashare/foo/bar?routing=baz").withPreemptiveAuthentication("cecile", "").should().respond(200)
                 .contain("uri=cecile-datashare/foo/bar?routing=baz");
-        post("/api/index/search/datashare/foo/bar").withPreemptiveAuthentication("cecile", "pass").should().respond(200)
+        post("/api/index/search/cecile-datashare/foo/bar").withPreemptiveAuthentication("cecile", "").should().respond(200)
                 .contain("uri=cecile-datashare/foo/bar");
+
+        get("/api/index/search/hacker/foo/bar?routing=baz").withPreemptiveAuthentication("cecile", "").should().respond(401);
+        post("/api/index/search/hacker/foo/bar").withPreemptiveAuthentication("cecile", "").should().respond(401);
     }
+
     @Test
     public void test_delete_should_return_method_not_allowed() {
         delete("/api/index/search/foo/bar").should().respond(405);
     }
 
     @Test
-    public void test_put_create_local_index_in_local_mode() throws Exception {
-        put("/api/index/create").should().respond(200);
-        verify(mockIndexer).createIndex("local-datashare");
+    public void test_put_should_return_method_not_allowed() {
+        server.configure(routes -> routes.add(new IndexResource(new PropertiesProvider(new HashMap<String, String>() {{
+            put("elasticsearchAddress", "http://localhost:" + mockElastic.port());
+        }}), mockIndexer)).filter(new BasicAuthFilter("/", "icij", HashMapUser.singleUser("cecile"))));
+
+        put("/api/index/search/cecile-datashare/_search").withPreemptiveAuthentication("cecile", "pass").should().respond(405);
     }
 
     @Test
-    public void test_get_returns_backend_status_405() {
-        server.configure(routes -> routes.add(new IndexResource(new PropertiesProvider(new HashMap<String, String>() {{
-                                    put("elasticsearchAddress", "http://localhost:" + mockElastic.port());
-                                }}), mockIndexer)).filter(new BasicAuthFilter("/", "icij", HashMapUser.singleUser("cecile"))));
-
-        put("/api/index/search/unknown").withPreemptiveAuthentication("cecile", "pass").should().respond(405);
+    public void test_put_create_local_index_in_local_mode() throws Exception {
+        put("/api/index/create").should().respond(200);
+        verify(mockIndexer).createIndex("local-datashare");
     }
 
     @Test
