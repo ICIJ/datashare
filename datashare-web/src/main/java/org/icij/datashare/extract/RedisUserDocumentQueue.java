99ecc6151a0006d111979bb5669549b51c0e8b04
Bruno Thomas <bruno@barreverte.fr>
2019-03-16 17:45:02 +0000

    adds a filter queue task that should be run alone
    it removes duplicates in the queue
    and removes already extracted documents

--- datashare-web/src/main/java/org/icij/datashare/extract/RedisUserDocumentQueue.java
+++ datashare-web/src/main/java/org/icij/datashare/extract/RedisUserDocumentQueue.java
@@ -2,6 +2,7 @@ package org.icij.datashare.extract;
 
 import org.icij.datashare.user.User;
 import org.icij.extract.document.DocumentFactory;
+import org.icij.extract.queue.DocumentQueue;
 import org.icij.extract.redis.RedisDocumentQueue;
 import org.icij.task.Option;
 import org.icij.task.Options;
@@ -12,16 +13,26 @@ import org.slf4j.LoggerFactory;
 
 public class RedisUserDocumentQueue extends RedisDocumentQueue {
     private static final String QUEUE_NAME = "queueName";
+    private final Options<String> options;
+    private final User user;
 
     public RedisUserDocumentQueue(@NotNull final User user, final Options<String> options) {
-        super(new DocumentFactory(options), createOrUpdateQueueNameInOptions(options, user));
+        super(new DocumentFactory(options), createOrUpdateQueueNameInOptions(options, user, ""));
+        this.options = options;
+        this.user = user;
     }
 
-    private static Options<String> createOrUpdateQueueNameInOptions(Options<String> userOptions, User user) {
+    @Override
+    public DocumentQueue newQueue() {
+        return new RedisUserDocumentQueue(user, createOrUpdateQueueNameInOptions(options, user, ":filtered"));
+    }
+
+    private static Options<String> createOrUpdateQueueNameInOptions(Options<String> userOptions, User user, String suffix) {
         if (userOptions.get(QUEUE_NAME) == null) {
             userOptions.add(new Option<>(QUEUE_NAME, StringOptionParser::new).update("extract:queue"));
         }
         String queueName = user.isNull() ? userOptions.valueIfPresent(QUEUE_NAME).orElse("extract:queue") : user.queueName();
+        queueName += suffix;
         LoggerFactory.getLogger(RedisDocumentQueue.class).info("using redis queue name {}", queueName);
         Options<String> options = userOptions.createFrom(new Options<String>()
                 .add(new Option<>(QUEUE_NAME, StringOptionParser::new).update(queueName)));
