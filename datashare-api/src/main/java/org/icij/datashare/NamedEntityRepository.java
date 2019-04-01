674bc7e59a7385ac1f9299d981e8c8d06f49eb58
Bruno Thomas <bruno@barreverte.fr>
2019-04-01 13:32:09 +0000

    updates NamedEntityRepository

--- datashare-api/src/main/java/org/icij/datashare/NamedEntityRepository.java
+++ datashare-api/src/main/java/org/icij/datashare/NamedEntityRepository.java
@@ -1,10 +1,13 @@
 package org.icij.datashare;
 
+import org.icij.datashare.text.Document;
 import org.icij.datashare.text.NamedEntity;
+import java.util.List;
 
 interface NamedEntityRepository {
    NamedEntity get(String id);
-   void create(NamedEntity ne);
+   void create(List<NamedEntity> neList);
+   void create(Document document);
    void update(NamedEntity ne);
     NamedEntity delete(String id);
 }
