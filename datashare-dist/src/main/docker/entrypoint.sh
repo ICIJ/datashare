f7b247bf658a4bce6d26ce44d8115c84e1ce2d68
Bruno Thomas <bruno@barreverte.fr>
2019-04-26 13:56:12 +0000

    change JRE from oracle to OpenJdk

--- datashare-dist/src/main/docker/entrypoint.sh
+++ datashare-dist/src/main/docker/entrypoint.sh
@@ -7,7 +7,6 @@ if [ "$1" = 'sh' ];
 then
     exec "$@"
 else
-    source /etc/profile.d/java.sh
     CLASSPATH=$(find /home/datashare/lib/ -name '*.jar' | sort | xargs | sed 's/ /:/g')
     exec java ${DS_JAVA_OPTS} -DPROD_MODE=true -cp "/home/datashare/dist/:${CLASSPATH}" ${MAIN_CLASS} "$@"
 fi
