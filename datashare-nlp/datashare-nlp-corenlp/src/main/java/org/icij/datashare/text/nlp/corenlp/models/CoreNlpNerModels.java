32671186f7e5cbc11c94346f402015e80708a429
Bruno Thomas <bruno@barreverte.fr>
2019-05-22 15:41:23 +0000

    [fix] issues #146 and #89

--- datashare-nlp/datashare-nlp-corenlp/src/main/java/org/icij/datashare/text/nlp/corenlp/models/CoreNlpNerModels.java
+++ datashare-nlp/datashare-nlp-corenlp/src/main/java/org/icij/datashare/text/nlp/corenlp/models/CoreNlpNerModels.java
@@ -84,9 +84,8 @@ public class CoreNlpNerModels extends CoreNlpModels<AbstractSequenceClassifier<C
         modelNames.put(SPANISH, "ner/spanish.ancora.distsim.s512.crf.ser.gz");
         modelNames.put(FRENCH, "ner/english.all.3class.distsim.crf.ser.gz");
         modelNames.put(CHINESE, "ner/chinese.misc.distsim.crf.ser.gz");
-        modelNames.put(GERMAN, "ner/german.conll.hgc_175m_600.crf.ser.gz");
+        modelNames.put(GERMAN, "ner/german.conll.germeval2014.hgc_175m_600.crf.ser.gz");
     }
     @Override
     String getPropertyName() { return "ner.model";}
 }
-
