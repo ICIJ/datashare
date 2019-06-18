cf12e775d061836ee2286d7cb7c0138356b595fd
Pierre Romera <hello@pirhoo.com>
2019-06-18 12:02:20 +0200

    Added preview host to launchback

--- launchBack.sh
+++ launchBack.sh
@@ -3,6 +3,7 @@
 VERSION=$(cat pom.xml | grep '<version>[0-9.]\+' | sed 's/<version>\([0-9.]\+\)<\/version>/\1/g' | tr -d '[:space:]')
 CLASSPATH=$(find datashare-dist/target/datashare-dist-${VERSION}-all/lib/ -name '*.jar' | sort | xargs | sed 's/ /:/g')
 
+export DS_DOCKER_PREVIEW_HOST="http://localhost:5000"
 export DS_DOCKER_BACK_HOST="http://localhost:$(docker ps|grep 8080|sed 's/.*0.0.0.0:\(.*\)->8080.*/\1/g')"
 export DS_DOCKER_FRONT_HOST="http://localhost:$(docker ps|grep 9090|sed 's/.*0.0.0.0:\(.*\)->9090.*/\1/g')"
 
