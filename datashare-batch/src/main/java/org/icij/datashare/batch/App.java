e55aec2c6d993ba0130c622a1a9575a3db5c0a52
Bruno Thomas <bruno@barreverte.fr>
2019-07-12 10:52:45 +0000

    adds clean closing of ES connections see #193

--- datashare-batch/src/main/java/org/icij/datashare/batch/App.java
+++ datashare-batch/src/main/java/org/icij/datashare/batch/App.java
@@ -2,11 +2,13 @@ package org.icij.datashare.batch;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import org.icij.datashare.text.indexing.Indexer;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Injector injector = Guice.createInjector(new AppInjector());
 
 		injector.getInstance(BatchSearchRunner.class).call();
+        injector.getInstance(Indexer.class).close();
     }
 }
