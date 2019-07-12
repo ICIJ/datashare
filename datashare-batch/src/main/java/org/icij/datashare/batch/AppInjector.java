62947e7d03a609f41328f1f8140a5fab478f9dab
Bruno Thomas <bruno@barreverte.fr>
2019-07-12 12:50:56 +0000

    [cli] adds a cli for the batch #193

--- datashare-batch/src/main/java/org/icij/datashare/batch/AppInjector.java
+++ datashare-batch/src/main/java/org/icij/datashare/batch/AppInjector.java
@@ -7,12 +7,20 @@ import org.icij.datashare.db.RepositoryFactoryImpl;
 import org.icij.datashare.text.indexing.Indexer;
 import org.icij.datashare.text.indexing.elasticsearch.ElasticsearchIndexer;
 
+import java.util.Properties;
+
 import static org.icij.datashare.text.indexing.elasticsearch.ElasticsearchConfiguration.createESClient;
 
 public class AppInjector extends AbstractModule {
+    private final Properties properties;
+
+    AppInjector(Properties properties) {
+        this.properties = properties;
+    }
+
     @Override
     protected void configure() {
-        PropertiesProvider propertiesProvider = new PropertiesProvider();
+        PropertiesProvider propertiesProvider = new PropertiesProvider(properties);
         RestHighLevelClient esClient = createESClient(propertiesProvider);
         bind(RestHighLevelClient.class).toInstance(esClient);
         bind(Indexer.class).to(ElasticsearchIndexer.class).asEagerSingleton();
