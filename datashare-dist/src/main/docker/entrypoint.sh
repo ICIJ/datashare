4a820fefebf103e90c9d3914048ab426c0acb14b
Bruno Thomas <bthomas@icij.org>
2019-07-18 15:37:02 +0000

    [fix] loading of logback : related to jar order

--- datashare-dist/src/main/docker/entrypoint.sh
+++ datashare-dist/src/main/docker/entrypoint.sh
@@ -7,6 +7,6 @@ if [ "$1" = 'sh' ];
 then
     exec "$@"
 else
-    CLASSPATH=$(find /home/datashare/lib/ -name '*.jar' | sort | xargs | sed 's/ /:/g')
+    CLASSPATH=$(find /home/datashare/lib/ -name '*.jar' | sort -r | xargs | sed 's/ /:/g')
     exec java ${DS_JAVA_OPTS} -DPROD_MODE=true -cp "/home/datashare/dist/:${CLASSPATH}" ${MAIN_CLASS} "$@"
 fi
--- launchBack.sh
+++ launchBack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION=$(cat pom.xml | grep '<version>[0-9.]\+' | sed 's/<version>\([0-9.]\+\)<\/version>/\1/g' | tr -d '[:space:]')
-CLASSPATH=$(find datashare-dist/target/datashare-dist-${VERSION}-all/lib/ -name '*.jar' | sort | xargs | sed 's/ /:/g')
+CLASSPATH=$(find datashare-dist/target/datashare-dist-${VERSION}-all/lib/ -name '*.jar' | sort -r | xargs | sed 's/ /:/g')
 
 export DS_DOCKER_PREVIEW_HOST="http://localhost:5000"
 export DS_DOCKER_BACK_HOST="http://localhost:$(docker ps|grep 8080|sed 's/.*0.0.0.0:\(.*\)->8080.*/\1/g')"
