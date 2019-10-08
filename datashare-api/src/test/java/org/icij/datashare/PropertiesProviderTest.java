37923ee1178fda73ddf9827465d206bf7ff71e1c
Bruno Thomas <bthomas@icij.org>
2019-10-08 10:16:34 +0000

    [refactor] inline putEnv
    and remove dependency datashare-api -> datashare-common-test

--- datashare-api/src/test/java/org/icij/datashare/PropertiesProviderTest.java
+++ datashare-api/src/test/java/org/icij/datashare/PropertiesProviderTest.java
@@ -2,14 +2,16 @@ package org.icij.datashare;
 
 import org.junit.Test;
 
+import java.lang.reflect.Field;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
 import static org.fest.assertions.Assertions.assertThat;
 import static org.fest.assertions.MapAssert.entry;
-import static org.icij.datashare.test.TestUtils.putEnv;
 
 public class PropertiesProviderTest {
     @Test
@@ -89,5 +91,35 @@ public class PropertiesProviderTest {
     public void test_adds_no_ds_env_variables() throws Exception {
         putEnv("MY_VARIABLE", "value");
         assertThat(new PropertiesProvider().getProperties().entrySet().size()).isEqualTo(2);
+    }
+
+    /**
+     * see https://stackoverflow.com/questions/318239/how-do-i-set-environment-variables-from-java
+     */
+    private static void putEnv(String name, String value) throws Exception {
+      try {
+        Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+        Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+        theEnvironmentField.setAccessible(true);
+        Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+        env.put(name, value);
+        Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+        theCaseInsensitiveEnvironmentField.setAccessible(true);
+        Map<String, String> cienv = (Map<String, String>)theCaseInsensitiveEnvironmentField.get(null);
+        cienv.put(name, value);
+      } catch (NoSuchFieldException e) {
+        Class[] classes = Collections.class.getDeclaredClasses();
+        Map<String, String> env = System.getenv();
+        for(Class cl : classes) {
+          if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+            Field field = cl.getDeclaredField("m");
+            field.setAccessible(true);
+            Object obj = field.get(env);
+            Map<String, String> map = (Map<String, String>) obj;
+            map.clear();
+            map.put(name, value);
+          }
+        }
+      }
     }
 }
