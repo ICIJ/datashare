f39319134b5f257895ef0fe9d7c43c11e627ca87
Bruno Thomas <bthomas@icij.org>
2019-10-11 17:37:38 +0000

    [doc] BatchSearchResource

--- datashare-app/src/main/java/org/icij/datashare/web/BatchSearchResource.java
+++ datashare-app/src/main/java/org/icij/datashare/web/BatchSearchResource.java
@@ -39,36 +39,117 @@ public class BatchSearchResource {
         this.propertiesProvider = propertiesProvider;
     }
 
+    /**
+     * Retrieve the batch search list for the user issuing the request.
+     *
+     * @return 200 and the list of batch searches
+     *
+     * Example : $(curl localhost:8080/api/batch/search | jq)
+     */
     @Get("/search")
     public List<BatchSearch> getSearches(Context context) {
         return batchSearchRepository.get((User) context.currentUser());
     }
 
-    @Options("/search")
-    public Payload optionsSearches(Context context) {
-        return ok().withAllowMethods("OPTIONS", "DELETE");
-    }
-
-    @Delete("/search")
-    public Payload deleteSearches(Context context) {
-        return batchSearchRepository.deleteAll((User) context.currentUser()) ? new Payload(204): notFound();
-    }
-
+    /**
+     * Retrieve the batch search with the given id
+     *
+     * @param batchId
+     * @return 200 and the batch search
+     *
+     * Example : $(curl localhost:8080/api/batch/search/314 | jq)
+     */
     @Get("/search/:batchid")
     public BatchSearch getBatch(String batchId, Context context) {
         return batchSearchRepository.get((User) context.currentUser(), batchId);
     }
 
+    /**
+     * preflight request
+     *
+     * @return 200 DELETE
+     */
+    @Options("/search")
+    public Payload optionsSearches(Context context) {
+        return ok().withAllowMethods("OPTIONS", "DELETE");
+    }
+
+    /**
+     * Delete batch searches and results for the current user.
+     *
+     * Returns 204 (No Content) if rows have been removed and 404 if nothing has been done (i.e. not found).
+     *
+     * @return 204 or 404
+     */
+    @Delete("/search")
+    public Payload deleteSearches(Context context) {
+        return batchSearchRepository.deleteAll((User) context.currentUser()) ? new Payload(204): notFound();
+    }
+
+    /**
+     * preflight resquest for removal of one batchsearch
+     *
+     * @param batchId
+     * @return 200 DELETE
+     */
     @Options("/search/:batchid")
     public Payload optionsDelete(String batchId, Context context) {
         return ok().withAllowMethods("OPTIONS", "DELETE");
     }
 
+    /**
+     * Delete batch search with the given id and its results.
+     *
+     * Returns 204 (No Content) if rows have been removed and 404 if nothing has been done (i.e. not found).
+     *
+     * @return 204 or 404
+     */
     @Delete("/search/:batchid")
     public Payload deleteBatch(String batchId, Context context) {
         return batchSearchRepository.delete((User) context.currentUser(), batchId) ? new Payload(204): notFound();
     }
 
+    /**
+     * Creates a new batch search. This is a multipart form with 8 fields :
+     * name, description, csvFile, published, fileTypes, paths, fuzziness, phrase_matches
+     *
+     * No matter the order. The name and csv file are mandatory else it will return 400 (bad request)
+     *
+     * To do so with bash you can create a text file like :
+     * ```
+     * --BOUNDARY
+     * Content-Disposition: form-data; name="name"
+     *
+     * my batch search
+     * --BOUNDARY
+     * Content-Disposition: form-data; name="description"
+     *
+     * search description
+     * --BOUNDARY
+     * Content-Disposition: form-data; name="csvFile"; filename="search.csv"
+     * Content-Type: text/csv
+     *
+     * Obama
+     * skype
+     * test
+     * query three
+     * --BOUNDARY--
+     * Content-Disposition: form-data; name="published"
+     *
+     * true
+     * --BOUNDARY--
+     * ```
+     * Then replace `\n` with `\r\n` with a sed like this:
+     *
+     * `sed -i 's/$/^M/g' ~/multipart.txt`
+     *
+     * TThen make a curl request with this file :
+     * curl -i -XPOST localhost:8080/api/batch/search/prj -H 'Content-Type: multipart/form-data; boundary=BOUNDARY' --data-binary @/home/dev/multipart.txt
+     *
+     * @param projectId
+     * @param context : the request body
+     * @return 200 or 400
+     */
     @Post("/search/:project")
     public Payload search(String projectId, Context context) throws Exception {
         List<Part> parts = context.parts();
@@ -93,11 +174,39 @@ public class BatchSearchResource {
                 new Payload("application/json", batchSearch.uuid, 200) : badRequest();
     }
 
+    /**
+     * Retrieve the results of a batch search as JSON.
+     *
+     * It needs a Query json body with the parameters :
+     *
+     * - from : index offset of the first document to return (mandatory)
+     * - size : window size of the results (mandatory)
+     * - queries: list of queries to be downloaded (default null)
+     * - sort: field to sort ("doc_nb", "doc_id", "root_id", "doc_name", "creation_date", "content_type", "content_length", "creation_date") (default "doc_nb")
+     * - order: "asc" or "desc" (default "asc")
+     *
+     * If from/size are not given their default values are 0, meaning that all the results are returned.
+     * @param batchId
+     * @param webQuery
+     * @return 200
+     *
+     * Example : $(curl -XPOST localhost:8080/api/batch/search/result/3f82a76f-29ae-4c93-80af-d510d515ae9e -d "{\"from\":0, \"size\": 2}"| jq)
+     */
     @Post("/search/result/:batchid")
     public List<SearchResult> getResult(String batchId, BatchSearchRepository.WebQuery webQuery, Context context) {
         return getResultsOrThrowUnauthorized(batchId, (User) context.currentUser(), webQuery);
     }
 
+    /**
+     * Retrieve the results of a batch search as a CSV file.
+     *
+     * The search request is by default all results of the batch search.
+     *
+     * @param batchId
+     * @return 200 and the CSV file as attached file
+     *
+     * Example : $(curl -i localhost:8080/api/batch/search/result/csv/3f82a76f-29ae-4c93-80af-d510d515ae9e)
+     */
     @Get("/search/result/csv/:batchid")
     public Payload getResultAsCsv(String batchId, Context context) {
         StringBuilder builder = new StringBuilder("\"query\", \"documentUrl\", \"documentId\",\"rootId\",\"contentType\",\"contentLength\",\"documentPath\",\"creationDate\",\"documentNumber\"\n");
