048a7013dd4dafe2b64ea38e6082dd0240bf9353
Bruno Thomas <bthomas@icij.org>
2019-10-22 12:12:36 +0000

    [doc] fix project parameter in apigen script

--- doc/apigen/datashare_for_apigen.sh
+++ doc/apigen/datashare_for_apigen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./launchBack.sh -n apigen-datashare -m CLI -s SCAN,INDEX,NLP --nlpp CORENLP -d doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:/home/dev/src/datashare/doc/apigen/apigen.db
+./launchBack.sh -p apigen-datashare -m CLI -s SCAN,INDEX,NLP --nlpp CORENLP -d doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:/home/dev/src/datashare/doc/apigen/apigen.db
 ./launchBack.sh -u apigen -d doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:/home/dev/src/datashare/doc/apigen/apigen.db
