cb11a67cf89acda97dc5e5c1b6fc7199973bd23b
Bruno Thomas <bthomas@icij.org>
2019-11-04 13:30:47 +0000

    [security][fix] jar not found (see CVE-2019-16942)

--- datashare-api/src/main/java/org/icij/datashare/json/JsonObjectMapper.java
+++ datashare-api/src/main/java/org/icij/datashare/json/JsonObjectMapper.java
@@ -54,18 +54,14 @@ public class JsonObjectMapper {
      * @return JSON representation of {@code obj}
      */
     public static <T extends Entity> Map<String, Object> getJson(T obj) {
-        // Convert Object to JSON string
-        String json = null;
+        String json;
         try {
             json = MAPPER.writeValueAsString(obj);
         } catch (JsonProcessingException e) {
             return null;
         }
-        // Map TypeReference
-        TypeReference typeRef = new TypeReference<Map<String, Object>>(){};
-        // convert JSON string to Map
         try {
-            return MAPPER.readValue(json, typeRef);
+            return MAPPER.readValue(json, new TypeReference<HashMap<String, Object>>(){});
         } catch (IOException e) {
             return null;
         }
