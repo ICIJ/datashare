c68524194c8f5277ebd34bce902abe1f56e40e09
Bruno Thomas <bthomas@icij.org>
2019-11-27 09:04:04 +0000

    [#310] adds doc for PATCH

--- datashare-app/src/main/java/org/icij/datashare/web/ConfigResource.java
+++ datashare-app/src/main/java/org/icij/datashare/web/ConfigResource.java
@@ -63,7 +63,15 @@ public class ConfigResource {
     /**
      * update the datashare configuration with provided body. It will save the configuration on disk.
      *
-     * Returns 404 if configuration is not found. It means that the configuration file has not been set.
+     * Returns 404 if configuration is not found. It means that the configuration file has not been set (or is not readable)
+     *
+     * The configuration priority is basically -c file > classpath:datashare.properties > command line. I.e. :
+     *
+     * - if a file is given (w/ -c path/to/file) to the command line it will be read and used (it can be empty or not present)
+     * - if no file is given, we are looking for datashare.properties in the classpath (for example in /dist)
+     * - if none of the two above cases is fulfilled we are taking the default CLI parameters (and those given by the user)
+     * - if there are common parameters in CLI and a config file, the config file "wins"
+     * - if a config file is not writable then 404 will be returned (and a WARN will be logged at start)
      *
      * @return 200 or 404
      *
