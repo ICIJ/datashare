d67e9497f1648e46927365ade042946f4150e9fe
Bruno Thomas <bthomas@icij.org>
2019-12-06 10:08:14 +0000

    [doc] update config documentation

--- datashare-app/src/main/java/org/icij/datashare/web/ConfigResource.java
+++ datashare-app/src/main/java/org/icij/datashare/web/ConfigResource.java
@@ -65,8 +65,10 @@ public class ConfigResource {
      *
      * Returns 404 if configuration is not found. It means that the configuration file has not been set (or is not readable)
      *
-     * The configuration priority is basically -c file > classpath:datashare.properties > command line. I.e. :
+     * The configuration priority is basically
+     * DS_DOCKER_* variables > -c file > classpath:datashare.properties > command line. I.e. :
      *
+     * - DS_DOCKER_* variables will be taken and override all keys (if any similar keys exist)
      * - if a file is given (w/ -c path/to/file) to the command line it will be read and used (it can be empty or not present)
      * - if no file is given, we are looking for datashare.properties in the classpath (for example in /dist)
      * - if none of the two above cases is fulfilled we are taking the default CLI parameters (and those given by the user)
