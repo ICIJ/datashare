f770abc08ee4119545b628c55c2f0cd9622f40de
Bruno Thomas <bthomas@icij.org>
2020-01-24 16:14:22 +0000

    uses memory bus/queue for apigen

--- doc/apigen/datashare_for_apigen.sh
+++ doc/apigen/datashare_for_apigen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./launchBack.sh -p apigen-datashare -m CLI -s SCAN,INDEX,NLP --nlpp CORENLP -d doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:/home/dev/src/datashare/doc/apigen/apigen.db
-./launchBack.sh -u apigen -d doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:/home/dev/src/datashare/doc/apigen/apigen.db
+./launchBack.sh -p apigen-datashare -m CLI -s SCAN,INDEX,NLP --nlpp CORENLP -d doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:/home/dev/src/datashare/doc/apigen/apigen.db --queueType memory --busType memory
+./launchBack.sh -u apigen -d doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:/home/dev/src/datashare/doc/apigen/apigen.db --queueType memory --busType memory
