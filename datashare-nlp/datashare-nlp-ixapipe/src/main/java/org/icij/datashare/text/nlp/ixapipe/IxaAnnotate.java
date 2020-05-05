263761524d93d26e69ebbf1c41aba549b0e36e0e
Bruno Thomas <bthomas@icij.org>
2020-05-05 09:07:01 +0000

    [#438] removes the sources for NLP libs

--- datashare-nlp/datashare-nlp-ixapipe/src/main/java/org/icij/datashare/text/nlp/ixapipe/IxaAnnotate.java
+++
@@ -1,9 +0,0 @@
-package org.icij.datashare.text.nlp.ixapipe;
-
-public class IxaAnnotate<T> {
-    public final T annotate;
-
-    IxaAnnotate(T annotate) {
-        this.annotate = annotate;
-    }
-}
