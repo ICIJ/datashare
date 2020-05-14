8a139799878328a271c5fff2c5a626cf7585b8ff
Bruno Thomas <bthomas@icij.org>
2020-05-14 15:09:24 +0000

    [#439] test more http methods

--- datashare-app/src/test/java/org/icij/datashare/mode/CommonModeTest.java
+++ datashare-app/src/test/java/org/icij/datashare/mode/CommonModeTest.java
@@ -1,37 +1,60 @@
 package org.icij.datashare.mode;
 
+import net.codestory.rest.FluentRestTest;
+import net.codestory.rest.RestAssert;
 import org.icij.datashare.web.testhelpers.AbstractProdWebServerTest;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.HashMap;
 
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
+import static java.util.Optional.ofNullable;
 import static org.icij.datashare.test.JarUtil.createJar;
 
+@RunWith(Parameterized.class)
 public class CommonModeTest extends AbstractProdWebServerTest {
+    private final String method;
     @Rule public TemporaryFolder pluginFolder = new TemporaryFolder();
     CommonMode mode;
+    @Parameterized.Parameters
+    public static Collection<Object[]> methods() {
+        return asList(new Object[][]{{"Get"}, {"Post"}, {"Patch"}, {"Put"}, {"Delete"}, {"Options"}});
+    }
 
     @Test
-    public void test_one_extension_with_prefix_and_get() throws Exception {
-        String source = "package org.icij.datashare.mode;\n" +
+    public void test_one_extension() throws Throwable {
+        test_one_extension(method, "/foo").should().respond(200).contain("hello from foo extension");
+    }
+
+    RestAssert test_one_extension(String method, String prefix) throws Throwable {
+        String prefixString = prefix == null ? "\n":"@Prefix(\""+ prefix + "\")\n";
+        String source = format("package org.icij.datashare.mode;\n" +
                 "\n" +
-                "import net.codestory.http.annotations.Get;\n" +
+                "import net.codestory.http.annotations.%s;\n" +
                 "import net.codestory.http.annotations.Prefix;\n" +
-                "\n" +
-                "@Prefix(\"foo\")\n" +
+                "\n" + prefixString +
                 "public class FooResource {\n" +
-                "    @Get(\"url\")\n" +
+                "    @%s(\"url\")\n" +
                 "    public String url() {\n" +
-                "        return \"hello from foo extension\";\n" +
+                "        return \"hello from foo extension with %s\";\n" +
                 "    }\n" +
-                "}";
+                "}", method, method, method);
         createJar(pluginFolder.getRoot().toPath(), "extension", source);
         configure(routes -> mode.addExtensionConfiguration(routes));
-        get("/foo/url").should().respond(200).contain("hello from foo extension");
+
+        Method restAssertMethod = FluentRestTest.class.getMethod(method.toLowerCase(), String.class);
+        return (RestAssert) restAssertMethod.invoke(this, ofNullable(prefix).orElse("") + "/url");
     }
+
+    public CommonModeTest(String method) { this.method = method;}
 
     @Before
     public void setUp() {
