193394b680a288b19ba0bbc03418bea3bcc3b4cf
Bruno Thomas <bthomas@icij.org>
2020-06-02 14:27:41 +0000

    S3 adds updload with authentication

--- datashare-app/src/main/java/org/icij/datashare/cli/RemoteFilesCli.java
+++ datashare-app/src/main/java/org/icij/datashare/cli/RemoteFilesCli.java
@@ -10,10 +10,10 @@ public class RemoteFilesCli {
     private static RemoteFiles remoteFiles = null;
 
     public static void main(String[] args) throws Exception {
-        if (RemoteFilesCli.remoteFiles == null) {
-            RemoteFilesCli.remoteFiles = RemoteFiles.getDefault();
-        }
         OptionSet cmd = parseArgs(args);
+        if (RemoteFilesCli.remoteFiles == null) {
+            RemoteFilesCli.remoteFiles = cmd.has("a") ? RemoteFiles.getAuthenticated(): RemoteFiles.getDefault();
+        }
         String remoteKey = cmd.has("D") ? (String)cmd.valueOf("D") : "/";
         if (cmd.has("u")) {
             for (Object arg : cmd.valuesOf("f")) {
@@ -29,7 +29,7 @@ public class RemoteFilesCli {
     }
 
     private static OptionSet parseArgs(final String[] args) {
-        OptionParser parser = new OptionParser("udD:f:");
+        OptionParser parser = new OptionParser("udaD:f:");
         OptionSet optionSet = null;
         try {
             optionSet = parser.parse(args);
@@ -44,6 +44,7 @@ public class RemoteFilesCli {
         System.out.println("usage : remote-files [-u|-d] [-D remoteDirectory] -f file1 -f file2...");
         System.out.println("-u: to upload content to remote destination");
         System.out.println("-d: to download content from remote destination");
+        System.out.println("-a: to use authentication (see DefaultAWSCredentialsProviderChain)");
         System.out.println("-D: remote directory (default : /)");
         System.out.println("-f: directories or files. For directory, the content will be uploaded recursively");
         System.out.println("example : copy-remote -u -D foo -f bar.txt -f qux/ ");
