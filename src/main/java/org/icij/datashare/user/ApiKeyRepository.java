dec503f7f9d97e8353d3545862a3783ab9a318fb
Maxime Vanza <maximevanza@live.fr>
2020-07-07 16:22:11 +0000

    [release] 7.7.0

--- src/main/java/org/icij/datashare/user/ApiKeyRepository.java
+++ src/main/java/org/icij/datashare/user/ApiKeyRepository.java
@@ -3,5 +3,8 @@ package org.icij.datashare.user;
 public interface ApiKeyRepository {
     ApiKey get(String base64Key);
     ApiKey get(User user);
+    
+    boolean delete(User user);
+
     boolean save(ApiKey apiKey);
 }
