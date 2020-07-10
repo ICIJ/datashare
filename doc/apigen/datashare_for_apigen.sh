6cb1d23436b7b94a708c8bc4d47b8769b7fe9572
Bruno Thomas <bthomas@icij.org>
2020-07-10 15:35:22 +0000

    [CI][#509] removes hard coded folders for apigen scripts

--- doc/apigen/datashare_for_apigen.sh
+++ doc/apigen/datashare_for_apigen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./launchBack.sh -p apigen-datashare -u apigen -d /home/dev/src/datashare/doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:/home/dev/src/datashare/doc/apigen/apigen.db --queueType memory --busType memory
+./launchBack.sh -p apigen-datashare -u apigen -d $PWD/doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:$PWD/doc/apigen/apigen.db --queueType memory --busType memory
