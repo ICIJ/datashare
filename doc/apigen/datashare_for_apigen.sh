4b8454831ae8f0654156c6190377ed6935a7b30f
Bruno Thomas <bthomas@icij.org>
2020-07-24 13:37:29 +0000

    [doc] adds a plugins folder

--- doc/apigen/datashare_for_apigen.sh
+++ doc/apigen/datashare_for_apigen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./launchBack.sh -p apigen-datashare -u apigen -d $PWD/doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:$PWD/doc/apigen/apigen.db --queueType memory --busType memory
+mkdir /tmp/plugins
+./launchBack.sh -p apigen-datashare -u apigen -d $PWD/doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:$PWD/doc/apigen/apigen.db --queueType memory --busType memory --pluginsDir /tmp/plugins
