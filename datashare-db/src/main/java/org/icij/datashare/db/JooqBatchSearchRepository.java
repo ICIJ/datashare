4f9fe74bfb0b1208d722022425a5d60092b84438
Bruno Thomas <bthomas@icij.org>
2020-08-04 16:12:04 +0000

    [#559] refactor duplicate code

--- datashare-db/src/main/java/org/icij/datashare/db/JooqBatchSearchRepository.java
+++ datashare-db/src/main/java/org/icij/datashare/db/JooqBatchSearchRepository.java
@@ -133,11 +133,7 @@ public class JooqBatchSearchRepository implements BatchSearchRepository {
 
     @Override
     public List<BatchSearchRecord> getRecords(User user, List<String> projectsIds) {
-        return createBatchSearchRecordWithQueriesSelectStatement(DSL.using(dataSource,dialect))
-                .where(BATCH_SEARCH.PRJ_ID.in(projectsIds).and(BATCH_SEARCH.USER_ID.eq(user.id).
-                        or(BATCH_SEARCH.PUBLISHED.greaterThan(0))))
-                .orderBy(BATCH_SEARCH.BATCH_DATE.desc()).
-                fetch().stream().map(this::createBatchSearchRecordFrom).collect(toList());
+        return getRecords(user, projectsIds, new WebQuery());
     }
 
     @Override
