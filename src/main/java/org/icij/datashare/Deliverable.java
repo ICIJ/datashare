4158ddaf0d7f550b65a5fb89711eeff087efa5a5
Bruno Thomas <bthomas@icij.org>
2020-08-05 15:30:32 +0000

    [refactor] adds a Deliverable interface

---
+++ src/main/java/org/icij/datashare/Deliverable.java
@@ -0,0 +1,13 @@
+package org.icij.datashare;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+
+public interface Deliverable extends Entity {
+    URL getUrl();
+    File download() throws IOException;
+    void install(File deliverable, Path targetDir) throws IOException;
+    void delete() throws IOException;
+}
