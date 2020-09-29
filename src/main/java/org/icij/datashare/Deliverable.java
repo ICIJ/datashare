87d1e5a005b352265712e7f57973c5ceda9f8285
Maxime Vanza <maximevanza@live.fr>
2020-09-29 16:56:29 +0000

    [ICIJ/datashare#637] add property getters and remove isInstalled()
    in Deliverable class

--- src/main/java/org/icij/datashare/Deliverable.java
+++ src/main/java/org/icij/datashare/Deliverable.java
@@ -6,13 +6,15 @@ import java.net.URL;
 import java.nio.file.Path;
 
 public interface Deliverable extends Entity {
+    enum Type {NLP, WEB, PLUGIN, UNKNOWN}
     URL getUrl();
     Path getBasePath();
     String getName();
     String getDescription();
+    String getVersion();
+    Type getType();
     File download() throws IOException;
     void install(File deliverable, Path targetDir) throws IOException;
     void install(Path targetDir) throws IOException; // install from local : url is the filesystem file
-    boolean isInstalled(Path targetDir) throws IOException;
     void delete(Path installDir) throws IOException;
 }
