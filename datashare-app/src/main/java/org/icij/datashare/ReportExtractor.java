2e4752c51e411cdef827a9bff67f40bfc8455ccc
Bruno Thomas <bthomas@icij.org>
2020-11-18 20:59:21 +0000

    adds a report extractor

---
+++ datashare-app/src/main/java/org/icij/datashare/ReportExtractor.java
@@ -0,0 +1,31 @@
+package org.icij.datashare;
+
+import org.icij.datashare.extract.RedisUserReportMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+
+public class ReportExtractor {
+    static Logger logger = LoggerFactory.getLogger(ReportExtractor.class.getName());
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.out.println("usage: report redis-url report-name");
+            System.exit(1);
+        }
+
+        try (RedisUserReportMap reportMap = new RedisUserReportMap(new PropertiesProvider(new HashMap<String, String>() {{ put("redisAddress", args[0]);}}), args[1])) {
+            reportMap.forEach((path, report) -> {
+                if (report.getException().isPresent()) {
+                    StringWriter sw = new StringWriter();
+                    PrintWriter pw = new PrintWriter(sw);
+                    report.getException().get().printStackTrace(pw);
+                    logger.info("\"{}\" \"{}\"", path, sw.toString().replace("\n", "|"));
+                }
+            });
+        }
+    }
+}
