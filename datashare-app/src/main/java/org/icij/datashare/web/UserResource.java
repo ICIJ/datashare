adb2a54b155eb741d765882a7009cdc02c96e0ce
Maxime Vanza <maximevanza@live.fr>
2021-06-15 10:15:44 +0000

    [#740] start user history api

--- datashare-app/src/main/java/org/icij/datashare/web/UserResource.java
+++ datashare-app/src/main/java/org/icij/datashare/web/UserResource.java
@@ -1,16 +1,27 @@
 package org.icij.datashare.web;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import net.codestory.http.Context;
 import net.codestory.http.annotations.Get;
 import net.codestory.http.annotations.Prefix;
+import org.icij.datashare.Repository;
+import org.icij.datashare.UserEvent;
 import org.icij.datashare.session.DatashareUser;
 
+import java.util.List;
 import java.util.Map;
 
 @Singleton
 @Prefix("/api/users")
 public class UserResource {
+    private final Repository repository;
+
+    @Inject
+    public UserResource(Repository repository) {
+        this.repository = repository;
+    }
+
     /**
      * gets the user's session information
      *
@@ -22,5 +33,18 @@ public class UserResource {
     @Get("/me")
     public Map<String, Object> getUser(Context context) {
         return ((DatashareUser) context.currentUser()).getDetails();
+    }
+
+    /**
+     * gets the user's history
+     *
+     * @return 200 and the user's list of events
+     *
+     * Example :
+     * $(curl -i localhost:8080/api/users/me/history)
+     */
+    @Get("/me/history")
+    public List<UserEvent> getUserHistory(Context context) {
+        return repository.getUserEvents((DatashareUser) context.currentUser());
     }
 }
--- datashare-app/src/test/java/org/icij/datashare/web/UserResourceTest.java
+++ datashare-app/src/test/java/org/icij/datashare/web/UserResourceTest.java
@@ -1,19 +1,47 @@
 package org.icij.datashare.web;
 
 import net.codestory.http.filters.basic.BasicAuthFilter;
+import org.icij.datashare.PropertiesProvider;
+import org.icij.datashare.Repository;
+import org.icij.datashare.UserEvent;
+import org.icij.datashare.session.LocalUserFilter;
+import org.icij.datashare.user.User;
 import org.icij.datashare.web.testhelpers.AbstractProdWebServerTest;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
+import java.nio.file.Paths;
+
+import static java.util.Collections.singletonList;
+import static org.icij.datashare.UserEvent.Type.DOCUMENT;
 import static org.icij.datashare.session.DatashareUser.singleUser;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class UserResourceTest extends AbstractProdWebServerTest {
+    @Mock Repository repository;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        configure(routes -> routes.add(new UserResource(repository)).filter(new LocalUserFilter(new PropertiesProvider())));
+    }
+
     @Test
     public void get_user_information_test() {
-        configure(routes -> routes.add(new UserResource()).
+        configure(routes -> routes.add(new UserResource(repository)).
                         filter(new BasicAuthFilter("/", "icij", singleUser("pierre"))));
 
         get("/api/users/me").withPreemptiveAuthentication("pierre", "pass").
                 should().respond(200).contain("\"uid\":\"pierre\"");
     }
 
+    @Test
+    public void test_get_user_history() {
+        UserEvent userEvent = new UserEvent(User.local(), DOCUMENT, "doc_name", Paths.get("doc_uri").toUri());
+        when(repository.getUserEvents(User.local())).thenReturn(singletonList(userEvent));
+
+        get("/api/users/me/history").should().contain(userEvent.uri.toString()).contain(User.local().id).respond(200);
+    }
 }
