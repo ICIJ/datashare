dbba488c4d61a42daac9f78b6de967a45bec0c9d
Bruno Thomas <bthomas@icij.org>
2021-07-05 15:01:29 +0000

    [ICIJ/datashare#809] harmonize parameters for tasks (remove user from BatchDownload)

--- src/test/java/org/icij/datashare/batch/BatchDownloadTest.java
+++ src/test/java/org/icij/datashare/batch/BatchDownloadTest.java
@@ -5,26 +5,20 @@ import org.junit.Test;
 import java.nio.file.Paths;
 
 import static org.icij.datashare.text.Project.project;
-import static org.icij.datashare.user.User.local;
 
 public class BatchDownloadTest {
     @Test(expected = IllegalArgumentException.class)
     public void test_constructor_null_project() {
-        new BatchDownload(null, Paths.get("test"), local(), "query");
+        new BatchDownload(null, Paths.get("test"), "query");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void test_constructor_null_path() {
-        new BatchDownload(project("prj"), null, local(), "query");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void test_constructor_null_user() {
-        new BatchDownload(project("prj"), Paths.get("test"), null, "query");
+        new BatchDownload(project("prj"), null, "query");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void test_constructor_null_query() {
-        new BatchDownload(project("prj"), Paths.get("test"), local(), null);
+        new BatchDownload(project("prj"), Paths.get("test"), null);
     }
 }
