c537915c529727e199146fac3aa5d339261b1d3a
Bruno Thomas <bthomas@icij.org>
2021-07-06 17:09:18 +0000

    Revert "[ICIJ/datashare#809] harmonize parameters for tasks (remove user from BatchDownload)"
    This reverts commit dbba488c4d61a42daac9f78b6de967a45bec0c9d.

--- src/main/java/org/icij/datashare/batch/BatchDownload.java
+++ src/main/java/org/icij/datashare/batch/BatchDownload.java
@@ -1,6 +1,7 @@
 package org.icij.datashare.batch;
 
 import org.icij.datashare.text.Project;
+import org.icij.datashare.user.User;
 
 import java.nio.file.Path;
 import java.util.Objects;
@@ -10,10 +11,12 @@ import static java.util.Optional.ofNullable;
 public class BatchDownload {
     public final Project project;
     public final Path filename;
+    public final User user;
     public final String queryString;
 
-    public BatchDownload(final Project project, final Path filename, String queryString) {
+    public BatchDownload(final Project project, final Path filename, User user, String queryString) {
         this.project = ofNullable(project).orElseThrow(() -> new IllegalArgumentException("project cannot be null or empty"));
+        this.user = ofNullable(user).orElseThrow(() -> new IllegalArgumentException("user cannot be null or empty"));
         this.queryString = ofNullable(queryString).orElseThrow(() -> new IllegalArgumentException("query cannot be null or empty"));
         this.filename = ofNullable(filename).orElseThrow(() -> new IllegalArgumentException("filename cannot be null or empty"));
     }
@@ -23,11 +26,11 @@ public class BatchDownload {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         BatchDownload that = (BatchDownload) o;
-        return Objects.equals(project, that.project) && Objects.equals(filename, that.filename) && Objects.equals(queryString, that.queryString);
+        return Objects.equals(project, that.project) && Objects.equals(filename, that.filename) && Objects.equals(user, that.user) && Objects.equals(queryString, that.queryString);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(project, filename, queryString);
+        return Objects.hash(project, filename, user, queryString);
     }
 }
