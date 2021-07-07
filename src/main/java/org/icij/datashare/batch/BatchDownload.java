44d2b98a7c38594671c055783c850fc005889e95
Bruno Thomas <bthomas@icij.org>
2021-07-07 17:46:58 +0000

    [ICIJ/datashare#809] make BatchDownload build the filename

--- src/main/java/org/icij/datashare/batch/BatchDownload.java
+++ src/main/java/org/icij/datashare/batch/BatchDownload.java
@@ -1,24 +1,39 @@
 package org.icij.datashare.batch;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.icij.datashare.text.PathDeserializer;
+import org.icij.datashare.text.PathSerializer;
 import org.icij.datashare.text.Project;
+import org.icij.datashare.time.DatashareTime;
 import org.icij.datashare.user.User;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.ZoneId;
 import java.util.Objects;
 
+import static java.lang.String.format;
+import static java.time.ZonedDateTime.from;
+import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
 import static java.util.Optional.ofNullable;
 
 public class BatchDownload {
     public final Project project;
+    @JsonSerialize(using = PathSerializer.class)
+    @JsonDeserialize(using = PathDeserializer.class)
     public final Path filename;
-    public final User user;
-    public final String queryString;
+    public final String query;
 
-    public BatchDownload(final Project project, final Path filename, User user, String queryString) {
+    public BatchDownload(final Project project, User user, String query) {
+        this(project, user, query, Paths.get(System.getProperty("java.io.tmpdir")));
+    }
+
+    public BatchDownload(final Project project, User user, String query, Path downloadDir) {
         this.project = ofNullable(project).orElseThrow(() -> new IllegalArgumentException("project cannot be null or empty"));
-        this.user = ofNullable(user).orElseThrow(() -> new IllegalArgumentException("user cannot be null or empty"));
-        this.queryString = ofNullable(queryString).orElseThrow(() -> new IllegalArgumentException("query cannot be null or empty"));
-        this.filename = ofNullable(filename).orElseThrow(() -> new IllegalArgumentException("filename cannot be null or empty"));
+        this.query = ofNullable(query).orElseThrow(() -> new IllegalArgumentException("query cannot be null or empty"));
+        User nonNullUser = ofNullable(user).orElseThrow(() -> new IllegalArgumentException("user cannot be null or empty"));
+        this.filename = downloadDir.resolve(createFilename(project, nonNullUser));
     }
 
     @Override
@@ -26,11 +41,16 @@ public class BatchDownload {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         BatchDownload that = (BatchDownload) o;
-        return Objects.equals(project, that.project) && Objects.equals(filename, that.filename) && Objects.equals(user, that.user) && Objects.equals(queryString, that.queryString);
+        return Objects.equals(project, that.project) && Objects.equals(filename, that.filename) && Objects.equals(query, that.query);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(project, filename, user, queryString);
+        return Objects.hash(project, filename, query);
+    }
+
+    private static Path createFilename(Project project, User user) {
+        String format = ISO_DATE_TIME.format(from(DatashareTime.getInstance().now().toInstant().atZone(ZoneId.of("GMT"))));
+        return Paths.get(format("archive_%s_%s_%s.zip", project.name, user.getId(), format));
     }
 }
--- src/test/java/org/icij/datashare/batch/BatchDownloadTest.java
+++ src/test/java/org/icij/datashare/batch/BatchDownloadTest.java
@@ -1,30 +1,50 @@
 package org.icij.datashare.batch;
 
+import org.icij.datashare.time.DatashareTime;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.nio.file.Paths;
 
+import static org.fest.assertions.Assertions.assertThat;
 import static org.icij.datashare.text.Project.project;
 import static org.icij.datashare.user.User.local;
 
 public class BatchDownloadTest {
     @Test(expected = IllegalArgumentException.class)
     public void test_constructor_null_project() {
-        new BatchDownload(null, Paths.get("test"), local(), "query");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void test_constructor_null_path() {
-        new BatchDownload(project("prj"), null, local(), "query");
+        new BatchDownload(null, local(), "query");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void test_constructor_null_user() {
-        new BatchDownload(project("prj"), Paths.get("test"), null, "query");
+        new BatchDownload(project("prj"),null, "query");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void test_constructor_null_query() {
-        new BatchDownload(project("prj"), Paths.get("test"), local(), null);
+        new BatchDownload(project("prj"), local(), null);
     }
-}
+
+    @Test
+    public void test_batch_download_constructor() {
+        DatashareTime.getInstance().setMockDate("2021-07-07T14:53:47Z");
+
+        BatchDownload batchDownload = new BatchDownload(project("prj"), local(), "foo");
+
+        assertThat(batchDownload.project).isEqualTo(project("prj"));
+        assertThat(batchDownload.query).isEqualTo("foo");
+        assertThat(batchDownload.filename.toString()).isEqualTo("/tmp/archive_prj_local_2021-07-07T14:53:47Z[GMT].zip");
+    }
+
+    @Test
+    public void test_batch_download_constructor_with_downloadDir() {
+        DatashareTime.getInstance().setMockDate("2021-07-07T14:15:16Z");
+
+        assertThat(new BatchDownload(project("prj"), local(), "foo", Paths.get("/bar")).filename.toString()).
+                isEqualTo("/bar/archive_prj_local_2021-07-07T14:15:16Z[GMT].zip");
+    }
+
+    @Before public void setUp() { DatashareTime.setMockTime(true); }
+    @After public void tearDown() { DatashareTime.setMockTime(false); }}
