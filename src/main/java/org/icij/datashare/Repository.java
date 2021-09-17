d7187fef8b66e4522a4b2b68b2fa046b0fbcd1d1
Maxime Vanza <maximevanza@live.fr>
2021-09-17 15:34:43 +0000

    [ICIJ/datashare#832] adds offset and size for getUserEvents
    adds getTotal for UserEvents

--- src/main/java/org/icij/datashare/Repository.java
+++ src/main/java/org/icij/datashare/Repository.java
@@ -19,7 +19,8 @@ public interface Repository {
     AggregateList<User> getRecommendations(Project project);
     AggregateList<User> getRecommendations(Project project, List<String> documentIds);
     boolean addToHistory(Project project, UserEvent userEvent);
-    List<UserEvent> getUserEvents(User user, UserEvent.Type type);
+    List<UserEvent> getUserEvents(User user, UserEvent.Type type, int from, int size);
+    int getTotalUserEvents(User user, UserEvent.Type type);
     boolean deleteUserHistory(User user, UserEvent.Type type);
     boolean deleteUserEvent(User user, int eventId);
 
