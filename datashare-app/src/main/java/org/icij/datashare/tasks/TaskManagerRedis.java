b76baa549a3ae37962fdf860ad7edcbcd78cc84b
Bruno Thomas <bthomas@icij.org>
2021-10-19 12:22:25 +0000

    tests to find the origin of the bug

--- datashare-app/src/main/java/org/icij/datashare/tasks/TaskManagerRedis.java
+++ datashare-app/src/main/java/org/icij/datashare/tasks/TaskManagerRedis.java
@@ -1,7 +1,15 @@
 package org.icij.datashare.tasks;
 
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.jsontype.TypeResolverBuilder;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
 import org.icij.datashare.PropertiesProvider;
 import org.icij.datashare.batch.BatchDownload;
 import org.icij.datashare.json.JsonObjectMapper;
@@ -10,6 +18,9 @@ import org.icij.task.Options;
 import org.redisson.Redisson;
 import org.redisson.RedissonMap;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.BaseCodec;
+import org.redisson.client.codec.ByteArrayCodec;
+import org.redisson.client.handler.State;
 import org.redisson.client.protocol.Decoder;
 import org.redisson.client.protocol.Encoder;
 import org.redisson.codec.JsonJacksonCodec;
@@ -17,6 +28,10 @@ import org.redisson.command.CommandSyncService;
 import org.redisson.liveobject.core.RedissonObjectBuilder;
 
 import javax.inject.Inject;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
@@ -77,7 +92,7 @@ public class TaskManagerRedis implements TaskManager {
     @Override public boolean stopTask(String taskName) { throw new IllegalStateException("not implemented"); }
     @Override public boolean shutdownAndAwaitTermination(int timeout, TimeUnit timeUnit) throws InterruptedException { throw new IllegalStateException("not implemented"); }
 
-    static class TaskViewCodec extends JsonJacksonCodec {
+    static class TaskViewCodec extends MyJsonJacksonCodec {
         private final Encoder keyEncoder;
         private final Decoder<Object> keyDecoder;
 
@@ -96,5 +111,161 @@ public class TaskManagerRedis implements TaskManager {
         }
         @Override public Decoder<Object> getMapKeyDecoder() { return keyDecoder; }
         @Override public Encoder getMapKeyEncoder() { return keyEncoder; }
+    }
+
+    static class MyJsonJacksonCodec extends BaseCodec {
+
+        public static final org.redisson.codec.JsonJacksonCodec INSTANCE = new org.redisson.codec.JsonJacksonCodec();
+
+        @JsonIdentityInfo(generator= ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
+        @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY,
+                getterVisibility = JsonAutoDetect.Visibility.PUBLIC_ONLY,
+                setterVisibility = JsonAutoDetect.Visibility.NONE,
+                isGetterVisibility = JsonAutoDetect.Visibility.NONE)
+        public static class ThrowableMixIn {
+
+        }
+
+        protected final ObjectMapper mapObjectMapper;
+
+        private final Encoder encoder = new Encoder() {
+            @Override
+            public ByteBuf encode(Object in) throws IOException {
+                ByteBuf out = ByteBufAllocator.DEFAULT.buffer();
+                try {
+                    ByteBufOutputStream os = new ByteBufOutputStream(out);
+                    mapObjectMapper.writeValue((OutputStream) os, in);
+                    return os.buffer();
+                } catch (IOException e) {
+                    out.release();
+                    throw e;
+                } catch (Exception e) {
+                    out.release();
+                    throw new IOException(e);
+                }
+            }
+        };
+
+        private final Decoder<Object> decoder = new Decoder<Object>() {
+            @Override
+            public Object decode(ByteBuf buf, State state) throws IOException {
+                return mapObjectMapper.readValue((InputStream) new ByteBufInputStream(buf), Object.class);
+            }
+        };
+
+        public MyJsonJacksonCodec() {
+            this(new ObjectMapper());
+        }
+
+        public MyJsonJacksonCodec(ClassLoader classLoader) {
+            this(createObjectMapper(classLoader, new ObjectMapper()));
+        }
+
+        public MyJsonJacksonCodec(ClassLoader classLoader, MyJsonJacksonCodec codec) {
+            this(createObjectMapper(classLoader, codec.mapObjectMapper.copy()));
+        }
+
+        private static boolean warmedup = false;
+
+        private void warmup() {
+            if (getValueEncoder() == null || getValueDecoder() == null || warmedup) {
+                return;
+            }
+            warmedup = true;
+
+            try {
+                ByteBuf d = getValueEncoder().encode("testValue");
+                getValueDecoder().decode(d, null);
+                d.release();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        protected static ObjectMapper createObjectMapper(ClassLoader classLoader, ObjectMapper om) {
+            TypeFactory tf = TypeFactory.defaultInstance().withClassLoader(classLoader);
+            om.setTypeFactory(tf);
+            return om;
+        }
+
+        public MyJsonJacksonCodec(ObjectMapper mapObjectMapper) {
+            this.mapObjectMapper = mapObjectMapper.copy();
+            this.mapObjectMapper.writerFor(new TypeReference<List<Throwable>>() {});
+            init(this.mapObjectMapper);
+            initTypeInclusion(this.mapObjectMapper);
+            warmup();
+        }
+
+        protected void initTypeInclusion(ObjectMapper mapObjectMapper) {
+            TypeResolverBuilder<?> mapTyper = new ObjectMapper.DefaultTypeResolverBuilder(ObjectMapper.DefaultTyping.NON_FINAL) {
+                public boolean useForType(JavaType t) {
+                    switch (_appliesFor) {
+                        case NON_CONCRETE_AND_ARRAYS:
+                            while (t.isArrayType()) {
+                                t = t.getContentType();
+                            }
+                            // fall through
+                        case OBJECT_AND_NON_CONCRETE:
+                            return (t.getRawClass() == Object.class) || !t.isConcrete();
+                        case NON_FINAL:
+                            while (t.isArrayType()) {
+                                t = t.getContentType();
+                            }
+                            // to fix problem with wrong long to int conversion
+                            if (t.getRawClass() == Long.class) {
+                                return true;
+                            }
+                            if (t.getRawClass() == XMLGregorianCalendar.class) {
+                                return false;
+                            }
+                            return !t.isFinal(); // includes Object.class
+                        default:
+                            // case JAVA_LANG_OBJECT:
+                            return t.getRawClass() == Object.class;
+                    }
+                }
+            };
+            mapTyper.init(JsonTypeInfo.Id.CLASS, null);
+            mapTyper.inclusion(JsonTypeInfo.As.PROPERTY);
+            mapObjectMapper.setDefaultTyping(mapTyper);
+        }
+
+        protected void init(ObjectMapper objectMapper) {
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            objectMapper.setVisibility(objectMapper.getSerializationConfig()
+                    .getDefaultVisibilityChecker()
+                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+                    .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
+                    .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
+            objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+            objectMapper.enable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN);
+            objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+            objectMapper.enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
+            objectMapper.addMixIn(Throwable.class, org.redisson.codec.JsonJacksonCodec.ThrowableMixIn.class);
+        }
+
+        @Override
+        public Decoder<Object> getValueDecoder() {
+            return decoder;
+        }
+
+        @Override
+        public Encoder getValueEncoder() {
+            return encoder;
+        }
+
+        @Override
+        public ClassLoader getClassLoader() {
+            if (mapObjectMapper.getTypeFactory().getClassLoader() != null) {
+                return mapObjectMapper.getTypeFactory().getClassLoader();
+            }
+
+            return super.getClassLoader();
+        }
+
+        public ObjectMapper getObjectMapper() {
+            return mapObjectMapper;
+        }
     }
 }
--- datashare-app/src/test/java/org/icij/datashare/tasks/TaskManagerRedisTest.java
+++ datashare-app/src/test/java/org/icij/datashare/tasks/TaskManagerRedisTest.java
@@ -10,8 +10,9 @@ import redis.clients.jedis.Jedis;
 
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.*;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static org.icij.datashare.text.Project.project;
@@ -33,6 +34,24 @@ public class TaskManagerRedisTest {
 
         assertThat(taskManager.get()).hasSize(1);
         assertThat(taskManager.get(task.name).name).isEqualTo(task.name);
+    }
+
+    @Test
+    public void test_save_failing_task() throws Exception {
+        MonitorableFutureTask<String> test_exception = new MonitorableFutureTask<>(() -> {
+            RuntimeException runtimeException = new RuntimeException("test exception");
+            //runtimeException.addSuppressed(new RuntimeException("suppressed"));
+            throw runtimeException;
+        });
+        try {
+            test_exception.run();
+            test_exception.get();
+        } catch (ExecutionException rex) {
+            taskManager.save(new TaskView<>(test_exception));
+        }
+        List<TaskView<?>> actual = taskManager.get();
+        assertThat(actual).hasSize(1);
+        assertThat(actual.get(0).getState()).isEqualTo(TaskView.State.ERROR);
     }
 
     @Test
