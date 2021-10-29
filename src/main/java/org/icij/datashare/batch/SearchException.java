9b449f15dc325e86d675b73d495ef137071e29b8
Maxime Vanza <maximevanza@live.fr>
2021-10-29 10:16:31 +0000

    Revert "[ICIJ/datashare#853] adds SearchException serializer/deserializer"
    This reverts commit 305d38968264727e084065d02f9e0441da4654c1.

--- src/main/java/org/icij/datashare/batch/SearchException.java
+++ src/main/java/org/icij/datashare/batch/SearchException.java
@@ -1,15 +1,9 @@
 package org.icij.datashare.batch;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-@JsonIgnoreProperties("message")
 public class SearchException extends RuntimeException {
     public final String query;
 
-    @JsonCreator
-    public SearchException(@JsonProperty("query") String query, @JsonProperty("cause") Throwable root) {
+    public SearchException(String query, Throwable root) {
         super(root);
         this.query = query;
     }
