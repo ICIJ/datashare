cb0a62c2faab1e3e57c019abf240daa8fe3065f4
Bruno Thomas <bthomas@icij.org>
2022-04-19 16:07:49 +0000

    chore: clean logback files and add a model download test (manual) ICIJ/datashare#626
    Co-authored-by: Caroline Desprat <caro3801@users.noreply.github.com>

---
+++ datashare-app/src/test/java/org/icij/datashare/nlp/CoreNlpTestManual.java
@@ -0,0 +1,28 @@
+package org.icij.datashare.nlp;
+
+import org.icij.datashare.DynamicClassLoader;
+import org.icij.datashare.PropertiesProvider;
+import org.icij.datashare.text.DocumentBuilder;
+import org.icij.datashare.text.Language;
+import org.icij.datashare.text.NamedEntity;
+import org.icij.datashare.text.nlp.corenlp.CorenlpPipeline;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class CoreNlpTestManual {
+    @Test
+    public void test_download_and_load_jar() throws Exception {
+        DynamicClassLoader systemClassLoader = (DynamicClassLoader)ClassLoader.getSystemClassLoader();
+        File distDir = new File("dist");
+        assertThat(distDir).exists();
+        systemClassLoader.add(distDir.toURI().toURL());
+        CorenlpPipeline corenlpPipeline = new CorenlpPipeline(new PropertiesProvider());
+        corenlpPipeline.initialize(Language.ENGLISH);
+        List<NamedEntity> process = corenlpPipeline.process(DocumentBuilder.createDoc("my_doc_id").with("this is Dwight's document").build());
+        assertThat(process.size()).isGreaterThan(0);
+    }
+}
