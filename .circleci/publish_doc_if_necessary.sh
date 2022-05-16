6c0c80fca482be4491a35faf4af78c59a2b219e3
mvanzalu <maximevanza@live.fr>
2022-05-16 15:04:18 +0000

    build(CI): generate and publish db doc if changed

--- .circleci/publish_doc_if_necessary.sh
+++ .circleci/publish_doc_if_necessary.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
 web_package_has_changed=$(git diff-index --exit-code HEAD~1 datashare-app/src/main/java/org/icij/datashare/web/)
+db_package_has_changed=$(git diff-index --exit-code HEAD~1 datashare-db/src/main/resources/liquibase/)
 CIRCLE_API="https://circleci.com/api"
 
-if [ -n "$web_package_has_changed" ]; then
+if [[ -n "$web_package_has_changed" || -n "$db_package_has_changed" ]]; then
   echo >&2 "Something changed in web package"
   URL="${CIRCLE_API}/v2/project/github/ICIJ/datashare/pipeline"
-  DATA='{"branch": "master", "parameters": { "publish_backend_api": true}}'
+  DATA='{"branch": "master", "parameters": { "publish_backend_doc": true}}'
   HTTP_RESPONSE=$(curl -s -u "${CIRCLE_API_USER_TOKEN}:" -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" "$URL")
 
   if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
