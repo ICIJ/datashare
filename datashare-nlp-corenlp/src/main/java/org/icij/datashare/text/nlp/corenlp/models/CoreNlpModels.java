b9cf5d546018e2d87e9d12d6c64fa40f93bc820a
Bruno Thomas <bthomas@icij.org>
2022-08-22 14:35:38 +0000

    fix: loading NER models on windows #931
    it seems that giving a windows path with \ don't find the in jar file

--- datashare-nlp-corenlp/src/main/java/org/icij/datashare/text/nlp/corenlp/models/CoreNlpModels.java
+++ datashare-nlp-corenlp/src/main/java/org/icij/datashare/text/nlp/corenlp/models/CoreNlpModels.java
@@ -15,18 +15,18 @@ import static java.util.Arrays.asList;
 public abstract class CoreNlpModels<T> extends AbstractModels<CoreNlpAnnotator<T>> {
     static final String VERSION = "4.0.0";
     final Map<Language, String> modelNames = new HashMap<>();
-    private static final Path IN_JAR_BASE_PATH = Paths.get("edu/stanford/nlp/models");
+    private static final String IN_JAR_BASE_PATH = "edu/stanford/nlp/models/";
 
     CoreNlpModels(NlpStage stage) {
         super(Pipeline.Type.CORENLP, stage);
     }
 
     String getJarFileName(Language language) {
-        return String.join("-", asList("stanford", "corenlp", "4.0.0", "models", language.iso6391Code() + ".jar"));
+        return String.join("-", asList("stanford", "corenlp", getVersion(), "models", language.iso6391Code() + ".jar"));
     }
 
     protected String getInJarModelPath(Language language) {
-        return IN_JAR_BASE_PATH.resolve(modelNames.get(language)).toString();
+        return IN_JAR_BASE_PATH + modelNames.get(language);
     }
 
     @Override
