bf2caf89fa0544610dfa3829c1831238c2fb45ab
Bruno Thomas <bthomas@icij.org>
2023-01-31 16:45:44 +0000

    ci: fixing the publish doc ICIJ/datashare#1005

--- doc/apigen/datashare_for_apigen.sh
+++ doc/apigen/datashare_for_apigen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir /tmp/plugins
-./launchBack.sh -p apigen-datashare -u apigen -d $PWD/doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:$PWD/doc/apigen/apigen.db --queueType memory --busType memory --pluginsDir /tmp/plugins
+./launchBack.sh -p apigen-datashare -u apigen -d $PWD/doc/apigen/docs --dataSourceUrl jdbc:sqlite:file:$PWD/doc/apigen/apigen.db --pluginsDir /tmp/plugins
