29c0a8b47c27b4c96206d86242f07d1c313f8bf1
Bruno Thomas <bthomas@icij.org>
2023-01-31 17:01:06 +0000

    ci: fixing the publish doc 2 ICIJ/datashare#1005

--- doc/apigen/populate_apigen.sh
+++ doc/apigen/populate_apigen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl -XDELETE elasticsearch:9200/apigen-datashare
-./launchBack.sh -p apigen-datashare -m CLI --stages SCAN,INDEX,NLP --nlpp CORENLP -d $PWD/doc/apigen/docs --dataSourceUrl jdbc:sqlite:$PWD/doc/apigen/apigen.db --queueType memory --busType memory
+./launchBack.sh -p apigen-datashare -m CLI --stages SCAN,INDEX,NLP --nlpp CORENLP -d $PWD/doc/apigen/docs --dataSourceUrl jdbc:sqlite:$PWD/doc/apigen/apigen.db
