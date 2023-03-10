facbe0a05d89508f94dba8696b3bae693fb6913a
mvanzalu <maximevanza@live.fr>
2023-03-10 17:55:15 +0000

    refactor: remove unused imports

--- datashare-app/src/test/java/org/icij/datashare/web/SettingsResourceTest.java
+++ datashare-app/src/test/java/org/icij/datashare/web/SettingsResourceTest.java
@@ -2,12 +2,8 @@ package org.icij.datashare.web;
 
 import net.codestory.http.filters.basic.BasicAuthFilter;
 import org.apache.tika.exception.TikaConfigException;
-import org.apache.tika.exception.TikaException;
-import org.apache.tika.parser.external.ExternalParser;
-import org.apache.tika.parser.ocr.TesseractOCRConfig;
 import org.apache.tika.parser.ocr.TesseractOCRParser;
 import org.icij.datashare.PropertiesProvider;
-import org.icij.datashare.tasks.DelApiKeyTask;
 import org.icij.datashare.web.testhelpers.AbstractProdWebServerTest;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,10 +20,8 @@ import static org.fest.assertions.Assertions.assertThat;
 import static org.fest.assertions.MapAssert.entry;
 import static org.icij.datashare.session.DatashareUser.local;
 import static org.icij.datashare.session.DatashareUser.singleUser;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
 
 public class SettingsResourceTest extends AbstractProdWebServerTest {
     @Rule public TemporaryFolder folder = new TemporaryFolder();
