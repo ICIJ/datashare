8f019f2b8ef8f05d9506be05ee474c7288364371
Clément Doumouro <clement.doumouro@gmail.com>
2023-06-29 16:40:00 +0200

    Save work

--- launchBack.sh
+++ launchBack.sh
@@ -11,18 +11,25 @@ else
   JAVA=${JAVA_HOME}/bin/java
 fi
 
+if [[ $(uname) == "Darwin" ]]; then
+  DOCKER_PS=docker ps
+else
+  DOCKER_PS=sudo docker ps
+fi
+
+
 export DS_DOCKER_PREVIEW_HOST="http://localhost:5000"
-export DS_DOCKER_BACK_HOST="http://localhost:$(sudo docker ps|grep 8080|sed 's/.*0.0.0.0:\([0-9]\{4\}\)->8080.*/\1/g')"
-export DS_DOCKER_FRONT_HOST="http://localhost:$(sudo docker ps|grep 9090|sed 's/.*0.0.0.0:\([0-9]\{4\}\)->9090.*/\1/g')"
+export DS_DOCKER_BACK_HOST="http://localhost:$($DOCKER_PS|grep 8080|sed 's/.*0.0.0.0:\([0-9]\{4\}\)->8080.*/\1/g')"
+export DS_DOCKER_FRONT_HOST="http://localhost:$($DOCKER_PS|grep 9090|sed 's/.*0.0.0.0:\([0-9]\{4\}\)->9090.*/\1/g')"
 export DS_DOCKER_USER_ADMIN="icij"
 
 mkdir -p $DIR/dist
 
-# options to force debug logs -Dlogback.debug=true -Dlog4j.debug 
+# options to force debug logs -Dlogback.debug=true -Dlog4j.debug
 
 $JAVA -agentlib:jdwp=transport=dt_socket,server=y,address=$JDWP_TRANSPORT_PORT,suspend=n \
  --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED \
- -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader \
+ -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader -Dlogback.debug=true -Dlog4j.debug \
  -Djavax.net.ssl.trustStorePassword=changeit \
  -Ddatashare.loghost=udp:localhost -Dlogback.configurationFile=logback.xml \
  -Xmx4g -DPROD_MODE=true -cp "$DIR/dist/:${CLASSPATH}" org.icij.datashare.Main --cors '*' \
