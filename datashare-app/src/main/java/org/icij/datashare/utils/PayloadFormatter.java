04e0c808b06efb7df0c41d07689b4da7a897bd42
Pierre Romera <hello@pirhoo.com>
2023-07-05 10:08:10 +0000

    refactor: use payloadFormatter

--- datashare-app/src/main/java/org/icij/datashare/utils/PayloadFormatter.java
+++ datashare-app/src/main/java/org/icij/datashare/utils/PayloadFormatter.java
@@ -8,7 +8,16 @@ import java.util.Map;
 public class PayloadFormatter {
 
     public Payload error(String message, int status) {
-        Map<String, String> body = Collections.singletonMap("error", message);
-        return new Payload(body).withCode(status);
+        Map<String, String> responseBody = Collections.singletonMap("error", message);
+        return new Payload(responseBody).withCode(status);
+    }
+
+    public Payload error(RuntimeException error, int status) {
+        String message = error.getMessage();
+        return error(message, status);
+    }
+
+    public Payload json(String responseBody) {
+        return new Payload("application/json", responseBody);
     }
 }
