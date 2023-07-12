78a73c31dc8875603c5aeb5e7e9b645fc4d400b1
Bruno Thomas <bthomas@icij.org>
2023-07-12 10:21:52 +0000

    doc: add the OpenApi documentation for the endpoint

--- datashare-app/src/main/java/org/icij/datashare/web/OpenApiResource.java
+++ datashare-app/src/main/java/org/icij/datashare/web/OpenApiResource.java
@@ -3,6 +3,8 @@ package org.icij.datashare.web;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Singleton;
 import io.swagger.v3.core.util.Json31;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.models.OpenAPI;
 import net.codestory.http.annotations.Get;
 import net.codestory.http.annotations.Prefix;
@@ -14,6 +16,8 @@ import static org.icij.swagger.ClassUtils.findAllClassesUsingClassLoader;
 @Singleton
 @Prefix("/api/openapi")
 public class OpenApiResource {
+    @Operation(description = "Get the JSON OpenAPI v3 contract specification")
+    @ApiResponse(responseCode = "200", description="returns the JSON file")
     @Get()
     public Payload get() {
         final OpenAPI openAPI = new FluentReader().read(findAllClassesUsingClassLoader(getClass().getPackageName()));
