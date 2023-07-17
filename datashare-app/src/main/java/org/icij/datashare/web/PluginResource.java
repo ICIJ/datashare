304a3ffe3731a5dea32884f45b209fc92be647c2
mvanzalu <maximevanza@live.fr>
2023-07-17 08:34:50 +0000

    refactor: add annotations for PluginResource

--- datashare-app/src/main/java/org/icij/datashare/web/PluginResource.java
+++ datashare-app/src/main/java/org/icij/datashare/web/PluginResource.java
@@ -2,13 +2,12 @@ package org.icij.datashare.web;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import liquibase.pro.packaged.E;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import net.codestory.http.Context;
-import net.codestory.http.annotations.Delete;
-import net.codestory.http.annotations.Get;
-import net.codestory.http.annotations.Options;
-import net.codestory.http.annotations.Prefix;
-import net.codestory.http.annotations.Put;
+import net.codestory.http.annotations.*;
 import net.codestory.http.payload.Payload;
 import org.apache.commons.compress.archivers.ArchiveException;
 import org.icij.datashare.DeliverablePackage;
@@ -32,47 +31,30 @@ public class PluginResource {
     @Inject
     public PluginResource(PluginService pluginService) { this.pluginService = pluginService; }
 
-    /**
-     * Gets the plugins set in JSON
-     *
-     * If a request parameter "filter" is provided, the regular expression will be applied to the list.
-     *
-     * see https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html
-     * for pattern syntax.
-     *
-     * Example:
-     * $(curl localhost:8080/api/plugins?filter=.*paginator)
-     */
+    @Operation(description = "Gets the plugins set in JSON.<br>" +
+            "If a request parameter \"filter\" is provided, the regular expression will be applied to the list.<br>" +
+            "See https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html for pattern syntax.",
+            parameters = {@Parameter(name = "filter", description = "regular expression to apply", in = ParameterIn.QUERY)})
+    @ApiResponse(responseCode = "200", description = "returns the plugins set", useReturnTypeSchema = true)
     @Get()
     public Set<DeliverablePackage> getPluginList(Context context) {
         return pluginService.list(ofNullable(context.request().query().get("filter")).orElse(".*"));
     }
 
-    /**
-     * Preflight request
-     *
-     * @return OPTIONS,PUT
-     */
+    @Operation(description = "Preflight request")
+    @ApiResponse(responseCode = "200", description = "returns OPTIONS and PUT")
     @Options("/install")
     public Payload installPluginPreflight() {
         return ok().withAllowMethods("OPTIONS", "PUT");
     }
 
-    /**
-     * Download (if necessary) and install plugin specified by its id or url
-     *
-     * request parameter `id` or `url` must be present.
-     *
-     * @return  200 if the plugin is installed
-     * @return  404 if the plugin is not found by the provided id or url
-     * @return  400 if neither id nor url is provided
-     *
-     * @throws IOException
-     * @throws ArchiveException
-     *
-     * Example:
-     * $(curl -i -XPUT localhost:8080/api/plugins/install?id=datashare-plugin-site-alert)
-     */
+    @Operation(description = "Download (if necessary) and install plugin specified by its id or url." +
+            "Request parameter `id` or `url` must be present.",
+            parameters = {@Parameter(name = "id", description = "id of the plugin", in = ParameterIn.QUERY),
+                    @Parameter(name = "url", description = "url of the plugin", in = ParameterIn.QUERY)})
+    @ApiResponse(responseCode = "200", description = "returns 200 if the plugin is installed")
+    @ApiResponse(responseCode = "400", description = "returns 400 if neither id nor url is provided")
+    @ApiResponse(responseCode = "404", description = "returns 404 if the plugin is not found by the provided id or url")
     @Put("/install")
     public Payload installPlugin(Context context) throws IOException, ArchiveException {
         String pluginUrlString = context.request().query().get("url");
@@ -93,26 +75,13 @@ public class PluginResource {
         }
     }
 
-    /**
-     * Preflight request
-     *
-     * @return OPTIONS,DELETE
-     */
+    @Operation(description = "Preflight request")
+    @ApiResponse(responseCode = "200", description = "returns OPTIONS and DELETE")
     @Options("/uninstall")
     public Payload uninstallPluginPreflight() { return ok().withAllowMethods("OPTIONS", "DELETE");}
 
-    /**
-     * Uninstall plugin specified by its id
-     * Always returns 204 or error 500.
-     *
-     * @param pluginId
-     * @return 204
-     *
-     * @throws IOException if there is a filesystem error
-     *
-     * Example:
-     * $(curl -i -XDELETE localhost:8080/api/plugins/uninstall?id=datashare-plugin-site-alert)
-     */
+    @Operation(description = "Uninstall plugin specified by its id.")
+    @ApiResponse(responseCode = "204", description = "returns 204 if the plugin is uninstalled (idempotent)")
     @Delete("/uninstall?id=:pluginId")
     public Payload uninstallPlugin(String pluginId) throws IOException {
         try {
