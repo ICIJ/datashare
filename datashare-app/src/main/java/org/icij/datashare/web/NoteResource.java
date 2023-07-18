c86392181ece83b6287fd9890a1074b7dab1c087
Bruno Thomas <bthomas@icij.org>
2023-07-18 09:54:14 +0000

    refactor: add openAPI annotations for NoteRessource

--- datashare-app/src/main/java/org/icij/datashare/web/NoteResource.java
+++ datashare-app/src/main/java/org/icij/datashare/web/NoteResource.java
@@ -2,6 +2,9 @@ package org.icij.datashare.web;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import net.codestory.http.Context;
 import net.codestory.http.annotations.Get;
 import net.codestory.http.annotations.Prefix;
@@ -22,39 +25,33 @@ public class NoteResource {
     @Inject
     public NoteResource(Repository repository) {this.repository = repository;}
 
-    /**
-     * Gets the list of notes for a project and a document path.
-     *
-     * if we have on disk:
-     *
-     * ```
-     * /a/b/doc1
-     * /a/c/doc2
-     * /d/doc3
-     * ```
-     *
-     * And in database
-     *
-     * projectId | path | note | variant
-     * --- | --- | --- | ---
-     * p1 | a | note A | info
-     * p1 | a/b | note B | danger
-     *
-     * then :
-     * - `GET /api/p1/notes/a/b/doc1` will return note A and B
-     * - `GET /api/p1/notes/a/c/doc2` will return note A
-     * - `GET /api/p1/notes/d/doc3` will return an empty list
-     *
-     * If the user doesn't have access to the project she gets a 403 Forbidden
-     *
-     * @param project the project the note belongs to
-     * @param documentPath the document path
-     * @param context HTTP context containing the user
-     * @return list of Note that match the document path
-     *
-     * Example:
-     * $(curl localhost:8080/api/apigen-datashare/notes/path/to/note/for/doc.txt)
-     */
+    @Operation(description = "Gets the list of notes for a project and a document path.<br/>" +
+            "if we have on disk:" +
+            "<pre>" +
+            "/a/b/doc1<br/>" +
+            "/a/c/doc2<br/>" +
+            "/d/doc3<br/>" +
+            "</pre>" +
+            "And in database:" +
+            "<pre>" +
+            "projectId | path | note | variant<br/>" +
+            "--- | --- | --- | ---<br/>" +
+            "p1 | a | note A | info<br/>" +
+            "p1 | a/b | note B | danger" +
+            "</pre>" +
+            " then :" +
+            "<pre>" +
+            "- `GET /api/p1/notes/a/b/doc1` will return note A and B<br/>" +
+            "- `GET /api/p1/notes/a/c/doc2` will return note A<br/>" +
+            "- `GET /api/p1/notes/d/doc3` will return an empty list<br/>" +
+            "</pre>",
+            parameters = {
+                @Parameter(name = "project", description = "the project id"),
+                @Parameter(name = "path", description = "the path of the document. It is a greedy parameter at the end of the url"),
+            }
+    )
+    @ApiResponse(responseCode = "403", description = "if the user is not granted for the project")
+    @ApiResponse(responseCode = "200", useReturnTypeSchema = true)
     @Get("/:project/notes/:path:")
     public List<Note> getPathNotes(String project, String documentPath, Context context) {
         DatashareUser user = (DatashareUser) context.currentUser();
@@ -64,18 +61,11 @@ public class NoteResource {
         return repository.getNotes(project(project), documentPath);
     }
 
-    /**
-     * Gets the list of notes for a project.
-     *
-     * If the user doesn't have access to the project she gets a 403 Forbidden
-     *
-     * @param project the project the note belongs to
-     * @param context HTTP context containing the user
-     * @return list of Note related to the project
-     *
-     * Example:
-     * $(curl localhost:8080/api/apigen-datashare/notes)
-     */
+    @Operation(description = "Gets the list of notes for a project.",
+            parameters = {@Parameter(name = "project", description = "the project id"),}
+    )
+    @ApiResponse(responseCode = "403", description = "if the user is not granted for the project")
+    @ApiResponse(responseCode = "200", useReturnTypeSchema = true)
     @Get("/:project/notes")
     public List<Note> getProjectNotes(String project, Context context) {
         DatashareUser user = (DatashareUser) context.currentUser();
