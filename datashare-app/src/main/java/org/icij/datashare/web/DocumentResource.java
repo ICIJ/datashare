3114e83d3dafacbd50f79dc2cdc52ae951a2aafe
Bruno Thomas <bthomas@icij.org>
2023-07-18 16:22:26 +0200

    Update datashare-app/src/main/java/org/icij/datashare/web/DocumentResource.java
    Co-authored-by: Caroline Desprat <cdesprat@icij.org>

--- datashare-app/src/main/java/org/icij/datashare/web/DocumentResource.java
+++ datashare-app/src/main/java/org/icij/datashare/web/DocumentResource.java
@@ -303,7 +303,7 @@ public class DocumentResource {
             },
             requestBody = @RequestBody(content = @Content(mediaType = "application/json", schema = @Schema(implementation = List.class)))
     )
-    @ApiResponse(responseCode = "200", description = "if tag was not in database")
+    @ApiResponse(responseCode = "200", description = "Document untagged")
     @ApiResponse(responseCode = "201", description = "if tag was deleted")
     @Put("/:project/documents/untag/:docId?routing=:routing")
     public Payload untagDocument(final String projectId, final String docId, String routing, Tag[] tags) throws IOException {
