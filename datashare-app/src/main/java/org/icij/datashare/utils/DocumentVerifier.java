ca306bf44f01fe7a8dd04760c1f1278dff871bd5
Pierre Romera <hello@pirhoo.com>
2023-08-08 19:12:10 +0000

    refactor: move document download check to a verifier

---
+++ datashare-app/src/main/java/org/icij/datashare/utils/DocumentVerifier.java
@@ -0,0 +1,55 @@
+package org.icij.datashare.utils;
+
+import org.icij.datashare.HumanReadableSize;
+import org.icij.datashare.PropertiesProvider;
+import org.icij.datashare.text.Document;
+import org.icij.datashare.text.indexing.Indexer;
+
+import static org.icij.datashare.cli.DatashareCliOptions.EMBEDDED_DOCUMENT_DOWNLOAD_MAX_SIZE;
+
+/**
+ * This class is responsible for verifying properties and conditions of documents.
+ */
+public class DocumentVerifier {
+
+    private static final String DEFAULT_MAX_SIZE = "1G";
+
+    private final Indexer indexer;
+    private final PropertiesProvider propertiesProvider;
+
+    /**
+     * Constructs a new DocumentVerifier with the provided indexer and propertiesProvider.
+     *
+     * @param indexer The indexer used to fetch document details.
+     * @param propertiesProvider The provider used to fetch system properties.
+     */
+    public DocumentVerifier(Indexer indexer, PropertiesProvider propertiesProvider) {
+        this.indexer = indexer;
+        this.propertiesProvider = propertiesProvider;
+    }
+
+    /**
+     * Checks if the root document size is allowed based on the provided document's properties.
+     *
+     * @param document The document to verify.
+     * @return true if the root document size is allowed, false otherwise.
+     */
+    public boolean isRootDocumentSizeAllowed(Document document) {
+        if (document.isRootDocument()) {
+            return true;
+        }
+        long maxSizeBytes = getEmbeddedDocumentDownloadMaxSizeBytes();
+        Document rootDocument = indexer.get(document.getProjectId(), document.getRootDocument());
+        return rootDocument.getContentLength() < maxSizeBytes;
+    }
+
+    /**
+     * Retrieves the max size in bytes based on app properties.
+     *
+     * @return The max size in bytes.
+     */
+    private long getEmbeddedDocumentDownloadMaxSizeBytes() {
+        String maxSize = propertiesProvider.get(EMBEDDED_DOCUMENT_DOWNLOAD_MAX_SIZE).orElse(DEFAULT_MAX_SIZE);
+        return HumanReadableSize.parse(maxSize);
+    }
+}
---
+++ datashare-app/src/test/java/org/icij/datashare/utils/DocumentVerifierTest.java
@@ -0,0 +1,64 @@
+package org.icij.datashare.utils;
+
+import org.icij.datashare.PropertiesProvider;
+import org.icij.datashare.text.Document;
+import org.icij.datashare.text.DocumentBuilder;
+import org.icij.datashare.text.Project;
+import org.icij.datashare.text.indexing.Indexer;
+import org.junit.Test;
+import org.junit.Before;
+import org.mockito.Mock;
+
+import java.util.Optional;
+
+import static org.icij.datashare.cli.DatashareCliOptions.EMBEDDED_DOCUMENT_DOWNLOAD_MAX_SIZE;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class DocumentVerifierTest {
+
+    @Mock private Indexer indexer;
+    @Mock private PropertiesProvider propertiesProvider;
+    private DocumentVerifier documentVerifier;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        documentVerifier = new DocumentVerifier(indexer, propertiesProvider);
+    }
+
+    @Test
+    public void testIsRootDocumentSizeAllowed_RootDocument() {
+        Document doc = DocumentBuilder.createDoc("foo").withContentLength(2L * 1024 * 1024 * 1024).build();
+        assertTrue(documentVerifier.isRootDocumentSizeAllowed(doc));
+    }
+
+    @Test
+    public void testIsRootDocumentSizeAllowed_SizeAllowed() {
+        Project project = new Project("local-datashare");
+        Document rootDoc = DocumentBuilder.createDoc("bar").with(project).withContentLength(1024).build();
+        Document doc = DocumentBuilder.createDoc("foo").with(project).withParentId("bar").withRootId("bar").build();
+
+        when(indexer.get(project.getId(), "bar")).thenReturn(rootDoc);
+        when(propertiesProvider.get(EMBEDDED_DOCUMENT_DOWNLOAD_MAX_SIZE)).thenReturn(Optional.of("200G"));
+
+        assertTrue(documentVerifier.isRootDocumentSizeAllowed(doc));
+    }
+
+    @Test
+    public void testIsRootDocumentSizeAllowed_SizeNotAllowed() {
+        Project project = new Project("local-datashare");
+        Document rootDoc = DocumentBuilder.createDoc("bar").with(project).withContentLength(1024).build();
+        Document doc = DocumentBuilder.createDoc("foo").with(project).withParentId("bar").withRootId("bar").build();
+
+        when(indexer.get(project.getId(), "bar")).thenReturn(rootDoc);
+        when(propertiesProvider.get(EMBEDDED_DOCUMENT_DOWNLOAD_MAX_SIZE)).thenReturn(Optional.of("200"));
+
+        assertFalse(documentVerifier.isRootDocumentSizeAllowed(doc));
+    }
+
+    private void indexFile(String index, Document document) {
+        when(indexer.get(index, document.getId())).thenReturn(document);
+    }
+}
