f99aff46ec232cc888d0e6b8f78b8911753b3449
Bruno Thomas <bthomas@icij.org>
2023-08-29 08:16:51 +0000

    refactor: remove unecessary executor in QpidAmqpServer class

--- datashare-app/src/test/java/org/icij/datashare/com/bus/AmqpServerRuleTest.java
+++ datashare-app/src/test/java/org/icij/datashare/com/bus/AmqpServerRuleTest.java
@@ -2,22 +2,45 @@ package org.icij.datashare.com.bus;
 
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
+
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.Fail.fail;
 
 public class AmqpServerRuleTest {
     @ClassRule static public AmqpServerRule qpid = new AmqpServerRule(12345);
 
     @Test
     public void test_amqp_client_connection() throws Exception {
+        assertAmqpIsUp(true);
+    }
+
+    @Test
+    public void test_restart_server() throws Exception {
+        assertThat(qpid.amqpServer.stop()).isTrue();
+        assertAmqpIsUp(false);
+        qpid.amqpServer.start();
+        assertAmqpIsUp(true);
+    }
+
+    private static void assertAmqpIsUp(boolean shouldBeUp) throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setUri("amqp://admin:admin@localhost:12345");
         try {
             Connection connection = factory.newConnection();
+            if (!shouldBeUp) {
+                fail("should not be able to connect broker");
+            }
             connection.close();
         } catch (Exception e) {
-            Assert.fail(e.getMessage());
+            if (shouldBeUp) {
+                fail("cannot connect broker", e);
+            }
         }
     }
 }
