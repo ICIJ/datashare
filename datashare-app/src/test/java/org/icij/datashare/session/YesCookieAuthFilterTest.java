2282dbc71a31c7ed8d87c2b01e780a3df1042e6a
Pierre Romera <hello@pirhoo.com>
2023-08-30 10:05:00 +0000

    feat: implement db project for YesCookieAuthFilter

--- datashare-app/src/test/java/org/icij/datashare/session/YesCookieAuthFilterTest.java
+++ datashare-app/src/test/java/org/icij/datashare/session/YesCookieAuthFilterTest.java
@@ -5,44 +5,75 @@ import net.codestory.http.filters.PayloadSupplier;
 import net.codestory.http.payload.Payload;
 import net.codestory.http.security.User;
 import org.icij.datashare.PropertiesProvider;
+import org.icij.datashare.db.JooqRepository;
+import org.icij.datashare.text.Project;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.ArgumentCaptor.forClass;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class YesCookieAuthFilterTest {
+    @Mock JooqRepository jooqRepository;
     private Payload next = Payload.ok();
     private PayloadSupplier nextFilter = () -> next;
     private Context context = mock(Context.class);
     private ArgumentCaptor<User> user = forClass(User.class);
 
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
     @Test
     public void test_matches() {
-        YesCookieAuthFilter filter = new YesCookieAuthFilter(new PropertiesProvider(new HashMap<String, String>() {{
-            put("protectedUrPrefix", "prefix");
-        }}));
+        when(jooqRepository.getProjects()).thenReturn(new ArrayList<>());
+        PropertiesProvider propertiesProvider = new PropertiesProvider(Map.of("protectedUrlPrefix", "prefix"));
+        YesCookieAuthFilter filter = new YesCookieAuthFilter(propertiesProvider, jooqRepository);
         assertThat(filter.matches("foo", null)).isFalse();
         assertThat(filter.matches("prefix", null)).isTrue();
     }
 
     @Test
-     public void test_adds_new_user_to_context() throws Exception {
-        YesCookieAuthFilter filter = new YesCookieAuthFilter(new PropertiesProvider(new HashMap<String, String>() {{
-            put("defaultProject", "demo");
-            put("messageBusAddress", "redis://redis:6379");
-        }}));
-         Payload payload = filter.apply("url", context, nextFilter);
+    public void test_adds_new_user_to_context() throws Exception {
+        when(jooqRepository.getProjects()).thenReturn(new ArrayList<>());
+        PropertiesProvider propertiesProvider = new PropertiesProvider();
+        propertiesProvider.getProperties().put("defaultProject", "demo");
+        propertiesProvider.getProperties().put("messageBusAddress", "redis://redis:6379");
+        YesCookieAuthFilter filter = new YesCookieAuthFilter(propertiesProvider, jooqRepository);
+        Payload payload = filter.apply("url", context, nextFilter);
 
-         assertThat(payload).isSameAs(next);
-         verify(context).setCurrentUser(user.capture());
-         assertThat(user.getValue().login()).isNotEmpty();
-         assertThat(((DatashareUser)user.getValue()).getProjectNames()).containsExactly("demo");
-         assertThat(user.getValue().isInRole("local")).isFalse();
-     }
+        assertThat(payload).isSameAs(next);
+        verify(context).setCurrentUser(user.capture());
+        assertThat(user.getValue().login()).isNotEmpty();
+        assertThat(((DatashareUser)user.getValue()).getProjectNames()).containsExactly("demo");
+        assertThat(user.getValue().isInRole("local")).isFalse();
+    }
 
+    @Test
+    public void test_adds_new_user_to_context_with_database_projects() throws Exception {
+        when(jooqRepository.getProjects()).thenReturn(new ArrayList<>(List.of(new Project("foo"))));
+        PropertiesProvider propertiesProvider = new PropertiesProvider();
+        propertiesProvider.getProperties().put("defaultProject", "demo");
+        propertiesProvider.getProperties().put("messageBusAddress", "redis://redis:6379");
+        YesCookieAuthFilter filter = new YesCookieAuthFilter(propertiesProvider, jooqRepository);
+        Payload payload = filter.apply("url", context, nextFilter);
+
+        assertThat(payload).isSameAs(next);
+        verify(context).setCurrentUser(user.capture());
+        assertThat(user.getValue().login()).isNotEmpty();
+        assertThat(((DatashareUser)user.getValue()).getProjectNames()).contains("demo");
+        assertThat(((DatashareUser)user.getValue()).getProjectNames()).contains("foo");
+        assertThat(user.getValue().isInRole("local")).isFalse();
+    }
 }
