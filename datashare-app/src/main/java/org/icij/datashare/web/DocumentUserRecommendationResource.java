fb17b3d6f2c1eb36ca2b4f22288911b763b89f19
Pierre Romera <hello@pirhoo.com>
2023-10-24 11:01:09 +0000

    feat: add endpoints to retrieve document user recommendations

---
+++ datashare-app/src/main/java/org/icij/datashare/web/DocumentUserRecommendationResource.java
@@ -0,0 +1,72 @@
+package org.icij.datashare.web;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import net.codestory.http.Context;
+import net.codestory.http.annotations.Get;
+import net.codestory.http.annotations.Options;
+import net.codestory.http.annotations.Prefix;
+import net.codestory.http.constants.HttpStatus;
+import net.codestory.http.payload.Payload;
+import net.codestory.http.security.User;
+import org.icij.datashare.DocumentUserRecommendation;
+import org.icij.datashare.Repository;
+import org.icij.datashare.session.DatashareUser;
+import org.icij.datashare.text.Project;
+import org.icij.datashare.text.ProjectProxy;
+import org.icij.datashare.utils.PayloadFormatter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.Integer.parseInt;
+import static java.util.Optional.ofNullable;
+import static net.codestory.http.payload.Payload.ok;
+
+@Singleton
+@Prefix("/api/document-user-recommendations")
+public class DocumentUserRecommendationResource {
+    private final Repository repository;
+
+    List<Project> getUserProjects(User user) {
+        DatashareUser datashareUser = (DatashareUser) user;
+        List<String> projectNames = datashareUser.getProjectNames();
+        return repository.getProjects(projectNames);
+    }
+
+    @Inject
+    public DocumentUserRecommendationResource(Repository repository) {
+        this.repository = repository;
+    }
+
+    @Operation(description = "Preflight options request")
+    @ApiResponse(responseCode = "200", description = "returns 200 with OPTIONS and GET")
+    @Options("/")
+    public Payload options() {
+        return PayloadFormatter.allowMethods("OPTIONS", "GET");
+    }
+
+    @Operation(description = "Gets all user's document recommendations.")
+    @ApiResponse(responseCode = "200", description = "returns the user's document recommendations", useReturnTypeSchema = true)
+    @ApiResponse(responseCode = "400", description = "if either `from` or `size` are present and cannot be parsed")
+    @Get("/")
+    public Payload get(Context context) {
+        String projectName = context.get("project");
+        // Filter the list of target projects by
+        // the given project (if any) in query parameters.
+        List<Project> projects = getUserProjects(context.currentUser())
+                .stream()
+                .filter(project -> projectName == null || project.name.equals(projectName))
+                .collect(Collectors.toList());
+        try {
+            int from = parseInt(ofNullable(context.get("from")).orElse("0"));
+            int size = parseInt(ofNullable(context.get("size")).orElse("50"));
+            List<DocumentUserRecommendation> recommendations = repository.getDocumentUserRecommendations(from, size, projects);
+            return PayloadFormatter.json(recommendations);
+        } catch (NumberFormatException e) {
+            return PayloadFormatter.error("Wrong pagination parameters (`from` or `size`).", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
