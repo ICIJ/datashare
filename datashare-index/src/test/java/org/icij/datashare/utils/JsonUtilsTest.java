60dfa9b1ad6c882df9315fd91d9b043001e48139
mvanzalu <maximevanza@live.fr>
2023-10-24 15:12:51 +0000

    refactor: extract nodeToMap method

---
+++ datashare-index/src/test/java/org/icij/datashare/utils/JsonUtilsTest.java
@@ -0,0 +1,24 @@
+package org.icij.datashare.utils;
+
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.fest.assertions.MapAssert;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.MapAssert.entry;
+
+public class JsonUtilsTest {
+
+    @Test
+    public void test_should_return_source_as_map() {
+        ObjectNode node = JsonNodeFactory.instance.objectNode()
+                .put("field1", "value1")
+                .put("field2", "value2");
+        assertThat(JsonUtils.nodeToMap(node)).includes(
+              entry("field1", "value1"),
+              entry("field2", "value2")
+        );
+    }
+}
