a88e938f2ea1cc905479af2b03d83f1523c3819e
Pierre Romera <hello@pirhoo.com>
2023-10-24 13:20:40 +0000

    refactor: use correct name for DocumentUserRecommendationResourceTest

--- datashare-app/src/test/java/org/icij/datashare/web/DocumentUserRecommendationResourceTest.java
+++ datashare-app/src/test/java/org/icij/datashare/web/DocumentUserRecommendationResourceTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-public class DocumentUserRecommendationTest  extends AbstractProdWebServerTest {
+public class DocumentUserRecommendationResourceTest extends AbstractProdWebServerTest {
     @Mock
     PropertiesProvider propertiesProvider;
     @Mock
