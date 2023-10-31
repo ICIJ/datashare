de2ff7297d1cbfe5ae07e756d7b4db6b5671feaa
Caroline Desprat <cdesprat@icij.org>
2023-10-31 10:21:06 +0000

    refactor: rely on Array stream methods

--- datashare-app/src/main/java/org/icij/datashare/utils/PayloadFormatter.java
+++ datashare-app/src/main/java/org/icij/datashare/utils/PayloadFormatter.java
@@ -4,9 +4,7 @@ import net.codestory.http.payload.Payload;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static net.codestory.http.payload.Payload.ok;
 
@@ -31,10 +29,8 @@ public class PayloadFormatter {
     }
 
     static public Payload allowMethods(String... methodOrMethods) {
-        List<String> methods = Arrays.stream(methodOrMethods)
+        return ok().withAllowMethods(Arrays.stream(methodOrMethods)
                 .flatMap(s -> Arrays.stream(s.split(",")))
-                .map(String::trim)
-                .collect(Collectors.toList());
-        return ok().withAllowMethods(methods.toArray(new String[0]));
+                .map(String::trim).toArray(String[]::new));
     }
 }
