97770366046cea4b9229163efa36b887c5331b31
mvanzalu <maximevanza@live.fr>
2024-01-18 16:54:29 +0000

    refactor: remove publisher from indexTask #1283

--- datashare-app/src/main/java/org/icij/datashare/tasks/IndexTask.java
+++ datashare-app/src/main/java/org/icij/datashare/tasks/IndexTask.java
@@ -4,10 +4,6 @@ import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 import org.icij.datashare.PropertiesProvider;
 import org.icij.datashare.cli.DatashareCli;
-import org.icij.datashare.com.Channel;
-import org.icij.datashare.com.Message;
-import org.icij.datashare.com.Publisher;
-import org.icij.datashare.com.ShutdownMessage;
 import org.icij.datashare.monitoring.Monitorable;
 import org.icij.datashare.text.indexing.elasticsearch.ElasticsearchSpewer;
 import org.icij.datashare.user.User;
@@ -26,12 +22,9 @@ import java.nio.file.Path;
 import java.util.Properties;
 
 import static java.lang.Math.max;
-import static java.lang.String.valueOf;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.icij.datashare.PropertiesProvider.MAP_NAME_OPTION;
-import static org.icij.datashare.com.Message.Field.VALUE;
-import static org.icij.datashare.com.Message.Type.INIT_MONITORING;
 
 @OptionsClass(Extractor.class)
 @OptionsClass(DocumentFactory.class)
@@ -40,17 +33,15 @@ public class IndexTask extends PipelineTask<Path> implements Monitorable{
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final DocumentQueueDrainer<Path> drainer;
     private final DocumentConsumer consumer;
-    private final Publisher publisher;
     private long totalToProcess;
     private final Integer parallelism;
 
     @Inject
-    public IndexTask(final ElasticsearchSpewer spewer, final Publisher publisher, final DocumentCollectionFactory<Path> factory, @Assisted User user, @Assisted String queueName,
+    public IndexTask(final ElasticsearchSpewer spewer, final DocumentCollectionFactory<Path> factory, @Assisted User user, @Assisted String queueName,
                      @Assisted final Properties properties) {
         super(DatashareCli.Stage.INDEX, user, queueName, factory, new PropertiesProvider(properties), Path.class);
         PropertiesProvider propertiesProvider = new PropertiesProvider(properties);
         parallelism = propertiesProvider.get("parallelism").map(Integer::parseInt).orElse(Runtime.getRuntime().availableProcessors());
-        this.publisher = publisher;
         String indexName = propertiesProvider.get("defaultProject").orElse("local-datashare");
         spewer.withIndex(indexName); // TODO: remove this
         try {
@@ -78,7 +69,6 @@ public class IndexTask extends PipelineTask<Path> implements Monitorable{
         drainer.shutdown();
         drainer.awaitTermination(10, SECONDS); // drain is finished
         logger.info("drained {} documents. Waiting for consumer to shutdown", totalToProcess);
-        publisher.publish(Channel.NLP, new Message(INIT_MONITORING).add(VALUE, valueOf(totalToProcess)));
 
         consumer.shutdown();
         // documents could be currently processed
@@ -89,7 +79,6 @@ public class IndexTask extends PipelineTask<Path> implements Monitorable{
         } catch (InterruptedException iex) {
             logger.info("Got InterruptedException while waiting for the consumer shutdown.");
         }
-        publisher.publish(Channel.NLP, new ShutdownMessage());
 
         if (consumer.getReporter() != null) consumer.getReporter().close();
         queue.close();
