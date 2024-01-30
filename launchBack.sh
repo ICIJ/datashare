7fdb2bb9470135d8cbe0a7357a0eee846f9b4552
Bruno Thomas <bthomas@icij.org>
2024-01-30 13:35:00 +0000

    feat: add sync env variable for NLP models

--- launchBack.sh
+++ launchBack.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 VERSION=$(cat $DIR/pom.xml | grep '<version>[0-9.]\+' | sed 's/<version>\([0-9a-z.\-]\+\)<\/version>/\1/g' | tr -d '[:space:]')
 CLASSPATH=${CLASSPATH:-$DIR/datashare-dist/target/datashare-dist-${VERSION}-all.jar}
 JDWP_TRANSPORT_PORT=${JDWP_TRANSPORT_PORT:-8000}
+DS_SYNC_NLP_MODELS=${DS_SYNC_NLP_MODELS:-true}
 
 if [[ $@ == *"SERVER"* ]]; then
   OAUTH_LOCAL_OPTIONS="--oauthAuthorizeUrl http://xemx:3001/oauth/authorize --oauthTokenUrl http://xemx:3001/oauth/token --oauthApiUrl http://xemx:3001/api/v1/me.json"
@@ -35,6 +36,7 @@ mkdir -p $DIR/dist
 $JAVA -agentlib:jdwp=transport=dt_socket,server=y,address=$JDWP_TRANSPORT_PORT,suspend=n \
  --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED \
  -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader \
+ -DDS_SYNC_NLP_MODELS=$DS_SYNC_NLP_MODELS \
  -Djavax.net.ssl.trustStorePassword=changeit \
  -Ddatashare.loghost=udp:localhost -Dlogback.configurationFile=logback.xml \
  -Xmx4g -DPROD_MODE=true -cp "$DIR/dist/:${CLASSPATH}" org.icij.datashare.Main $OAUTH_LOCAL_OPTIONS "$@"
