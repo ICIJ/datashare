8c04027994630219e0bce080a67d9bc5c77c78d6
Bruno Thomas <bthomas@icij.org>
2024-01-31 13:46:12 +0000

    fix: unused import

--- datashare-cli/src/main/java/org/icij/datashare/cli/DatashareCli.java
+++ datashare-cli/src/main/java/org/icij/datashare/cli/DatashareCli.java
@@ -9,7 +9,6 @@ import org.icij.datashare.cli.spi.CliExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
