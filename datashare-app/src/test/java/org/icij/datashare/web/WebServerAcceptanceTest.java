134b2426fce14cb3e7b15307d0b6d6a19706101f
Pierre Romera <hello@pirhoo.com>
2024-02-20 20:09:58 +0000

    test: create temporary datashare home

--- datashare-app/src/test/java/org/icij/datashare/web/WebServerAcceptanceTest.java
+++ datashare-app/src/test/java/org/icij/datashare/web/WebServerAcceptanceTest.java
@@ -2,18 +2,25 @@ package org.icij.datashare.web;
 
 import org.icij.datashare.cli.DatashareCli;
 import org.icij.datashare.mode.CommonMode;
-import org.icij.datashare.mode.ServerMode;
 import org.icij.datashare.web.testhelpers.AbstractProdWebServerTest;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.lang.String.format;
+
 public class WebServerAcceptanceTest extends AbstractProdWebServerTest {
     @Before
     public void setUp() throws Exception {
+        Path datashareHome = Files.createTempDirectory("datashare-");
         String[] args = {
             "--cors=*",
             "--mode=SERVER",
-            "--dataSourceUrl=jdbc:sqlite:file:/tmp/datashare.db"
+            format("--dataSourceUrl=jdbc:sqlite:file:%s/db.db", datashareHome),
+            format("--pluginsDir=%s", datashareHome),
+            format("--extensionsDir=%s", datashareHome)
         };
         configure(CommonMode.create(new DatashareCli().parseArguments(args).properties).createWebConfiguration());
         waitForDatashare();
