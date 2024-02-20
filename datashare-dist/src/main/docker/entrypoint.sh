ba05ddacc627c35a53fa3933b01c7ef25892e1ea
Pierre Romera <hello@pirhoo.com>
2024-02-20 19:05:33 +0000

    build: use same entrypoint params than in datashare bin

--- datashare-dist/src/main/docker/entrypoint.sh
+++ datashare-dist/src/main/docker/entrypoint.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 set -e
 
-MAIN_CLASS=org.icij.datashare.Main
+java_bin=${JAVA_HOME:-/usr}/bin/java
+java_opts=${DS_JAVA_OPTS:-''}
+datashare_jar=$(find /home/datashare/lib/ -name '*.jar' | xargs | sed 's/ /:/g')
+datashare_home=${DATASHARE_HOME:-$HOME/.local/share/datashare}
+datashare_jna_tmpdir=${DATASHARE_JNA_TMPDIR:-$datashare_home/index/tmp}
+datashare_sync_nlp_models=${DATASHARE_SYNC_NLP_MODELS:-true}
 
 if [ "$1" = 'sh' ];
 then
     exec "$@"
 else
-    CLASSPATH=$(find /home/datashare/lib/ -name '*.jar' | xargs | sed 's/ /:/g')${CLASSPATH:+:$CLASSPATH}
-    # shellcheck disable=SC2086
-    # https://github.com/koalaman/shellcheck/wiki/Sc2086
-    exec java ${DS_JAVA_OPTS} \
+    $java_bin $java_opts \
+      --add-opens java.base/java.lang=ALL-UNNAMED \
+      --add-opens java.base/java.util=ALL-UNNAMED \
       -DPROD_MODE=true \
+      -Dfile.encoding=UTF-8 \
       -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader \
-      -cp "/home/datashare/dist/:${CLASSPATH}" \
-      ${MAIN_CLASS} "$@"
+      -Djna.tmpdir=$datashare_jna_tmpdir \
+      -DDS_SYNC_NLP_MODELS=$datashare_sync_nlp_models \
+      -cp ./dist:$datashare_jar org.icij.datashare.Main \
+        "$@"
 fi
