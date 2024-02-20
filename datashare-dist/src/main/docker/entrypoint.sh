0f8efa14a129bc6f2c42a64b22dfeb310af70491
Pierre Romera <hello@pirhoo.com>
2024-02-20 12:30:53 +0000

    refactor: format

--- datashare-dist/src/main/docker/entrypoint.sh
+++ datashare-dist/src/main/docker/entrypoint.sh
@@ -10,5 +10,9 @@ else
     CLASSPATH=$(find /home/datashare/lib/ -name '*.jar' | xargs | sed 's/ /:/g')${CLASSPATH:+:$CLASSPATH}
     # shellcheck disable=SC2086
     # https://github.com/koalaman/shellcheck/wiki/Sc2086
-    exec java ${DS_JAVA_OPTS} -DPROD_MODE=true -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader -cp "/home/datashare/dist/:${CLASSPATH}" ${MAIN_CLASS} "$@"
+    exec java ${DS_JAVA_OPTS} \
+      -DPROD_MODE=true \
+      -Djava.system.class.loader=org.icij.datashare.DynamicClassLoader \
+      -cp "/home/datashare/dist/:${CLASSPATH}" \
+      ${MAIN_CLASS} "$@"
 fi
