542841126fe63d3db1707ba14c00b13da9c3ff36
Pierre Romera <hello@pirhoo.com>
2024-02-27 12:34:19 +0000

    fix: remove final keywork

--- datashare-app/src/main/java/org/icij/datashare/tasks/TaskFactory.java
+++ datashare-app/src/main/java/org/icij/datashare/tasks/TaskFactory.java
@@ -19,7 +19,7 @@ public interface TaskFactory {
     DelApiKeyTask createDelApiKey(User user);
     GetApiKeyTask createGetApiKey(User user);
 
-    ScanIndexTask createScanIndexTask(final User user, final Properties properties);
+    ScanIndexTask createScanIndexTask(final User user, Properties properties);
     ScanTask createScanTask(final User user, final Path path, Properties properties);
     IndexTask createIndexTask(final User user, final Properties properties);
     ExtractNlpTask createNlpTask(final User user, final Properties properties);
