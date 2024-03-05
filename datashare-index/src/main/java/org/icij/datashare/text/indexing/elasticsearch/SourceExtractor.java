e775a5f37ff34d7856091bab74be4741b3f83f7d
Pierre Romera <hello@pirhoo.com>
2024-03-05 15:53:12 +0000

    fix: use retrocompatible digester with defaultProject from PropertiesProvider #1356

--- datashare-index/src/main/java/org/icij/datashare/text/indexing/elasticsearch/SourceExtractor.java
+++ datashare-index/src/main/java/org/icij/datashare/text/indexing/elasticsearch/SourceExtractor.java
@@ -1,9 +1,10 @@
 package org.icij.datashare.text.indexing.elasticsearch;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.parser.DigestingParser;
 import org.apache.tika.parser.digestutils.CommonsDigester;
+import org.icij.datashare.PropertiesProvider;
+import org.icij.datashare.cli.Mode;
 import org.icij.datashare.text.Document;
 import org.icij.datashare.text.Hasher;
 import org.icij.datashare.text.Project;
@@ -21,19 +22,33 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.icij.datashare.PropertiesProvider.DEFAULT_PROJECT_OPTION;
+
 
 public class SourceExtractor {
     Logger LOGGER = LoggerFactory.getLogger(SourceExtractor.class);
+    private final PropertiesProvider propertiesProvider;
     private final boolean filterMetadata;
     private final MetadataCleaner metadataCleaner = new MetadataCleaner();
 
     public SourceExtractor() {
-        this(false);
+        this(new PropertiesProvider(), false);
     }
 
-    public SourceExtractor(boolean filterMetadata) {
+    public SourceExtractor(PropertiesProvider propertiesProvider) {
+        this(propertiesProvider, false);
+    }
+
+    public SourceExtractor( boolean filterMetadata) {
+        this.propertiesProvider = new PropertiesProvider();
         this.filterMetadata = filterMetadata;
     }
+
+    public SourceExtractor(PropertiesProvider propertiesProvider, boolean filterMetadata) {
+        this.propertiesProvider = propertiesProvider;
+        this.filterMetadata = filterMetadata;
+    }
+
 
     public InputStream getSource(final Document document) throws FileNotFoundException {
         return getSource(document.getProject(), document);
@@ -51,7 +66,7 @@ public class SourceExtractor {
                 return new FileInputStream(document.getPath().toFile());
             }
         } else {
-            LOGGER.info("extracting embedded document " + Identifier.shorten(document.getId(), 4) + " from root document " + document.getPath());
+            LOGGER.info("Extracting embedded document " + Identifier.shorten(document.getId(), 4) + " from root document " + document.getPath());
             return getEmbeddedSource(project, document);
         }
     }
@@ -59,9 +74,16 @@ public class SourceExtractor {
     public InputStream getEmbeddedSource(final Project project, final Document document) {
         Hasher hasher = Hasher.valueOf(document.getId().length());
         String algorithm = hasher.toString();
+        int i = 0;
         List<DigestingParser.Digester> digesters = new ArrayList<>(List.of());
-        digesters.add(new UpdatableDigester(project.getId(), algorithm));
+        // Digester with the project name
         digesters.add(new CommonsDigester(20 * 1024 * 1024,  algorithm.replace("-", "")));
+        // Digester with the project name
+        digesters.add(new UpdatableDigester(project.getId(), algorithm));
+        // Digester with the project name set on "defaultProject" for retro-compatibility
+        if (mightUseLegacyDigester(document)) {
+            digesters.add(new UpdatableDigester(getDefaultProject(), algorithm));
+        }
 
         // Try each digester to find embedded doc and ensure we 
         // used every available digesters to find it.
@@ -78,7 +100,7 @@ public class SourceExtractor {
                 }
                 return inputStream;
             } catch (ContentNotFoundException | SAXException | TikaException | IOException ex) {
-                LOGGER.info("Extract attempt for embedded document failed:");
+                LOGGER.info(String.format("Extract attempt %s/%s for embedded document failed:", ++i, digesters.size()));
                 LOGGER.info(String.format("\t├── exception: %s",  ex.getClass().getSimpleName()));
                 LOGGER.info(String.format("\t├── algorithm: %s",  algorithm));
                 LOGGER.info(String.format("\t├── digester: %s",  digester.getClass().getSimpleName()));
@@ -90,4 +112,18 @@ public class SourceExtractor {
 
         throw new ContentNotFoundException(document.getRootDocument(), document.getId());
     }
+
+    private boolean mightUseLegacyDigester(Document document) {
+        return !isServerMode() && document.getExtractionLevel() > 0 && !document.getProject().name.equals(getDefaultProject());
+    }
+
+    private String getDefaultProject() {
+        return this.propertiesProvider.get(DEFAULT_PROJECT_OPTION).orElse("local-datashare");
+    }
+
+    private boolean isServerMode() {
+        return propertiesProvider.get("mode").orElse(Mode.SERVER.name()).equals((Mode.SERVER.name()));
+    }
+
+
 }
