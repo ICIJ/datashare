9a6b3099e4cc6eecc60666715ed9c818eedc27e7
Clément Doumouro <clement.doumouro@gmail.com>
2024-05-15 16:56:41 +0200

    refactor: remove redission dependency in AMPQ task manager

--- datashare-app/src/main/java/org/icij/datashare/tasks/TaskManagerAmqp.java
+++ datashare-app/src/main/java/org/icij/datashare/tasks/TaskManagerAmqp.java
@@ -3,9 +3,15 @@ package org.icij.datashare.tasks;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.io.IOException;
+import org.icij.datashare.asynctasks.TaskManagerRedis;
+import org.icij.datashare.asynctasks.TaskView;
 import org.icij.datashare.asynctasks.bus.amqp.AmqpInterlocutor;
 import org.icij.datashare.mode.CommonMode;
+import org.redisson.Redisson;
+import org.redisson.RedissonMap;
 import org.redisson.api.RedissonClient;
+import org.redisson.command.CommandSyncService;
+import org.redisson.liveobject.core.RedissonObjectBuilder;
 
 @Singleton
 public class TaskManagerAmqp extends org.icij.datashare.asynctasks.TaskManagerAmqp {
@@ -17,6 +23,19 @@ public class TaskManagerAmqp extends org.icij.datashare.asynctasks.TaskManagerAm
     }
 
     TaskManagerAmqp(AmqpInterlocutor amqp, RedissonClient redissonClient, Runnable eventCallback)  throws IOException {
-        super(amqp, redissonClient, CommonMode.DS_TASK_MANAGER_QUEUE_NAME, eventCallback);
+        // We start with a fresh list of known task everytime, we could decide to allow inheriting
+        // existing tasks
+        super(amqp, createTaskQueue(redissonClient), eventCallback);
+    }
+
+    private static RedissonMap<String, TaskView<?>> createTaskQueue(RedissonClient redissonClient) {
+        return new RedissonMap<>(new TaskManagerRedis.TaskViewCodec(),
+            new CommandSyncService(((Redisson) redissonClient).getConnectionManager(),
+                new RedissonObjectBuilder(redissonClient)),
+            CommonMode.DS_TASK_MANAGER_QUEUE_NAME,
+            redissonClient,
+            null,
+            null
+        );
     }
 }
