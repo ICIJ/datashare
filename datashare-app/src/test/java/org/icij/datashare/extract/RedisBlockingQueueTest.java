edd34e0d19fedd429019f66d14a083c330e16076
Bruno Thomas <bthomas@icij.org>
2024-05-28 14:03:12 +0000

    chore: merge adjustments to be able to merge PR
    Co-authored-by: Maxime Vanza Lutonda <mvanzalu@users.noreply.github.com>

--- datashare-app/src/test/java/org/icij/datashare/extract/RedisBlockingQueueTest.java
+++ datashare-app/src/test/java/org/icij/datashare/extract/RedisBlockingQueueTest.java
@@ -1,7 +1,5 @@
 package org.icij.datashare.extract;
 
-import org.icij.datashare.PropertiesProvider;
-import org.icij.datashare.asynctasks.bus.redis.RedisBlockingQueue;
 import org.icij.extract.redis.RedissonClientFactory;
 import org.icij.task.Options;
 import org.junit.After;
@@ -17,7 +15,7 @@ import static org.fest.assertions.Assertions.assertThat;
 public class RedisBlockingQueueTest {
     Map<String, Object> redisConfig = Map.of("redisAddress", "redis://redis:6379", "redisPoolSize", "5");
     RedissonClient client = new RedissonClientFactory().withOptions(Options.from(redisConfig)).create();
-    RedisBlockingQueue<String> queue = new RedisBlockingQueue<>(client, CommonMode.DS_BATCHSEARCH_QUEUE_NAME);
+    RedisBlockingQueue<String> queue = new RedisBlockingQueue<>(client, "ds:tasks:queue:test");
 
     @Test
     public void test_redisson_connection(){
