73bea304011ca6c40eea1edbc20acfa1cceacfde
Bruno Thomas <bthomas@icij.org>
2024-06-05 10:01:46 +0000

    fix: add the cause to the RuntimeException

--- datashare-tasks/src/main/java/org/icij/datashare/asynctasks/UnknownTask.java
+++ datashare-tasks/src/main/java/org/icij/datashare/asynctasks/UnknownTask.java
@@ -1,10 +1,7 @@
 package org.icij.datashare.asynctasks;
 
 public class UnknownTask extends RuntimeException {
-    private final ReflectiveOperationException exc;
-
     public UnknownTask(String s, ReflectiveOperationException source) {
-        super(s);
-        this.exc = source;
+        super(s, source);
     }
 }
