757e1de27e1529be356a51cf863dba55bf065b4f
Bruno Thomas <bthomas@icij.org>
2024-06-17 14:03:21 +0000

    fix: remove module-info errors when loading extensions #1387

---
+++ datashare-api/src/main/java/org/icij/datashare/test/LogbackAppenderWrapper.java
@@ -0,0 +1,70 @@
+package org.icij.datashare.test;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.Context;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogbackAppenderWrapper {
+    public final Logger logger;
+    private final TestAppender appender;
+
+    public LogbackAppenderWrapper() {
+        appender = new TestAppender();
+        logger = createLogger(appender);
+    }
+
+    List<String> logs(Level level) {
+        return appender.events.stream().filter(e -> e.getLevel() == level).map(ILoggingEvent::getFormattedMessage).collect(Collectors.toList());
+    }
+
+    public List<String> logs(org.slf4j.event.Level level) {
+        return logs(Level.valueOf(level.toString()));
+    }
+
+    public List<String> logs() {
+        return appender.events.stream().map(ILoggingEvent::getFormattedMessage).collect(Collectors.toList());
+    }
+
+    private static Logger createLogger(AppenderBase<ILoggingEvent> appender) {
+        ILoggerFactory lc = LoggerFactory.getILoggerFactory();
+        PatternLayoutEncoder ple = new PatternLayoutEncoder();
+
+        ple.setPattern("%d [%thread] %-5level %logger{0} - %msg%n");
+        ple.setContext((Context) lc);
+        ple.start();
+        appender.setContext((Context) lc);
+        appender.start();
+
+        ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+        logger.addAppender(appender);
+        logger.setLevel(Level.DEBUG);
+        logger.setAdditive(true);
+
+        return logger;
+    }
+
+    public void reset() {
+        appender.reset();
+    }
+
+    private static class TestAppender extends AppenderBase<ILoggingEvent> {
+        List<ILoggingEvent> events = new ArrayList<>();
+
+        @Override
+        protected void append(ILoggingEvent iLoggingEvent) {
+            events.add(iLoggingEvent);
+        }
+        void reset() {
+            events.clear();
+        }
+    }
+}
--- datashare-api/src/test/java/org/icij/datashare/extension/ExtensionLoaderTest.java
+++ datashare-api/src/test/java/org/icij/datashare/extension/ExtensionLoaderTest.java
@@ -1,8 +1,11 @@
 package org.icij.datashare.extension;
 
+import org.icij.datashare.test.LogbackAppenderWrapper;
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.slf4j.event.Level;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,6 +15,7 @@ import static org.icij.datashare.test.JarUtil.createJar;
 
 public class ExtensionLoaderTest {
     public @Rule TemporaryFolder folder = new TemporaryFolder();
+    private final LogbackAppenderWrapper logWrapper = new LogbackAppenderWrapper();
 
     @Test
     public void test_load_jars() throws Exception {
@@ -22,13 +26,26 @@ public class ExtensionLoaderTest {
         assertThat(new ExtensionLoader(extensionsDir).getJars()).containsOnly(extensionsDir.resolve("executable.jar").toFile());
     }
 
-    @Test(timeout = 5000)
+    @Test(timeout = 50000)
     public void test_no_static_initializers() throws Exception {
         Path extensionsDir = folder.getRoot().toPath();
         createJar(extensionsDir,"extension",INFINITE_INITIALIZER_LOOP_SOURCE);
         extensionsDir.resolve("extension.jar").toFile().setExecutable(true);
 
         new ExtensionLoader(extensionsDir).load(c -> {},c -> true); // should not timeout
+    }
+
+    @Test
+    public void test_module_info_should_not_log_errors() throws Exception {
+        Path extensionsDir = Paths.get(getClass().getResource("/extensions").toURI());
+        new ExtensionLoader(extensionsDir).findClassesInJar(aClass -> true, extensionsDir.resolve("executable.jar").toFile());
+        assertThat(logWrapper.logs(Level.WARN)).isEmpty();
+        assertThat(logWrapper.logs(Level.ERROR)).isEmpty();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        logWrapper.reset();
     }
 
     String INFINITE_INITIALIZER_LOOP_SOURCE = "package org.icij.datashare.extension;\n" +
