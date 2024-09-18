b82bbbd84f6f5f7d93e88e3c696c3e67d3cac790
Bruno Thomas <bthomas@icij.org>
2024-09-18 12:46:42 +0000

    feat: extraction of all artifacts #1534

--- datashare-app/src/test/java/org/icij/datashare/tasks/ArtifactTaskTest.java
+++ datashare-app/src/test/java/org/icij/datashare/tasks/ArtifactTaskTest.java
@@ -4,7 +4,6 @@ package org.icij.datashare.tasks;
 import org.icij.datashare.PropertiesProvider;
 import org.icij.datashare.asynctasks.Task;
 import org.icij.datashare.extract.MemoryDocumentCollectionFactory;
-import org.icij.datashare.function.Pair;
 import org.icij.datashare.text.indexing.Indexer;
 import org.icij.datashare.user.User;
 import org.icij.extract.queue.DocumentQueue;
@@ -15,6 +14,7 @@ import org.junit.rules.TemporaryFolder;
 import org.mockito.Mock;
 
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -24,7 +24,7 @@ public class ArtifactTaskTest {
     @Rule public TemporaryFolder artifactDir = new TemporaryFolder();
     @Mock Indexer mockEs;
     MockIndexer mockIndexer;
-    private final MemoryDocumentCollectionFactory<Pair<String, String>> factory = new MemoryDocumentCollectionFactory<>();
+    private final MemoryDocumentCollectionFactory<String> factory = new MemoryDocumentCollectionFactory<>();
 
     @Test(expected = IllegalArgumentException.class)
     public void test_missing_artifact_dir() {
@@ -34,14 +34,15 @@ public class ArtifactTaskTest {
     @Test
     public void test_create_artifact_cache_one_file() throws Exception {
         Path path = Path.of(getClass().getResource("/docs/embedded_doc.eml").getPath());
-        mockIndexer.indexFile("prj", "6abb96950946b62bb993307c8945c0c096982783bab7fa24901522426840ca3e", path, "application/pdf", "embedded_doc");
+        String sha256 = "0f95ef97e4619f7bae2a585c6cf24587cd7a3a81a26599c8774d669e5c175e5e";
+        mockIndexer.indexFile("prj", sha256, path, "message/rfc822");
 
-        DocumentQueue<Pair<String, String>> queue = factory.createQueue("extract:queue:artifact", (Class<Pair<String, String>>)(Object)Pair.class);
-        queue.add(new Pair<>("6abb96950946b62bb993307c8945c0c096982783bab7fa24901522426840ca3e", "embedded_doc"));
-        queue.add(new Pair<>("POISON", "POISON"));
+        DocumentQueue<String> queue = factory.createQueue("extract:queue:artifact", String.class);
+        queue.add(sha256);
+        queue.add("POISON");
 
-        Long numberOfDocuments = new ArtifactTask(factory, mockEs, new PropertiesProvider(Map.of()),
-                new Task<>(ArtifactTask.class.getName(), User.local(), Map.of("artifactDir", artifactDir.getRoot().toString(), "defaultProject", "prj")), null)
+        Long numberOfDocuments = new ArtifactTask(factory, mockEs, new PropertiesProvider(Map.of("artifactDir", artifactDir.getRoot().toString(), "defaultProject", "prj")),
+                new Task<>(ArtifactTask.class.getName(), User.local(), new HashMap<>()), null)
                 .call();
 
         assertThat(numberOfDocuments).isEqualTo(1);
