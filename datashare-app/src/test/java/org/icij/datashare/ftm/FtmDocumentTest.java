2f316c6ec55b578797cce67ea41dd657888599a5
Bruno Thomas <bthomas@icij.org>
2024-11-05 10:54:09 +0000

    feat: add translation fields

---
+++ datashare-app/src/test/java/org/icij/datashare/ftm/FtmDocumentTest.java
@@ -0,0 +1,42 @@
+package org.icij.datashare.ftm;
+
+import org.icij.datashare.text.DocumentBuilder;
+import org.icij.datashare.text.Language;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class FtmDocumentTest  {
+    @Test
+    public void test_translated_language() {
+        FtmDocument doc = new FtmDocument(DocumentBuilder.createDoc("docId").with(Language.FRENCH).with("un doc en français").with(
+                List.of(Map.of(
+                        "translator", "ARGOS",
+                        "source_language", "FRENCH",
+                        "target_language", "ENGLISH",
+                        "content", "a french doc"))
+        ).build());
+        assertThat(doc.getTranslatedLanguage()).isEqualTo("ENGLISH");
+    }
+
+    @Test
+    public void test_translated_content() {
+        FtmDocument doc = new FtmDocument(DocumentBuilder.createDoc("docId").with(Language.FRENCH).with("un doc en français").with(
+                List.of(
+                        Map.of(
+                        "translator", "ARGOS",
+                        "source_language", "FRENCH",
+                        "target_language", "ENGLISH",
+                        "content", "a french"),
+                Map.of(
+                        "translator", "ARGOS",
+                        "source_language", "FRENCH",
+                        "target_language", "ENGLISH",
+                        "content", " doc"))
+        ).build());
+        assertThat(doc.getTranslatedText()).isEqualTo("a french doc");
+    }
+}
