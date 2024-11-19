088cd35e91c4e2681228f7c16460daa8a7fa86c7
Clément Doumouro <clement.doumouro@gmail.com>
2024-11-19 12:31:29 +0100

    feature: start worker process when starting `WebApp` in embedded mode

--- datashare-app/src/main/java/org/icij/datashare/WebApp.java
+++ datashare-app/src/main/java/org/icij/datashare/WebApp.java
@@ -1,5 +1,24 @@
 package org.icij.datashare;
 
+import static java.lang.Boolean.parseBoolean;
+import static java.lang.Integer.parseInt;
+import static org.icij.datashare.cli.DatashareCliOptions.BROWSER_OPEN_LINK_OPT;
+import static org.icij.datashare.cli.DatashareCliOptions.NLP_PARALLELISM_OPT;
+import static org.icij.datashare.utils.ProcessHandler.dumpPid;
+import static org.icij.datashare.utils.ProcessHandler.findPidPaths;
+import static org.icij.datashare.utils.ProcessHandler.isProcessRunning;
+import static org.icij.datashare.utils.ProcessHandler.killProcessById;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 import net.codestory.http.WebServer;
 import org.icij.datashare.asynctasks.TaskManager;
 import org.icij.datashare.asynctasks.bus.amqp.QpidAmqpServer;
@@ -8,31 +27,31 @@ import org.icij.datashare.batch.BatchSearchRepository;
 import org.icij.datashare.cli.DatashareCli;
 import org.icij.datashare.cli.Mode;
 import org.icij.datashare.cli.QueueType;
+import org.icij.datashare.json.JsonObjectMapper;
 import org.icij.datashare.mode.CommonMode;
+import org.icij.datashare.mode.EmbeddedMode;
 import org.icij.datashare.tasks.BatchSearchRunner;
 
-import java.awt.*;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.URI;
-import java.util.Properties;
-
-import static java.lang.Boolean.parseBoolean;
-import static java.lang.Integer.parseInt;
-import static org.icij.datashare.cli.DatashareCliOptions.BROWSER_OPEN_LINK_OPT;
-
 public class WebApp {
+    private static final int AMQP_PORT = 5672;
 
     public static void main(String[] args) throws Exception {
         start(new DatashareCli().parseArguments(args).properties);
     }
 
     static void start(Properties properties) throws Exception {
-        if (shouldStartQpid(properties)) {
+        if (isEmbeddedAMQP(properties)) {
             // before creating mode because AmqpInterlocutor will try to connect the broker
-            new QpidAmqpServer(5672).start();
+            new QpidAmqpServer(AMQP_PORT).start();
         }
         CommonMode mode = CommonMode.create(properties);
+        Process nlpWorkerProcess = null;
+        Path nlpWorkersPidPath = null;
+        if (isEmbeddedAMQP(properties)) {
+            nlpWorkerProcess = startNlpWorkers((EmbeddedMode) mode, true);
+            nlpWorkersPidPath = Files.createTempFile("datashare-spacy-worker-", ".pid");
+            dumpPid(nlpWorkersPidPath.toFile(), nlpWorkerProcess.pid());
+        }
 
         Thread webServerThread = new Thread(() ->
                 new WebServer()
@@ -49,10 +68,17 @@ public class WebApp {
             Desktop.getDesktop().browse(URI.create(new URI("http://localhost:")+mode.properties().getProperty(PropertiesProvider.TCP_LISTEN_PORT)));
         }
         requeueDatabaseBatchSearches(mode.get(BatchSearchRepository.class), mode.get(TaskManager.class));
-        webServerThread.join();
+        try {
+            webServerThread.join();
+        } finally {
+            if (nlpWorkerProcess != null && nlpWorkerProcess.isAlive()) {
+                killProcessById(nlpWorkerProcess.pid());
+                Files.deleteIfExists(nlpWorkersPidPath);
+            }
+        }
     }
 
-    private static boolean shouldStartQpid(Properties properties) {
+    private static boolean isEmbeddedAMQP(Properties properties) {
         return CommonMode.getMode(properties) == Mode.EMBEDDED && properties.containsValue(QueueType.AMQP.name());
     }
 
@@ -79,5 +105,52 @@ public class WebApp {
         } catch (IOException ignored) {
             return false;
         }
+    }
+
+    protected static Process startNlpWorkers(ExecutableExtensionHelper extensionHelper, int nWorkers, boolean inheritIO) throws IOException {
+        Path tmpRoot = Path.of(System.getProperty("java.io.tmpdir"));
+        for (Path p: findPidPaths("regex:" + extensionHelper.getPidFilePattern(), tmpRoot)) {
+            if (isProcessRunning(p, 1, TimeUnit.SECONDS)) {
+                    String pid;
+                    try {
+                        pid = Files.readAllLines(p).get(0);
+                    } catch (IOException e) {
+                        throw new RuntimeException("failed to read pid from " + p);
+                    }
+                    String msg = "found phantom worker running in process " + pid
+                        + ", kill this process before restarting datashare !";
+                    throw new RuntimeException(msg);
+                }
+                try {
+                    Files.deleteIfExists(p);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+        }
+        // If not we start them
+        Path workerConfigPath = dumpNlpWorkerConfig();
+        return extensionHelper.executeExtension(inheritIO, workerConfigPath.toString(), "-n", String.valueOf(nWorkers));
+    }
+
+    private static Process startNlpWorkers(EmbeddedMode mode, boolean inheritIO) throws IOException {
+        PropertiesProvider propertiesProvider = mode.get(PropertiesProvider.class);
+        ExecutableExtensionHelper nlpExtHelper = new ExecutableExtensionHelper(
+            propertiesProvider, mode.get(ExtensionService.class), "^datashare-spacy-worker-[\\d\\.]+$"
+        );
+        int nWorkers = mode.get(PropertiesProvider.class).get(NLP_PARALLELISM_OPT).map(Integer::parseInt).orElse(1);
+        return startNlpWorkers(nlpExtHelper, nWorkers, inheritIO);
+    }
+
+    private static Path dumpNlpWorkerConfig() throws IOException {
+        Map<String, String> workerConfig = Map.of(
+            "type", "amqp",
+            "rabbitmq_host", "localhost",
+            "rabbitmq_port", String.valueOf(AMQP_PORT)
+        );
+        Path workerConfigPath = Files.createTempFile("datashare-spacy-worker-config-", ".json");
+        File tempFile = workerConfigPath.toFile();
+        // Write the JSON object to the temporary file
+        JsonObjectMapper.MAPPER.writeValue(tempFile, workerConfig);
+        return workerConfigPath;
     }
 }
