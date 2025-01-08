e6e58fb5a94a1b6b22fd3f3e49a5a5630367f0b8
mvanzalu <maximevanza@live.fr>
2025-01-08 13:07:22 +0000

    test: refactor mock result #1535

--- datashare-tasks/src/main/java/org/icij/datashare/asynctasks/TaskManager.java
+++ datashare-tasks/src/main/java/org/icij/datashare/asynctasks/TaskManager.java
@@ -16,7 +16,6 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
