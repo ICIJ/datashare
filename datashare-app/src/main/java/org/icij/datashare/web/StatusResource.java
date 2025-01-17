fb47d47d8b45712e957b4dcc8c2cb5172fa29fed
mvanzalu <maximevanza@live.fr>
2025-01-17 18:29:49 +0000

    feat: include TaskManager in TaskResource #1638

--- datashare-app/src/main/java/org/icij/datashare/web/StatusResource.java
+++ datashare-app/src/main/java/org/icij/datashare/web/StatusResource.java
@@ -14,6 +14,7 @@ import net.codestory.http.constants.HttpStatus;
 import net.codestory.http.payload.Payload;
 import org.icij.datashare.PropertiesProvider;
 import org.icij.datashare.Repository;
+import org.icij.datashare.asynctasks.TaskManager;
 import org.icij.datashare.openmetrics.StatusMapper;
 import org.icij.datashare.extract.DocumentCollectionFactory;
 import org.icij.datashare.text.indexing.Indexer;
@@ -26,16 +27,17 @@ import java.nio.file.Path;
 @Singleton
 @Prefix("/api")
 public class StatusResource {
-    Logger logger = LoggerFactory.getLogger(getClass());
     private final PropertiesProvider propertiesProvider;
     private final Repository repository;
     private final Indexer indexer;
+    private final TaskManager taskManager;
 
     @Inject
-    public StatusResource(PropertiesProvider propertiesProvider, Repository repository, Indexer indexer) {
+    public StatusResource(PropertiesProvider propertiesProvider, Repository repository, Indexer indexer, TaskManager taskManager) {
         this.propertiesProvider = propertiesProvider;
         this.repository = repository;
         this.indexer = indexer;
+        this.taskManager = taskManager;
     }
 
     @Operation(description = "Retrieve the status of databus connection, database connection and index.",
@@ -45,7 +47,7 @@ public class StatusResource {
     @ApiResponse(responseCode = "503", description = "service unavailable when other services are down", useReturnTypeSchema = true)
     @Get("/status")
     public Payload getStatus(Context context) {
-        Status status = new Status(repository.getHealth(), indexer.getHealth());
+        Status status = new Status(repository.getHealth(), indexer.getHealth(), taskManager.getHealth());
         if ("openmetrics".equals(context.request().query().get("format"))) {
             return new Payload("text/plain;version=0.0.4",
                     new StatusMapper("datashare", status, propertiesProvider.get("platform").orElse(null)).toString());
@@ -57,17 +59,19 @@ public class StatusResource {
     public static class Status {
         public final boolean database;
         public final boolean index;
+        public final boolean taskManager;
 
-        Status(boolean database, boolean index) {
+        Status(boolean database, boolean index, boolean taskManager) {
             this.database = database;
             this.index = index;
+            this.taskManager = taskManager;
         }
 
         @JsonIgnore
         int getHttpStatus() {
             if (!index) {
                 return HttpStatus.GATEWAY_TIMEOUT;
-            } else if (!database) {
+            } else if (!database || !taskManager) {
                 return HttpStatus.SERVICE_UNAVAILABLE;
             }
             return HttpStatus.OK;
