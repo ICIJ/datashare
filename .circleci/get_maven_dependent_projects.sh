a5dda7d67faf90ffc43a31680a61a7c893c90dcb
mvanzalu <maximevanza@live.fr>
2025-03-03 14:00:45 +0000

    ci: check if datashare-api is deployed
    when deploying datashare-tasks
    Co-authored-by: Bruno Thomas <bamthomas@users.noreply.github.com>

--- .circleci/get_maven_dependent_projects.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]; then
-  echo "Usage: $0 <project_name>"
-  exit 1
-fi
-
-mvn clean install -pl "$1" -am -s "$(dirname "${BASH_SOURCE[0]}")/maven-release-settings.xml" -Dexec.executable='echo' -Dmaven.test.skip=true -Dexec.args='${project.artifactId} ${project.version}' exec:exec -q 2>&1 | grep -v '\[.*\]' | head -n -1
