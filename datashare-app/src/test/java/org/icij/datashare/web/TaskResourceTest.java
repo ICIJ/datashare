0d93caab54d432cbe53bfc3a83b75942ba5f2612
Pierre Romera <hello@pirhoo.com>
2025-05-06 08:25:45 +0000

    spec: add missing count test

--- datashare-app/src/test/java/org/icij/datashare/web/TaskResourceTest.java
+++ datashare-app/src/test/java/org/icij/datashare/web/TaskResourceTest.java
@@ -130,6 +130,7 @@ public class TaskResourceTest extends AbstractProdWebServerTest {
         post("/api/task/batchUpdate/index/" + subpath, body).should().haveType("application/json");
 
         get("/api/task?size=2&from=1").should().haveType("application/json")
+                .contain("\"count\":2")
                 .contain("\"total\":4")
                 .contain("\"from\":1")
                 .contain("\"size\":2");
