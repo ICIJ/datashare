2192f51b399874bc3e0240369fb3311feb1f0d59
mvanzalu <maximevanza@live.fr>
2025-05-14 16:32:52 +0000

    feat: add function to register sub types to repository mapper

--- datashare-tasks/src/main/java/org/icij/datashare/asynctasks/TaskRepository.java
+++ datashare-tasks/src/main/java/org/icij/datashare/asynctasks/TaskRepository.java
@@ -1,10 +1,15 @@
 package org.icij.datashare.asynctasks;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.icij.datashare.json.JsonObjectMapper;
+
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.stream.Stream;
 
 public interface TaskRepository {
+    ObjectMapper TYPE_INCLUSION_MAPPER = JsonObjectMapper.createTypeInclusionMapper();
+
     <V extends Serializable> void insert(Task<V> task, Group group) throws IOException, TaskAlreadyExists;
 
     <V extends Serializable> Task<V> getTask(String taskId) throws IOException, UnknownTask;
@@ -19,4 +24,8 @@ public interface TaskRepository {
 
 
     Stream<Task<? extends Serializable>> getTasks(TaskFilters filters) throws IOException, UnknownTask;
+
+    default void registerTaskResultTypes(Class<?> ...classesToRegister) {
+        TYPE_INCLUSION_MAPPER.registerSubtypes(classesToRegister);
+    }
 }
