044447fd58cb4095917a5631d19f475c9d291b6b
mvanzalu <maximevanza@live.fr>
2025-07-02 10:37:40 +0000

    Revert "refactor: remove sub types definition in TaskResult #1687"
    This reverts commit 4b2b63c823d4accc1a1b46e3daaa27e14340fabb.

--- datashare-tasks/src/main/java/org/icij/datashare/asynctasks/TaskResult.java
+++ datashare-tasks/src/main/java/org/icij/datashare/asynctasks/TaskResult.java
@@ -1,11 +1,17 @@
 package org.icij.datashare.asynctasks;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.icij.datashare.asynctasks.bus.amqp.UriResult;
 
 import java.io.Serializable;
 
 
 public record TaskResult<V extends Serializable> (
+        @JsonSubTypes({
+                @JsonSubTypes.Type(value = UriResult.class),
+                @JsonSubTypes.Type(value = Long.class)
+        })
         @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "@type")
         V value) implements Serializable {
     public TaskResult(V value) {
