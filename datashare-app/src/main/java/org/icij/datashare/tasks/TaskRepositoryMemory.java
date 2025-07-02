53c2580156e519446e218c97df141092c20cfc5b
mvanzalu <maximevanza@live.fr>
2025-07-02 09:23:14 +0000

    refactor: optimize imports #1687

--- datashare-app/src/main/java/org/icij/datashare/tasks/TaskRepositoryMemory.java
+++ datashare-app/src/main/java/org/icij/datashare/tasks/TaskRepositoryMemory.java
@@ -2,7 +2,6 @@ package org.icij.datashare.tasks;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import org.redisson.api.RedissonClient;
 
 @Singleton
 public class TaskRepositoryMemory extends org.icij.datashare.asynctasks.TaskRepositoryMemory{
