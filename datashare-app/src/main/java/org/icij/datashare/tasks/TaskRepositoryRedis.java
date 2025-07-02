9e3d78f8a5ec46c9efc43398da37cdc3ac6a659d
mvanzalu <maximevanza@live.fr>
2025-07-02 09:23:14 +0000

    refactor: optimize imports #1687

--- datashare-app/src/main/java/org/icij/datashare/tasks/TaskRepositoryRedis.java
+++ datashare-app/src/main/java/org/icij/datashare/tasks/TaskRepositoryRedis.java
@@ -2,7 +2,6 @@ package org.icij.datashare.tasks;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import org.icij.datashare.asynctasks.TaskResult;
 import org.redisson.api.RedissonClient;
 
 @Singleton
