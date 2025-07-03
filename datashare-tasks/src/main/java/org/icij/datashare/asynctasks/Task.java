6c3c50041a028848199af724da129f7051350ec6
mvanzalu <maximevanza@live.fr>
2025-07-03 15:41:18 +0000

    feat: adds JsonUnwrapped on TaskResult field in Task  #1687

--- datashare-tasks/src/main/java/org/icij/datashare/asynctasks/Task.java
+++ datashare-tasks/src/main/java/org/icij/datashare/asynctasks/Task.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -53,6 +54,7 @@ public class Task<V extends Serializable> extends Event implements Entity, Compa
     private volatile State state;
     private volatile Date completedAt;
     private volatile double progress;
+    @JsonUnwrapped
     private volatile TaskResult<V> result;
 
     public Task(String name, User user, Map<String, Object> args) {
