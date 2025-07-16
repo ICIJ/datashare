7d7af4da883e5f604acd9386eed0013692185592
Bruno Thomas <bthomas@icij.org>
2025-07-16 12:54:54 +0000

    ci: test publish db doc
    Co-authored-by: Maxime Vanza Lutonda <mvanzalu@users.noreply.github.com>

--- .circleci/publish_doc_if_necessary.sh
+++ .circleci/publish_doc_if_necessary.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-web_package_has_changed=$(git diff-index --exit-code HEAD~1 datashare-app/src/main/java/org/icij/datashare/web/)
 db_package_has_changed=$(git diff-index --exit-code HEAD~1 datashare-db/src/main/resources/liquibase/)
 CIRCLE_API="https://circleci.com/api"
 
-if [[ -n "$web_package_has_changed" || -n "$db_package_has_changed" ]]; then
-  echo >&2 "Something changed in web package"
+if [[ -n "$db_package_has_changed" ]]; then
+  echo >&2 "Something changed in db package"
   URL="${CIRCLE_API}/v2/project/github/ICIJ/datashare/pipeline"
   DATA='{"branch": "main", "parameters": { "publish_backend_doc": true}}'
   HTTP_RESPONSE=$(curl -s -u "${CIRCLE_API_USER_TOKEN}:" -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" "$URL")
