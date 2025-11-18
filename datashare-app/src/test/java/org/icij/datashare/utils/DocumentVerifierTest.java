ef1e4be050ec1db195105cc8df60f1ee0f844310
mvanzalu <maximevanza@live.fr>
2025-11-18 15:43:42 +0000

    fix: checks duplicate document size

--- datashare-app/src/test/java/org/icij/datashare/utils/DocumentVerifierTest.java
+++ datashare-app/src/test/java/org/icij/datashare/utils/DocumentVerifierTest.java
@@ -3,6 +3,7 @@ package org.icij.datashare.utils;
 import org.icij.datashare.PropertiesProvider;
 import org.icij.datashare.text.Document;
 import org.icij.datashare.text.DocumentBuilder;
+import org.icij.datashare.text.Duplicate;
 import org.icij.datashare.text.Project;
 import org.icij.datashare.text.indexing.Indexer;
 import org.junit.Test;
@@ -41,6 +42,20 @@ public class DocumentVerifierTest {
         Document doc = DocumentBuilder.createDoc("foo").with(project).withParentId("bar").withRootId("bar").build();
 
         when(indexer.get(project.getId(), "bar")).thenReturn(rootDoc);
+        when(propertiesProvider.get(EMBEDDED_DOCUMENT_DOWNLOAD_MAX_SIZE_OPT)).thenReturn(Optional.of("200G"));
+
+        assertTrue(documentVerifier.isRootDocumentSizeAllowed(doc));
+    }
+
+    @Test
+    public void test_is_root_document_duplicate() {
+        Project project = new Project("local-datashare");
+        Document rootDoc = DocumentBuilder.createDoc("bar").with(project).withContentLength(1024).build();
+        Duplicate dup = new Duplicate(rootDoc.getPath(), rootDoc.getId());
+        Document doc = DocumentBuilder.createDoc("foo").with(project).withParentId(dup.getId()).withRootId(dup.getId()).build();
+
+        when(indexer.get(project.getId(), "bar")).thenReturn(rootDoc);
+        when(indexer.get(project.getId(), dup.getId())).thenReturn(dup);
         when(propertiesProvider.get(EMBEDDED_DOCUMENT_DOWNLOAD_MAX_SIZE_OPT)).thenReturn(Optional.of("200G"));
 
         assertTrue(documentVerifier.isRootDocumentSizeAllowed(doc));
