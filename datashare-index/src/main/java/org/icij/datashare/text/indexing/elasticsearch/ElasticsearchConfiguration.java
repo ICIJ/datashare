16f19325b055fa3bfe3208286f5f0ad75c522889
Bruno Thomas <bthomas@icij.org>
2026-01-13 17:55:38 +0000

    fix: with configuration for embedded opensearch #1951

--- datashare-index/src/main/java/org/icij/datashare/text/indexing/elasticsearch/ElasticsearchConfiguration.java
+++ datashare-index/src/main/java/org/icij/datashare/text/indexing/elasticsearch/ElasticsearchConfiguration.java
@@ -82,6 +82,10 @@ public class ElasticsearchConfiguration {
 
             RestClientBuilder.HttpClientConfigCallback clientConfigCallback = httpAsyncClientBuilder -> {
                 httpAsyncClientBuilder.disableAuthCaching();
+                if (propertiesProvider.get("mode").orElse("LOCAL").equals("EMBEDDED")) {
+                    httpAsyncClientBuilder.setDefaultHeaders(
+                            singletonList(new BasicHeader("Content-type", "application/json")));
+                }
                 httpAsyncClientBuilder.addInterceptorLast((HttpResponseInterceptor)
                         (response, context) ->
                                 // This header is expected from the client, versions of ES server below 7.14 don't provide it
