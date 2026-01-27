0732fb992ca4ff4529a6aeeb428fdae852d9f961
Bruno Thomas <bthomas@icij.org>
2026-01-27 16:48:16 +0000

    feat: add a Process class to launch a command with stdout/err #2017
    Co-authored-by: Maxime Vanza Lutonda <mvanzalu@users.noreply.github.com>

---
+++ datashare-app/src/main/java/org/icij/datashare/process/Process.java
@@ -0,0 +1,125 @@
+package org.icij.datashare.process;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Class to launch a process originally from
+ * cf <a href="https://github.com/owenthereal/gaffer">owenthereal/gaffer</a>
+ */
+public class Process {
+    private final class StdoutRunnable implements Runnable {
+        private final InputStream src;
+
+        private StdoutRunnable(final InputStream src) {
+            this.src = src;
+        }
+
+        @Override
+        public void run() {
+            final Scanner sc = new Scanner(src);
+            try {
+                while (sc.hasNextLine()) {
+                    logger.info(sc.nextLine());
+                }
+            } finally {
+                sc.close();
+            }
+        }
+    }
+
+    private final String dir;
+    private final String name;
+    private final String[] cmd;
+
+    private java.lang.Process p;
+    private final Logger logger;
+    private final ExecutorService pool;
+    private final int port;
+
+    public Process(final String dir, final String name, final String[] cmd, final int port) {
+        this.dir = dir;
+        this.name = name;
+        this.cmd = cmd;
+        this.port = port;
+        this.logger = LoggerFactory.getLogger(getName());
+        this.pool = Executors.newCachedThreadPool();
+    }
+
+    public void start() throws ProcessException {
+        final ProcessBuilder pb = new ProcessBuilder(cmd);
+
+        final Map<String, String> env = pb.environment();
+        env.put("PORT", String.valueOf(port));
+
+        pb.directory(new File(dir));
+        pb.redirectInput(Redirect.INHERIT);
+        pb.redirectErrorStream();
+
+        try {
+            p = pb.start();
+            pool.execute(new StdoutRunnable(new BufferedInputStream(p.getInputStream())));
+            pool.execute(new StdoutRunnable(new BufferedInputStream(p.getErrorStream())));
+        } catch (final Exception e) {
+            logger.error(e.getMessage());
+            throw new ProcessException(e.getMessage());
+        } finally {
+            pool.shutdown();
+        }
+    }
+
+    public void waitFor() throws ProcessException {
+        if (isAlive()) {
+            try {
+                p.waitFor();
+            } catch (final InterruptedException e) {
+                throw new ProcessException(e.getMessage());
+            }
+        }
+    }
+
+    public boolean isAlive() {
+        if (p == null) {
+            return false;
+        }
+
+        try {
+            p.exitValue();
+        } catch (final IllegalThreadStateException e) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean exitWithError() {
+        return !isAlive() && p.exitValue() != 0;
+    }
+
+    public void kill() {
+        if (isAlive()) {
+            p.destroy();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public static void main(String[] args) {
+        new Process(System.getProperty("user.dir"), "java.Process", args, 12345).start();
+    }
+}
---
+++ datashare-app/src/main/java/org/icij/datashare/process/ProcessException.java
@@ -0,0 +1,7 @@
+package org.icij.datashare.process;
+
+public class ProcessException extends RuntimeException {
+   public ProcessException(String message) {
+        super(message);
+    }
+}
