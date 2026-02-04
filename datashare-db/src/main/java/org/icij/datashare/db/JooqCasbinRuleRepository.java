b1e3f0437f7c9c848baba703371c9e88d0d785b2
Caroline Desprat <cdesprat@icij.org>
2026-02-04 10:51:25 +0000

    feat: implement Authorizer class and update Casbin rule repository methods

--- datashare-db/src/main/java/org/icij/datashare/db/JooqCasbinRuleRepository.java
+++ datashare-db/src/main/java/org/icij/datashare/db/JooqCasbinRuleRepository.java
@@ -21,41 +21,59 @@ public class JooqCasbinRuleRepository implements CasbinRuleRepository {
         this.dialect = dialect;
     }
 
+    @Override
     public void rebuildCasbinRules() {
         DSLContext ctx = using(connectionProvider, dialect);
 
-        // wipe execution table
-        ctx.deleteFrom(CASBIN_RULE).execute();
+        ctx.transaction(configuration -> {
+            DSLContext tCtx = using(configuration);
+            // wipe execution table
+            tCtx.deleteFrom(CASBIN_RULE).execute();
 
-        // g: user -> role -> domain
-        ctx.selectFrom(USER_DOMAIN_ROLE)
-                .fetch()
-                .forEach(r ->
-                        ctx.insertInto(CASBIN_RULE)
-                                .set(CASBIN_RULE.PTYPE, "g")
-                                .set(CASBIN_RULE.V0, r.getUserId().toString())
-                                .set(CASBIN_RULE.V1, r.getRole())
-                                .set(CASBIN_RULE.V2, r.getDomainId().toString())
-                                .execute()
-                );
+            // g: user -> role -> domain
+            tCtx.selectFrom(USER_DOMAIN_ROLE)
+                    .fetch()
+                    .forEach(r ->
+                            tCtx.insertInto(CASBIN_RULE)
+                                    .set(CASBIN_RULE.PTYPE, "g")
+                                    .set(CASBIN_RULE.V0, r.getUserId().toString())
+                                    .set(CASBIN_RULE.V1, r.getRole())
+                                    .set(CASBIN_RULE.V2, r.getDomainId().toString())
+                                    .execute()
+                    );
 
-        // p: role -> domain -> project -> action
-        ctx.selectFrom(ROLE_PROJECT_PERMISSION)
-                .fetch()
-                .forEach(r ->
-                        ctx.insertInto(CASBIN_RULE)
-                                .set(CASBIN_RULE.PTYPE, "p")
-                                .set(CASBIN_RULE.V0, r.getRole())
-                                .set(CASBIN_RULE.V1, r.getDomainId().toString())
-                                .set(CASBIN_RULE.V2, r.getProjectId().toString())
-                                .set(CASBIN_RULE.V3, r.getAction())
-                                .execute()
-                );
+            // p: role -> domain -> project -> action
+            tCtx.selectFrom(ROLE_PROJECT_PERMISSION)
+                    .fetch()
+                    .forEach(r ->
+                            tCtx.insertInto(CASBIN_RULE)
+                                    .set(CASBIN_RULE.PTYPE, "p")
+                                    .set(CASBIN_RULE.V0, r.getRole())
+                                    .set(CASBIN_RULE.V1, r.getDomainId().toString())
+                                    .set(CASBIN_RULE.V2, r.getProjectId().toString())
+                                    .set(CASBIN_RULE.V3, r.getAction())
+                                    .execute()
+                    );
+        });
     }
 
     @Override
-    public void loadPolicy(Model var1) {
-
+    public void loadPolicy(Model model) {
+        DSLContext ctx = using(connectionProvider, dialect);
+        ctx.selectFrom(CASBIN_RULE)
+                .fetch()
+                .forEach(r -> {
+                    String ptype = r.getPtype();
+                    String sec = ptype.substring(0, 1);
+                    java.util.List<String> rule = new java.util.ArrayList<>();
+                    if (r.getV0() != null) rule.add(r.getV0());
+                    if (r.getV1() != null) rule.add(r.getV1());
+                    if (r.getV2() != null) rule.add(r.getV2());
+                    if (r.getV3() != null) rule.add(r.getV3());
+                    if (r.getV4() != null) rule.add(r.getV4());
+                    if (r.getV5() != null) rule.add(r.getV5());
+                    model.addPolicy(sec, ptype, rule);
+                });
     }
 
     @Override
