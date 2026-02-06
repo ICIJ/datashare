aa0ad430b0feb8fa00cf96378d5f100a71b154de
Clément Doumouro <clement.doumouro@gmail.com>
2026-02-06 17:31:31 +0100

    refactor(datashare-tasks): rename `TaskManager.getTaskStates` into `TaskManager.getTaskIds`

--- datashare-tasks/src/main/java/org/icij/datashare/asynctasks/TaskManager.java
+++ datashare-tasks/src/main/java/org/icij/datashare/asynctasks/TaskManager.java
@@ -25,7 +25,7 @@ public interface TaskManager extends Closeable {
     int POLLING_INTERVAL = 5000;
 
     Logger logger = LoggerFactory.getLogger(TaskManager.class);
-    <V extends Serializable> String startTask(Task<V> taskView, Group group) throws IOException, TaskAlreadyExists;
+    <V extends Serializable> String     startTask(Task<V> taskView, Group group) throws IOException, TaskAlreadyExists;
 
     <V extends Serializable> Task<V> getTask(String taskId) throws IOException, UnknownTask;
     <V extends Serializable> Task<V> clearTask(String taskId) throws IOException, UnknownTask;
