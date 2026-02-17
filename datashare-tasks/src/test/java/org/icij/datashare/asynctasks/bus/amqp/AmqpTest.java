92f7be9a7931f761f34cf7d8aff5a16c315edd58
Pierre Romera <hello@pirhoo.com>
2026-02-17 15:22:49 +0000

    refactor: reduce test flakines for amqp

--- datashare-tasks/src/test/java/org/icij/datashare/asynctasks/bus/amqp/AmqpTest.java
+++ datashare-tasks/src/test/java/org/icij/datashare/asynctasks/bus/amqp/AmqpTest.java
@@ -114,10 +114,9 @@ public class AmqpTest {
     @Test
     public void test_consume_nack_without_requeue() throws Exception {
         NackExceptionConsumer nackExceptionConsumer = new NackExceptionConsumer(false);
-        new AmqpConsumer<>(amqp, nackExceptionConsumer, AmqpQueue.EVENT, TestEvent.class ).consumeEvents(1);
-        new AmqpConsumer<>(amqp, new TestEventConsumer(), AmqpQueue.MANAGER_EVENT, TestEvent.class).consumeEvents(1);
+        new AmqpConsumer<>(amqp, nackExceptionConsumer, AmqpQueue.MANAGER_EVENT, TestEvent.class).consumeEvents(1);
 
-        amqp.publish(AmqpQueue.EVENT, new TestEvent("boom no requeue !!"));
+        amqp.publish(AmqpQueue.MANAGER_EVENT, new TestEvent("boom no requeue !!"));
 
         TestEvent polled = eventQueue.poll(1, TimeUnit.SECONDS);
         String asString = Optional.ofNullable(polled).map(rethrowFunction(JsonObjectMapper::writeValueAsString)).orElse(null);
