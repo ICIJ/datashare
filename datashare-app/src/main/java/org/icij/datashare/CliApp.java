92ab96eef950dd986b37dc5c7101f491c00c70e1
Caroline Desprat <cdesprat@icij.org>
2026-02-20 11:22:38 +0000

    feat: refactor user and project policy handling with Domain integration

--- datashare-app/src/main/java/org/icij/datashare/CliApp.java
+++ datashare-app/src/main/java/org/icij/datashare/CliApp.java
@@ -16,6 +16,7 @@ import org.icij.datashare.tasks.ScanIndexTask;
 import org.icij.datashare.tasks.ScanTask;
 import org.icij.datashare.text.Project;
 import org.icij.datashare.text.indexing.Indexer;
+import org.icij.datashare.user.Domain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -101,7 +102,7 @@ class CliApp {
         if (properties.getProperty(GRANT_ADMIN_OPT) != null) {
             String userName = properties.getProperty(GRANT_ADMIN_OPT);
             String project = properties.getProperty(DEFAULT_PROJECT_OPT);
-            taskFactory.createGrantAdminPolicyTask(localUser(userName), Project.project(project)).call();
+            taskFactory.createGrantAdminPolicyTask(localUser(userName), Domain.DEFAULT, Project.project(project)).call();
             System.exit(0);
         }
 
