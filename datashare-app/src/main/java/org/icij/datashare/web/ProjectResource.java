4e4119db4a148a7e305ce93e67be668135e192fd
Caroline Desprat <cdesprat@icij.org>
2026-02-20 11:23:26 +0000

    feat: update ProjectResource and tests to use new Policy and Authorizer for role-based access control

--- datashare-app/src/main/java/org/icij/datashare/web/ProjectResource.java
+++ datashare-app/src/main/java/org/icij/datashare/web/ProjectResource.java
@@ -20,7 +20,7 @@
     import org.icij.datashare.cli.Mode;
     import org.icij.datashare.extract.DocumentCollectionFactory;
     import org.icij.datashare.session.DatashareUser;
-    import org.icij.datashare.session.ProjectPolicy;
+    import org.icij.datashare.session.Policy;
     import org.icij.datashare.tasks.DatashareTaskManager;
     import org.icij.datashare.text.Project;
     import org.icij.datashare.text.indexing.Indexer;
@@ -136,7 +136,7 @@
         @ApiResponse(responseCode = "404", description = "if project doesn't exist in database")
         @ApiResponse(responseCode = "500", description = "if project json id is not the same as the url id or if save failed")
         @Put("/:id")
-        @ProjectPolicy(roles = {Role.ADMIN}, idParam = "id")
+        @Policy(role = Role.PROJECT_ADMIN, idParam = "id")
         public Payload projectUpdate(String id, Project projectPayload, Context context) {
             if (isProjectNameEmpty(projectPayload)) {
                 return PayloadFormatter.error("`name` field is required.", HttpStatus.BAD_REQUEST);
