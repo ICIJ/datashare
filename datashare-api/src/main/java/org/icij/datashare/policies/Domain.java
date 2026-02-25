b17825588bec2503878d88f09ece26b7b76299b1
Caroline Desprat <cdesprat@icij.org>
2026-02-25 17:16:09 +0000

    feat: use default domain in admin policy tasks and checks

--- datashare-api/src/main/java/org/icij/datashare/policies/Domain.java
+++ datashare-api/src/main/java/org/icij/datashare/policies/Domain.java
@@ -5,4 +5,7 @@ public record Domain(String id) {
     public static Domain of(String id) {
         return new Domain(id);
     }
+
+    // this temporary while we don't handle multiple domains
+    public static final Domain DEFAULT = new Domain("default");
 }
