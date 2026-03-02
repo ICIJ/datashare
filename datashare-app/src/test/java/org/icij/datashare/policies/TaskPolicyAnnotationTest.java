40333122f71d79a3cdfa53490cbb7e003bcb4fc6
Caroline Desprat <cdesprat@icij.org>
2026-03-02 14:47:55 +0000

    test: refactor TaskPolicyAnnotationTest to remove unused user variables and improve test clarity

--- datashare-app/src/test/java/org/icij/datashare/policies/TaskPolicyAnnotationTest.java
+++ datashare-app/src/test/java/org/icij/datashare/policies/TaskPolicyAnnotationTest.java
@@ -1,7 +1,6 @@
 package org.icij.datashare.policies;
 
 import net.codestory.http.Context;
-import net.codestory.http.errors.UnauthorizedException;
 import net.codestory.http.payload.Payload;
 import org.icij.datashare.PropertiesProvider;
 import org.icij.datashare.asynctasks.TaskRepositoryMemory;
@@ -55,8 +54,6 @@ public class TaskPolicyAnnotationTest {
     @Mock
     CasbinRuleAdapter jooqCasbinRuleRepository;
     Authorizer authorizer;
-    private DatashareUser adminUser;
-    private DatashareUser nonAdminUser;
     private String taskId = "test-task";
     private String projectId = "test-datashare";
     private TaskPolicyAnnotation annotation;
@@ -64,8 +61,6 @@ public class TaskPolicyAnnotationTest {
     @Before
     public void setUp() {
         mocks = openMocks(this);
-        adminUser = new DatashareUser("cecile");
-        nonAdminUser = new DatashareUser("john");
         authorizer = new Authorizer(jooqCasbinRuleRepository);
         authorizer.addRoleForUserInProject("cecile", Role.PROJECT_ADMIN, Domain.DEFAULT, projectId);
         authorizer.addRoleForUserInProject("john", Role.PROJECT_MEMBER, Domain.DEFAULT, projectId);
@@ -73,16 +68,18 @@ public class TaskPolicyAnnotationTest {
     }
 
     @After
-    public void tearDown() throws IOException {
+    public void tearDown() throws Exception {
         taskManager.stopTasks(User.local());
         taskManager.clear();
+        mocks.close();
     }
 
-    @Test(expected = UnauthorizedException.class)
-    public void should_throw_unauthorized_if_no_user() {
+    @Test
+    public void should_return_forbidden_if_no_user() {
         Context context = mock(Context.class);
         when(context.pathParam("taskName:")).thenReturn(taskId);
-        annotation.apply(adminTaskPolicy, context, (c) -> Payload.ok());
+        Payload result = annotation.apply(adminTaskPolicy, context, c -> Payload.ok());
+        assertEquals(403, result.code());
     }
 
     @Test
