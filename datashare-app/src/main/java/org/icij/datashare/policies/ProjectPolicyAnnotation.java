661e2a123eb851fc9d60402698ee53e59cdaa53a
Caroline Desprat <cdesprat@icij.org>
2026-03-03 14:29:24 +0000

    refactor: remove AbstractPolicyAnnotation

--- datashare-app/src/main/java/org/icij/datashare/policies/ProjectPolicyAnnotation.java
+++ datashare-app/src/main/java/org/icij/datashare/policies/ProjectPolicyAnnotation.java
@@ -2,29 +2,33 @@ package org.icij.datashare.policies;
 
 import com.google.inject.Inject;
 import net.codestory.http.Context;
+import net.codestory.http.annotations.ApplyAroundAnnotation;
 import net.codestory.http.payload.Payload;
 import org.icij.datashare.session.DatashareUser;
 
 import java.util.function.Function;
 
+import static org.icij.datashare.policies.Authorizer.*;
 
-public class ProjectPolicyAnnotation extends AbstractPolicyAnnotation<ProjectPolicy> {
+
+public class ProjectPolicyAnnotation implements ApplyAroundAnnotation<ProjectPolicy> {
+    private final Authorizer authorizer;
 
     @Inject
     public ProjectPolicyAnnotation(Authorizer authorizer) {
-        super(authorizer);
+        this.authorizer = authorizer;
     }
 
     @Override
     public Payload apply(ProjectPolicy annotation, Context context, Function<Context, Payload> payloadSupplier) {
 
-        DatashareUser user = requireUser(context);
+        DatashareUser user = requireCurrentUser(context);
         //TODO #DOMAIN Currently Domain is not handled so we can't check it from query params
-        Domain domain = requireDomain(annotation.domain());
+        Domain domain = requireDomain(annotation.domain(), true);
 
         String projectId = requireIdParam(context, annotation.idParam());
 
-        if (isNotAllowed(user, domain, projectId, annotation.role())) {
+        if (!authorizer.can(user.id, domain, projectId, annotation.role())) {
             return Payload.forbidden();
         }
 
