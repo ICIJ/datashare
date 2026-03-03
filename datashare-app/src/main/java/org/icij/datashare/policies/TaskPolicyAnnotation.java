1faeb30807daadc90cf7242968045d268e22df16
Caroline Desprat <cdesprat@icij.org>
2026-03-03 14:29:24 +0000

    refactor: remove AbstractPolicyAnnotation

--- datashare-app/src/main/java/org/icij/datashare/policies/TaskPolicyAnnotation.java
+++ datashare-app/src/main/java/org/icij/datashare/policies/TaskPolicyAnnotation.java
@@ -2,6 +2,7 @@ package org.icij.datashare.policies;
 
 import com.google.inject.Inject;
 import net.codestory.http.Context;
+import net.codestory.http.annotations.ApplyAroundAnnotation;
 import net.codestory.http.payload.Payload;
 import org.icij.datashare.asynctasks.Task;
 import org.icij.datashare.asynctasks.UnknownTask;
@@ -17,14 +18,14 @@ import static java.util.Optional.ofNullable;
 import static org.icij.datashare.PropertiesProvider.DEFAULT_PROJECT_OPT;
 import static org.icij.datashare.cli.DatashareCliOptions.DEFAULT_DEFAULT_PROJECT;
 
-public class TaskPolicyAnnotation extends AbstractPolicyAnnotation<TaskPolicy> {
-
+public class TaskPolicyAnnotation implements ApplyAroundAnnotation<TaskPolicy> {
+    private final Authorizer authorizer;
     private final DatashareTaskManager taskManager;
 
     @Inject
     public TaskPolicyAnnotation(Authorizer authorizer,
                                 DatashareTaskManager taskManager) {
-        super(authorizer);
+        this.authorizer = authorizer;
         this.taskManager = taskManager;
     }
 
@@ -34,10 +35,10 @@ public class TaskPolicyAnnotation extends AbstractPolicyAnnotation<TaskPolicy> {
     @Override
     public Payload apply(TaskPolicy annotation, Context context, Function<Context, Payload> payloadSupplier) {
 
-        DatashareUser user = requireUser(context);
+        DatashareUser user = Authorizer.requireCurrentUser(context);
         //TODO #DOMAIN Currently Domain is not handled so we can't check it from query params
-        Domain domain = requireDomain(annotation.domain());
-        String taskId = requireIdParam(context, annotation.idParam());
+        Domain domain = Authorizer.requireDomain(annotation.domain(), true);
+        String taskId = Authorizer.requireIdParam(context, annotation.idParam());
 
         try {
             Task<Serializable> task = taskManager.getTask(taskId);
@@ -45,10 +46,10 @@ public class TaskPolicyAnnotation extends AbstractPolicyAnnotation<TaskPolicy> {
             // Tasks are linked to ONE project at a time
             // BatchSearches are not handled yet (they are linked to multiple projects...)
             Project project = Project.project(ofNullable((String) task.args.get(DEFAULT_PROJECT_OPT)).orElse(DEFAULT_DEFAULT_PROJECT));
-            String projectId = requireValue(project.name);
+            String projectId = Authorizer.requireValue(project.name, true);
 
             // Check if user as role based rights or is owner (if an annotation allowOwner flag is true)
-            if (isNotAllowed(user, domain, projectId, annotation.role()) && (!annotation.allowOwner() || !isTaskOwner(user, task))) {
+            if (!authorizer.can(user.id, domain, projectId, annotation.role()) && (!annotation.allowOwner() || !isTaskOwner(user, task))) {
                 return Payload.forbidden();
             }
             return payloadSupplier.apply(context);
