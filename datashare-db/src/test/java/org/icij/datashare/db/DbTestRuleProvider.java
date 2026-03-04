97f7e9d7d85f0991a01562e7c1547fb033397e2e
mvanzalu <maximevanza@live.fr>
2026-03-04 15:09:41 +0000

    test: add javadoc for DbTestRuleProvider

--- datashare-db/src/test/java/org/icij/datashare/db/DbTestRuleProvider.java
+++ datashare-db/src/test/java/org/icij/datashare/db/DbTestRuleProvider.java
@@ -2,6 +2,12 @@ package org.icij.datashare.db;
 
 import org.icij.datashare.EnvUtils;
 
+
+/**
+ * Singleton provider for shared DbSetupRule instances across all test classes.
+ *
+ * This provider ensures only 2 pools are created and reused by all test classes
+ */
 public class DbTestRuleProvider {
     private static final DbSetupRule SQLITE_RULE = new DbSetupRule("jdbc:sqlite:file:memorydb.db?mode=memory&cache=shared");
     private static final DbSetupRule POSTGRES_RULE = new DbSetupRule(
