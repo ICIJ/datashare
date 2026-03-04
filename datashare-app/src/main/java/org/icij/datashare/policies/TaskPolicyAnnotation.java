fd3b8b1d5b0d3ede6b0b7a98c48bd559d73d0711
Caroline Desprat <cdesprat@icij.org>
2026-03-04 14:21:46 +0000

    fix: throw exception if task has no project id in arguments

--- datashare-app/src/main/java/org/icij/datashare/policies/TaskPolicyAnnotation.java
+++ datashare-app/src/main/java/org/icij/datashare/policies/TaskPolicyAnnotation.java
@@ -8,7 +8,6 @@ import org.icij.datashare.asynctasks.Task;
 import org.icij.datashare.asynctasks.UnknownTask;
 import org.icij.datashare.session.DatashareUser;
 import org.icij.datashare.tasks.DatashareTaskManager;
-import org.icij.datashare.text.Project;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -16,7 +15,6 @@ import java.util.function.Function;
 
 import static java.util.Optional.ofNullable;
 import static org.icij.datashare.PropertiesProvider.DEFAULT_PROJECT_OPT;
-import static org.icij.datashare.cli.DatashareCliOptions.DEFAULT_DEFAULT_PROJECT;
 
 public class TaskPolicyAnnotation implements ApplyAroundAnnotation<TaskPolicy> {
     private final Authorizer authorizer;
@@ -45,8 +43,8 @@ public class TaskPolicyAnnotation implements ApplyAroundAnnotation<TaskPolicy> {
 
             // Tasks are linked to ONE project at a time
             // BatchSearches are not handled yet (they are linked to multiple projects...)
-            Project project = Project.project(ofNullable((String) task.args.get(DEFAULT_PROJECT_OPT)).orElse(DEFAULT_DEFAULT_PROJECT));
-            String projectId = Authorizer.requireValue(project.name, true);
+            String projectId = ofNullable((String) task.args.get(DEFAULT_PROJECT_OPT)).orElseThrow(() -> new IllegalStateException("Task " + taskId + " does not have a project id in its arguments"));
+            Authorizer.requireValue(projectId, false);
 
             // Check if user as role based rights or is owner (if an annotation allowOwner flag is true)
             if (!authorizer.can(user.id, domain, projectId, annotation.role()) && (!annotation.allowOwner() || !isTaskOwner(user, task))) {
