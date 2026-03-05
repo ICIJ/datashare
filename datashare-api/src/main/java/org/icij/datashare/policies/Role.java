be5499be7d7dd04d8e3a2eafcf4660ed12d1c30d
Caroline Desprat <cdesprat@icij.org>
2026-03-05 14:59:06 +0000

    refactor: remove comments

--- datashare-api/src/main/java/org/icij/datashare/policies/Role.java
+++ datashare-api/src/main/java/org/icij/datashare/policies/Role.java
@@ -9,26 +9,3 @@ public enum Role {
     PROJECT_VISITOR,
     NONE
 }
-
-
-//
-//alice,*,delete
-//alice,  domain:domA,manage
-//alice,domain:domB|proj:p3,    write
-//bob, domain:domA,            delete
-//bob, domain:domA|proj:p1,    write
-//bob,domain:domB,            read
-//bob,domain:domB|proj:p3,    read
-//carol,domain:domA|proj:p1,    delete
-//carol,domain:domA|proj:p2,    manage
-//carol,  domain:domB|proj:p1,    read
-//carol,  domain:domA,delete
-//dave,   domain:domA|proj:p1,    read
-//dave,   domain:domA|proj:p1,    write
-//dave,   domain:domA|proj:p1,    delete
-//dave,   domain:domA|proj:p2,    write
-//eve,    domain:domA|proj:p1,    read
-//eve,    domain:domA|proj:p1,    comment
-//eve,    domain:domA|proj:p1,    write
-//frank,  domain:domA|proj:p1,    read
-//frank,  domain:domA|proj:p1,    comment
