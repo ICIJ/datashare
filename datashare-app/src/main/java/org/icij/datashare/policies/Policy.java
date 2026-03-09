fc2566721f8c501d547a6220e6c789fbc0800158
Caroline Desprat <cdesprat@icij.org>
2026-03-09 13:57:12 +0000

    refactor: remove unused @BasePolicy annotation from Policy and TaskPolicy

--- datashare-app/src/main/java/org/icij/datashare/policies/Policy.java
+++ datashare-app/src/main/java/org/icij/datashare/policies/Policy.java
@@ -5,7 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@BasePolicy
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Policy {
