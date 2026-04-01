5a16c0aca1a6c3bc3a25a86734a6f0edd83c2afe
pirhoo <hello@pirhoo.com>
2026-04-01 13:17:00 +0000

    fix(cli): close git.properties stream in DatashareVersionProvider

---
+++ datashare-cli/src/main/java/org/icij/datashare/cli/command/DatashareVersionProvider.java
@@ -0,0 +1,21 @@
+package org.icij.datashare.cli.command;
+
+import picocli.CommandLine;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DatashareVersionProvider implements CommandLine.IVersionProvider {
+    /** Returns a single-element array containing {@code "datashare <version>"}, read from {@code /git.properties}. */
+    @Override
+    public String[] getVersion() throws Exception {
+        Properties versions = new Properties();
+        try (InputStream gitProperties = getClass().getResourceAsStream("/git.properties")) {
+            if (gitProperties != null) {
+                versions.load(gitProperties);
+            }
+        }
+        String version = versions.getProperty("git.build.version", "unknown");
+        return new String[]{"datashare " + version};
+    }
+}
