016848700db1e8b8db11dfab93199c50e0351304
Bruno Thomas <bthomas@icij.org>
2026-06-01 13:42:11 +0000

    refactor: make AsyncSearchOwner immutable

--- datashare-app/src/main/java/org/icij/datashare/asyncsearch/AsyncSearchOwner.java
+++ datashare-app/src/main/java/org/icij/datashare/asyncsearch/AsyncSearchOwner.java
@@ -1,22 +1,27 @@
 package org.icij.datashare.asyncsearch;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Records who submitted an async search and on which projects, so poll/cancel
  * requests carrying only the opaque ES id can be authorized.
  *
+ * Record are not working likely because the public visibility of the properties is removed.
+ *
  * Public no-arg constructor and public fields are required by the Redisson
  * JSON codec used in {@link RedisAsyncSearchStore}.
  */
 public class AsyncSearchOwner {
-    public String userId;
-    public List<String> projects;
+    public final String userId;
+    public final List<String> projects;
 
-    public AsyncSearchOwner() {}
-
-    public AsyncSearchOwner(String userId, List<String> projects) {
+    @JsonCreator
+    public AsyncSearchOwner(@JsonProperty("userId") String userId, @JsonProperty("projects") List<String> projects) {
         this.userId = userId;
-        this.projects = projects;
+        this.projects = Collections.unmodifiableList(projects);
     }
 }
