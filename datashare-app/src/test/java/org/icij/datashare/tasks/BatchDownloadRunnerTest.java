3725dfcce751b6b0eb32d60941b6a9c24300068e
Caroline Desprat <cdesprat@icij.org>
2026-06-02 09:04:18 +0000

    fix(BatchDownloadApp): introduce scheduled cleaner for expired zips #2164
    - Add cleanup logic to `BatchDownloadApp` using a scheduled executor service.
    - Added `BatchDownloadAppTest` to validate scheduler behavior.

--- datashare-app/src/test/java/org/icij/datashare/tasks/BatchDownloadRunnerTest.java
+++ datashare-app/src/test/java/org/icij/datashare/tasks/BatchDownloadRunnerTest.java
@@ -21,7 +21,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
-import java.util.List;
 import java.util.stream.IntStream;
 import java.util.zip.ZipFile;
 
