fff1ba6bcc16ec1ed0c54d94a614dd39bf757518
Caroline Desprat <cdesprat@icij.org>
2026-06-08 17:04:40 +0000

    refactor(session): make UsersInRedis and UsersInDb read-only
    - Change UsersInRedis and UsersInDb to implement Users instead of UsersWritable
    - Remove saveOrUpdate method from both classes (now provided by UsersIdProviderRedisCache)
    - Remove unused imports (JsonObjectMapper, Transaction, List from UsersInRedis)

--- datashare-app/src/main/java/org/icij/datashare/session/UsersInDb.java
+++ datashare-app/src/main/java/org/icij/datashare/session/UsersInDb.java
@@ -3,11 +3,12 @@ package org.icij.datashare.session;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import net.codestory.http.security.User;
+import net.codestory.http.security.Users;
 import org.icij.datashare.Repository;
 import org.icij.datashare.text.Hasher;
 
 @Singleton
-public class UsersInDb implements UsersWritable {
+public class UsersInDb implements Users {
     private final Repository userRepository;
 
     @Inject
@@ -28,10 +29,5 @@ public class UsersInDb implements UsersWritable {
     public User find(String login, String password) {
         org.icij.datashare.user.User user = userRepository.getUser(login);
         return user != null && Hasher.SHA_256.hash(password).equals(user.details.get("password")) ? new DatashareUser(user): null;
-    }
-
-    @Override
-    public boolean saveOrUpdate(User user) {
-        return userRepository.save((DatashareUser)user);
     }
 }
