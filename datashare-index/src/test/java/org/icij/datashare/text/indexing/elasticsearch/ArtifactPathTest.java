c34acce71b08d262575568f30dbb9d521bfd2c47
pirhoo <hello@pirhoo.com>
2026-06-12 11:11:06 +0000

    refactor(structure): drop unused single-file structure path helper

--- datashare-index/src/test/java/org/icij/datashare/text/indexing/elasticsearch/ArtifactPathTest.java
+++ datashare-index/src/test/java/org/icij/datashare/text/indexing/elasticsearch/ArtifactPathTest.java
@@ -13,13 +13,6 @@ public class ArtifactPathTest {
     }
 
     @Test
-    public void test_structure_markdown_file_path() {
-        Path root = Path.of("/artifact/prj");
-        assertThat(ArtifactPath.structureMarkdown(root, "6abb96950946b62bb993307c8945c0c096982783bab7fa24901522426840ca3e").toString())
-                .isEqualTo("/artifact/prj/6a/bb/6abb96950946b62bb993307c8945c0c096982783bab7fa24901522426840ca3e/structure.md");
-    }
-
-    @Test
     public void test_structure_dir_path() {
         Path root = Path.of("/artifact/prj");
         assertThat(ArtifactPath.structureDir(root, "6abb96950946b62bb993307c8945c0c096982783bab7fa24901522426840ca3e").toString())
