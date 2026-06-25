87a53067f66ff73858928d28d6d1ef7acb8b10b9
pirhoo <hello@pirhoo.com>
2026-06-25 09:28:11 +0000

    fix(security): enforce project membership on FtM document endpoint

--- datashare-app/src/test/java/org/icij/datashare/web/FtmResourceTest.java
+++ datashare-app/src/test/java/org/icij/datashare/web/FtmResourceTest.java
@@ -1,5 +1,8 @@
 package org.icij.datashare.web;
 
+import org.icij.datashare.PropertiesProvider;
+import org.icij.datashare.db.JooqRepository;
+import org.icij.datashare.session.LocalUserFilter;
 import org.icij.datashare.tasks.MockIndexer;
 import org.icij.datashare.text.indexing.Indexer;
 import org.icij.datashare.web.testhelpers.AbstractProdWebServerTest;
@@ -13,25 +16,33 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 public class FtmResourceTest extends AbstractProdWebServerTest {
     @Mock Indexer mockEs;
+    @Mock JooqRepository jooqRepository;
     MockIndexer mockIndexer;
 
     @Test
     public void test_doc_id() {
-        mockIndexer.indexFile("prj", "doc_id", Path.of("path/to/doc.pdf"), "application/pdf");
-        get("/api/ftm/prj/doc_id").should().respond(200).haveType("application/json")
+        mockIndexer.indexFile("local-datashare", "doc_id", Path.of("path/to/doc.pdf"), "application/pdf");
+        get("/api/ftm/local-datashare/doc_id").should().respond(200).haveType("application/json")
                 .contain("path/to/doc.pdf")
                 .not().contain("null");
     }
 
     @Test
     public void test_doc_id_not_found() {
-        get("/api/ftm/prj/unknown_doc_id").should().respond(404);
+        get("/api/ftm/local-datashare/unknown_doc_id").should().respond(404);
+    }
+
+    @Test
+    public void test_doc_id_forbidden_for_non_member_project() {
+        mockIndexer.indexFile("foo_index", "doc_id", Path.of("path/to/doc.pdf"), "application/pdf");
+        get("/api/ftm/foo_index/doc_id").should().respond(403);
     }
 
     @Before
     public void setUp() {
         initMocks(this);
         this.mockIndexer = new MockIndexer(mockEs);
-        configure(routes -> routes.add(new FtmResource(mockEs)));
+        configure(routes -> routes.add(new FtmResource(mockEs))
+                .filter(new LocalUserFilter(new PropertiesProvider(), jooqRepository)));
     }
 }
