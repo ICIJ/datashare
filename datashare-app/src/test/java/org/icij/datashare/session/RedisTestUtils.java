092342066f4edfd17448d86fdaa4800faa58ab2f
Caroline Desprat <cdesprat@icij.org>
2026-07-20 17:13:59 +0000

    refactor(test): move `addrForConnectionName` to `RedisPoolFactoryTest` for improved test encapsulation

--- datashare-app/src/test/java/org/icij/datashare/session/RedisTestUtils.java
+++ datashare-app/src/test/java/org/icij/datashare/session/RedisTestUtils.java
@@ -7,19 +7,6 @@ import java.lang.reflect.Field;
 final class RedisTestUtils {
     private RedisTestUtils() {}
 
-    static String addrForConnectionName(String clientList, String connectionName) {
-        for (String line : clientList.split("\n")) {
-            if (line.contains("name=" + connectionName + " ")) {
-                for (String field : line.split(" ")) {
-                    if (field.startsWith("addr=")) {
-                        return field.substring("addr=".length());
-                    }
-                }
-            }
-        }
-        throw new IllegalStateException("no client found with name " + connectionName + " in:\n" + clientList);
-    }
-
     static void closeRedisPool(Object redisBackedInstance) throws Exception {
         Field poolField = redisBackedInstance.getClass().getDeclaredField("redis");
         poolField.setAccessible(true);
