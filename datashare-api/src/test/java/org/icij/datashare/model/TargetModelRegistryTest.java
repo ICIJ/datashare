e1aaebe6accf191fa8ca2cbaef6d070899a67d36
pirhoo <hello@pirhoo.com>
2026-08-21 11:06:47 +0000

    feat(model): select a data model by name through a registry
    Closes #2197
    Closes #2200

---
+++ datashare-api/src/test/java/org/icij/datashare/model/TargetModelRegistryTest.java
@@ -0,0 +1,29 @@
+package org.icij.datashare.model;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+public class TargetModelRegistryTest {
+    @Test
+    public void test_resolves_the_ftm_model() {
+        assertThat(TargetModelRegistry.get("ftm").name()).isEqualTo("ftm");
+    }
+
+    @Test
+    public void test_parses_the_model_once() {
+        assertThat(TargetModelRegistry.get("ftm")).isSameAs(TargetModelRegistry.get("ftm"));
+    }
+
+    @Test
+    public void test_an_unknown_model_names_the_known_ones() {
+        try {
+            TargetModelRegistry.get("wikidata");
+            fail("should have refused an unregistered model");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).contains("wikidata");
+            assertThat(e.getMessage()).contains("ftm");
+        }
+    }
+}
